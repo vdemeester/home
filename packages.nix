fe52de15c68b5d4a83ddc8f4427b02ff0d1bacaa
Vincent Demeester <vincent@sbr.pm>
2016-09-11 21:58:09 +0200

    Add zsh and set it default for my user

--- packages.nix
+++ packages.nix
@@ -1,11 +1,16 @@
 { config, pkgs, ... }:
 
 {
+	programs = {
+		 zsh.enable = true;
+	};
 	environment = {
 		systemPackages = with pkgs; [
 				wget
 				git
 				vim
+				tmux
+				htop
 		];
 	};
 }
--- users.nix
+++ users.nix
@@ -8,6 +8,7 @@
 				uid = 1000;
 				createHome = true;
 				extraGroups = [ "networkmanager" "wheel" ];
+				shell = "/run/current-system/sw/bin/zsh";
 				initialPassword = "changeMe";
 			};
 		};
