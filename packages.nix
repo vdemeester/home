40731b3fcb83c3cae0af44390f4a4e46ffea8a40
Vincent Demeester <vincent@sbr.pm>
2016-09-17 22:16:23 +0200

    Order packages alphabetically

--- packages.nix
+++ packages.nix
@@ -9,19 +9,19 @@
 			EDITOR = pkgs.lib.mkOverride 0 "vim";
 		};
 		systemPackages = with pkgs; [
-				wget
-				git
-				haskellPackages.git-annex
-				vim
-				tmux
-				htop
-				tree
-				lsof
-				psmisc
 				aspell
 				aspellDicts.en
 				aspellDicts.fr
 				direnv
+				git
+				haskellPackages.git-annex
+				htop
+				lsof
+				psmisc
+				tmux
+				tree
+				vim
+				wget
 		];
 	};
         
