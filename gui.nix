8502eefb8045579bbe8a5e7f7c04de20db2bf943
Vincent Demeester <vincent@sbr.pm>
2016-09-20 10:50:40 +0200

    Use pkgs expression
    Signed-off-by: Vincent Demeester <vincent@sbr.pm>

--- gui.nix
+++ gui.nix
@@ -40,7 +40,7 @@
 			};
 			displayManager = {
 				sessionCommands = ''
-xmodmap ~/.Xmodmap &
+${pkgs.xlibs.xmodmap}/bin/xmodmap ~/.Xmodmap &
 xss-lock -- ${pkgs.slim}/bin/slimlock &
 ${pkgs.networkmanagerapplet}/bin/nm-applet &
 ${pkgs.pythonPackages.udiskie}/bin/udiskie -a -t -n -F &
