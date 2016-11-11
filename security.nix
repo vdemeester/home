3c44b25b21b6ab91c63cb91d1f4ed107c7df4052
Vincent Demeester <vincent@sbr.pm>
2016-11-11 14:02:06 +0100

    Commenting yubikey stuff out

--- security.nix
+++ security.nix
@@ -10,11 +10,11 @@
 
 	services.pcscd.enable = true;
 	
-	services.udev.extraRules = ''
+	#services.udev.extraRules = ''
     # Yubico YubiKey
-    KERNEL=="hidraw*", SUBSYSTEM=="hidraw", ATTRS{idVendor}=="1050", ATTRS{idProduct}=="0113|0114|0115|0116|0120|0402|0403|0406|0407|0410", TAG+="uaccess"
-    ACTION=="remove", ENV{ID_VENDOR_ID}=="1050", ENV{ID_MODEL_ID}=="0113|0114|0115|0116|0120|0402|0403|0406|0407|0410", RUN+="/run/current-system/sw/bin/loginctl lock-sessions"
-'';
+    #KERNEL=="hidraw*", SUBSYSTEM=="hidraw", ATTRS{idVendor}=="1050", ATTRS{idProduct}=="0113|0114|0115|0116|0120|0402|0403|0406|0407|0410", TAG+="uaccess"
+   #ACTION=="remove", ENV{ID_VENDOR_ID}=="1050", ENV{ID_MODEL_ID}=="0113|0114|0115|0116|0120|0402|0403|0406|0407|0410", RUN+="/run/current-system/sw/bin/loginctl lock-sessions"
+#'';
 	environment = {
 		systemPackages = with pkgs; [
 				yubico-piv-tool
