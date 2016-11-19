bc5bd8e549d1e15f5db646bbadbbe2773e08acaf
Vincent Demeester <vincent@sbr.pm>
2016-11-20 00:43:35 +0100

    More fine-tuning for tlp
    Signed-off-by: Vincent Demeester <vincent@sbr.pm>

--- hardware/thinkpad-t460s-configuration.nix
+++ hardware/thinkpad-t460s-configuration.nix
@@ -15,6 +15,25 @@ fi
 		tlp = {
 			enable = true;
 			extraConfig = ''
+# CPU optimizations
+CPU_SCALING_GOVERNOR_ON_AC=performance
+CPU_SCALING_GOVERNOR_ON_BAT=powersave
+CPU_MIN_PERF_ON_AC=0
+CPU_MAX_PERF_ON_AC=100
+CPU_MIN_PERF_ON_BAT=0
+CPU_MAX_PERF_ON_BAT=50
+# DEVICES (wifi, ..)
+DEVICES_TO_DISABLE_ON_STARTUP="wifi wwan"
+DEVICES_TO_ENABLE_ON_AC="bluetooth wifi wwan"
+DEVICES_TO_DISABLE_ON_BAT="bluetooth"
+# Network management
+DEVICES_TO_DISABLE_ON_LAN_CONNECT="wifi wwan"
+DEVICES_TO_DISABLE_ON_WIFI_CONNECT=""
+DEVICES_TO_DISABLE_ON_WWAN_CONNECT=""
+DEVICES_TO_ENABLE_ON_LAN_DISCONNECT="wifi wwan"
+DEVICES_TO_ENABLE_ON_WIFI_DISCONNECT=""
+DEVICES_TO_ENABLE_ON_WWAN_DISCONNECT="" 
+# Make sure it uses the right hard drive
 DISK_DEVICES="nvme0n1p3"
 			'';
 		};
