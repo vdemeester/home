7437d52ece69f6502d0e55e7542accc70eed69d8
Vincent Demeester <vincent@sbr.pm>
2016-11-20 15:14:38 +0100

    Update custom-packages format :)
    Signed-off-by: Vincent Demeester <vincent@sbr.pm>

--- configuration/custom-packages.nix
+++ configuration/custom-packages.nix
@@ -1,21 +1,20 @@
 { config, lib, pkgs, ... }:
 
 {
-  nixpkgs.config = {
-  packageOverrides = self: with self; let
-    fetchNixPkgs = { rev, sha256 }:
-    fetchFromGitHub {
-      inherit sha256 rev;
-      owner = "vdemeester";
-      repo = "sbrpkgs";
-    };
-    pinnedPkgs = import (fetchNixPkgs {
-      rev = "8e0a7940b2e18996dd18905b444b65f08d5917fa";
-      sha256 = "05wg6sa7hi95zppkr3lj8sgxfisz7m8293wbgav3glvmv80jhwif";
-    }) {};
-
-  in {
-    inherit (pinnedPkgs) doctl dobi vndr sbr-i3-config;
-  };
-  };
+	nixpkgs.config = {
+		packageOverrides = self: with self; let
+			fetchNixPkgs = { rev, sha256 }:
+				fetchFromGitHub {
+					inherit sha256 rev;
+					owner = "vdemeester";
+					repo = "sbrpkgs";
+				};
+			pinnedPkgs = import (fetchNixPkgs {
+				rev = "8e0a7940b2e18996dd18905b444b65f08d5917fa";
+				sha256 = "05wg6sa7hi95zppkr3lj8sgxfisz7m8293wbgav3glvmv80jhwif";
+			}) {};
+		in {
+			inherit (pinnedPkgs) doctl dobi vndr;
+		};
+	};
 }
