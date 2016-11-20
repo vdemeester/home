4295efc87b7473d6465bdf5309c39e078f9430a5
Vincent Demeester <vincent@sbr.pm>
2016-11-20 15:23:56 +0100

    Add custom-packages to honshu
    To make it build.. But this should be better handled in the future. Like
    nix files that needs custom-packages should be the one to import it —
    and we should split files even more…

--- machine/honshu.nix
+++ machine/honshu.nix
@@ -4,6 +4,7 @@
 	imports =
 		[ # Include the results of the hardware scan.
 			../hardware-configuration.nix
+			../configuration/custom-packages.nix
 			../configuration/common.nix
 			../hardware/dell-latitude-e6540.nix
 		];
