8195486ee3612cc94f8953378cf17b996096535a
Vincent Demeester <vincent@sbr.pm>
2016-11-22 16:50:11 +0100

    Share boot.kernelParams for both thinkpad :)

--- hardware/thinkpad-t460s.nix
+++ hardware/thinkpad-t460s.nix
@@ -5,18 +5,6 @@
 		[
 			./thinkpad.nix
 		];
-        boot = {
-                kernelParams = [
-      		        # Kernel GPU Savings Options (NOTE i915 chipset only)
-			"i915.enable_rc6=1" "i915.enable_fbc=1"
-			"i915.lvds_use_ssc=0"
-      			"drm.debug=0" "drm.vblankoffdelay=1"
-    		];
-    	blacklistedKernelModules = [
-      	        # Kernel GPU Savings Options (NOTE i915 chipset only)
-      		"sierra_net" "cdc_mbim" "cdc_ncm"
-    	];
-};
 	services = {
 		acpid = {
 			lidEventCommands = ''
--- hardware/thinkpad.nix
+++ hardware/thinkpad.nix
@@ -4,6 +4,18 @@
 	environment.systemPackages = with pkgs; [
 		linuxPackages_4_8.tp_smapi
 	];
+	boot = {
+		kernelParams = [
+			# Kernel GPU Savings Options (NOTE i915 chipset only)
+			"i915.enable_rc6=1" "i915.enable_fbc=1"
+			"i915.lvds_use_ssc=0"
+			"drm.debug=0" "drm.vblankoffdelay=1"
+		];
+		blacklistedKernelModules = [
+			# Kernel GPU Savings Options (NOTE i915 chipset only)
+			"sierra_net" "cdc_mbim" "cdc_ncm"
+		];
+	};
 	services = {
 		acpid = {
 			enable = true;
