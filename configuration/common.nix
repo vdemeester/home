50338444a926d2afce4d712cf742bf92a0e055f6
Vincent Demeester <vincent@sbr.pm>
2016-11-26 01:05:03 +0100

    Disable supported locales for now…
    … nixos is acting weird…
    
    Signed-off-by: Vincent Demeester <vincent@sbr.pm>

--- configuration/common.nix
+++ configuration/common.nix
@@ -23,7 +23,7 @@
 		consoleFont = "Lat2-Terminus16";
 		consoleKeyMap = "fr";
 		defaultLocale = "en_US.UTF-8";
-		supportedLocales = [ "en_US/ISO-8859-1" "fr_FR/ISO-8859-1" "fr_FR@euro/ISO-8859-15" ];
+		# supportedLocales = [ "en_US/ISO-8859-1" "fr_FR/ISO-8859-1" "fr_FR@euro/ISO-8859-15" ];
 	};
 
 	time.timeZone = "Europe/Paris";
