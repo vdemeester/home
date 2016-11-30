78ea097658c2cfa269fdcdb1f49efe4e3ac48340
Vincent Demeester <vincent@sbr.pm>
2016-12-01 00:40:24 +0100

    Update hokkaido slim theme hash
    Signed-off-by: Vincent Demeester <vincent@sbr.pm>

--- machine/hokkaido.nix
+++ machine/hokkaido.nix
@@ -13,6 +13,6 @@
 
 	services.xserver.displayManager.slim.theme = pkgs.fetchurl {
 						url = "https://github.com/vdemeester/slim-themes/raw/master/docker-nuage-theme-0.1.tar.xz";
-						sha256 = "127893l1nzqya0g68k8841g5lm3hlnx7b3b3h06axvplc54a1jd8";
+						sha256 = "1ds7p3d8dn21bankgs68i53hqrj4d2abpk437h6dbjz36q1ys839";
 						};
 }
