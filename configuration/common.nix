859c12c6e9b4c17432371dc5bc8e3e5aa47bcacb
Vincent Demeester <vincent@sbr.pm>
2016-12-10 13:16:23 +0100

    Remove duplicate configurations
    Signed-off-by: Vincent Demeester <vincent@sbr.pm>

--- configuration/common.nix
+++ configuration/common.nix
@@ -18,36 +18,10 @@
 			./users.nix
 			./packages.nix
 		];
-	boot.loader.systemd-boot.enable = true;
-	boot.loader.efi.canTouchEfiVariables = true;
-	boot.kernelPackages = pkgs.linuxPackages_4_8;
 	i18n = {
 		consoleFont = "Lat2-Terminus16";
 		consoleKeyMap = "fr";
 		defaultLocale = "en_US.UTF-8";
 		# supportedLocales = [ "en_US/ISO-8859-1" "fr_FR/ISO-8859-1" "fr_FR@euro/ISO-8859-15" ];
-	};
-
-	time.timeZone = "Europe/Paris";
-
-	system = {
-		stateVersion = "16.09";	
-		autoUpgrade = {
-			enable = true;
-			dates = "13:00";
-		};
-	};
-
-	nix = {
-		useSandbox = true;
-		# if hydra is down, don't wait forever
-		extraOptions = ''
-		    connect-timeout = 20
-		'';
-	};
-	nixpkgs = {
-		config = {
-			allowUnfree = true;
-		};
 	};
 }
