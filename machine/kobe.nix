3007bb897ac9e2e538f418028a5f43e0c69ba0f7
Vincent Demeester <vincent@sbr.pm>
2017-01-30 10:40:44 +0000

    Add a timer for ape up 🐒
    Using `systemd --user`… Also update timezone to the right one.
    
    Signed-off-by: Vincent Demeester <vincent@sbr.pm>

--- machine/kobe.nix
+++ machine/kobe.nix
@@ -13,6 +13,8 @@
 			../service/ssh-tunnel.nix
 		];
 
+	time.timeZone = "Europe/Paris";
+
 	# Enable the OpenSSH daemon.
 	services.openssh.enable = true;
 	users.users.root.openssh.authorizedKeys.keys =
@@ -21,6 +23,7 @@
 	virtualisation.docker.extraOptions = "--label=type=server -H unix:///var/run/docker.sock -H 0.0.0.0:2375";
 	networking.firewall.allowedTCPPorts = [ 2375 ];
 
+	# remote reverse ssh tunnel
 	services.ssh-tunnel = {
 		enable = true;
 		localUser = "vincent";
@@ -29,4 +32,28 @@
 		remoteUser = "vincent";
 		bindPort = 2222;
 	};
+
+	# TODO Move this in a profile
+	environment.systemPackages = with pkgs; [
+		ape
+	];
+	# ape – sync git mirrors
+	systemd.user.services.ape = {
+		description = "Ape - sync git mirrors";
+		wantedBy = [ "multi-user.target" ];
+		serviceConfig = {
+			Type = "oneshot";
+			ExecStart = "${pkgs.ape}/bin/ape up /home/vincent/mirrors/";
+			Environment = "PATH=/run/current-system/sw/bin/";
+		};
+	};
+	systemd.user.timers.ape = {
+		description = "Ape hourly";
+		wantedBy = [ "timers.target" ];
+		timerConfig = {
+			OnCalendar = "hourly";
+			Persistent = "true";
+		};
+	};
+	systemd.user.timers.ape.enable = true;
 }
