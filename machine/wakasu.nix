0e7a6d052fcb03cccbc940cfd02a03f095568cb2
Vincent Demeester <vincent@sbr.pm>
2017-02-16 13:57:12 +0100

    Try to setup synergy at work
    Signed-off-by: Vincent Demeester <vincent@sbr.pm>

--- machine/wakasu.nix
+++ machine/wakasu.nix
@@ -14,6 +14,13 @@
 			../hardware/thinkpad-t460s.nix
 		];
 
+	services.synergy.client = {
+		enable = true;
+		autoStart = false;
+		screenName = "wakasu";
+		serverAddress = "hokkaido.local";
+	};
+
 	services.xserver.displayManager.slim.theme = pkgs.fetchurl {
 						url = "https://github.com/vdemeester/slim-themes/raw/master/docker-key-theme-0.1.tar.xz";
 						sha256 = "127893l1nzqya0g68k8841g5lm3hlnx7b3b3h06axvplc54a1jd8";
