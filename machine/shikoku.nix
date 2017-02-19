1104879d4dbd98636fac39f4227f641c8480ceff
Vincent Demeester <vincent@sbr.pm>
2017-02-19 23:09:08 +0100

    Add a slim theme for shikoku
    Signed-off-by: Vincent Demeester <vincent@sbr.pm>

--- machine/shikoku.nix
+++ machine/shikoku.nix
@@ -39,4 +39,9 @@ section: options
 	keystroke(super+shift+right) = switchInDirection(right)
 end
 ''; };
+
+	services.xserver.displayManager.slim.theme = pkgs.fetchurl {
+						url = "https://github.com/vdemeester/slim-themes/raw/master/docker-paris-theme-0.1.tar.xz";
+						sha256 = "1kp30qbxiwv0g4z6gsy2hjacpmm96lr2id1cdwizzf2lrash2hsi";
+						};
 }
