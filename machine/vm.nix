dde7578aae72376548d6fac02528113b7852dd4e
Vincent Demeester <vincent@sbr.pm>
2017-02-28 18:19:23 +0100

    Add a simple vm configuration
    Signed-off-by: Vincent Demeester <vincent@sbr.pm>

---
+++ machine/vm.nix
@@ -0,0 +1,30 @@
+{ config, pkgs, ... }:
+
+{
+	imports =
+		[ # Include the results of the hardware scan.
+			../hardware-configuration.nix
+			../configuration/custom-packages.nix
+			../configuration/common.nix
+			../profiles/desktop.nix
+			../profiles/ssh.nix
+			../profiles/virtualization.nix
+			../profiles/dockerization.nix
+		];
+
+	boot = {
+		loader = {
+			grub = {
+				enable = true;
+				device = "/dev/sda";
+			};
+		};
+		initrd = {
+			checkJournalingFS = false;
+		};
+	};
+
+	virtualisation.virtualbox.guest.enable = true;
+
+	service.xserver.xkbVariant = "bepo";
+}
