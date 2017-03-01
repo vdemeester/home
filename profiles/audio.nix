6d01d9df28aa91d244fb592d3991da0fe3c63186
Vincent Demeester <vincent@sbr.pm>
2017-03-01 16:40:38 +0100

    Put the right port..
    Signed-off-by: Vincent Demeester <vincent@sbr.pm>

--- profiles/audio.nix
+++ profiles/audio.nix
@@ -11,8 +11,8 @@
 	sound.enableMediaKeys = true;
 
 	# spotify
-	networking.firewall.allowedTCPPorts = [ 57621 5762 ];
-	networking.firewall.allowedUDPPorts = [ 57621 5762 ];
+	networking.firewall.allowedTCPPorts = [ 57621 57622 ];
+	networking.firewall.allowedUDPPorts = [ 57621 57622 ];
 	
 	environment.systemPackages = with pkgs; [
 		apulse       # allow alsa application to use pulse
