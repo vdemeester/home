161927f89b8688d9b291387d06d6ab02e50ed23b
Vincent Demeester <vincent@sbr.pm>
2017-04-22 14:09:28 +0200

    Update hokkaido machine
    - Remove synergy server (it's now a client)
    - Setup multi-screen
    
    Signed-off-by: Vincent Demeester <vincent@sbr.pm>

--- machine/hokkaido.nix
+++ machine/hokkaido.nix
@@ -10,7 +10,6 @@
 			../profiles/laptop.nix
 			../profiles/virtualization.nix
 			../profiles/dockerization.nix
-			../profiles/synergy-server.nix
 			../location/docker.nix
 			../hardware/thinkpad-x220.nix
 			../service/ssh-tunnel.nix
@@ -19,23 +18,6 @@
 	services.openssh.enable = true;
 	services.openssh.forwardX11 = true;
 
-	environment.etc."synergy-server.conf" = { text = ''
-section: screens
-	hokkaido:
-	wakasu:
-end
-section: links
-	hokkaido:
-		left = wakasu
-	wakasu:
-		right = hokkaido
-end
-section: options
-	keystroke(super+shift+left) = switchInDirection(left)
-	keystroke(super+shift+right) = switchInDirection(right)
-end
-''; };
-	
 	services.ssh-tunnel = {
 		enable = true;
 		localUser = "vincent";
@@ -44,9 +26,32 @@ end
 		remoteUser = "vincent";
 		bindPort = 2223;
 	};
-	
-	services.xserver.displayManager.slim.theme = pkgs.fetchurl {
-						url = "https://github.com/vdemeester/slim-themes/raw/master/docker-nuage-theme-0.1.tar.xz";
-						sha256 = "1ds7p3d8dn21bankgs68i53hqrj4d2abpk437h6dbjz36q1ys839";
-						};
+	services.xserver = {
+		enable = true;
+		videoDrivers = [ "intel" ];
+		#xrandrHeads = [ "LVDS1" "DP1" ];
+		monitorSection = ''
+EndSection
+Section "Monitor"
+	Identifier "DP1"
+	Option "PreferredMode" "2560x1080"
+	Option "DPMS" "true"
+	Option "RightOf" "LVDS1"
+EndSection
+Section "Monitor"
+	Identifier "LVDS1"
+	Option "Primary" "true"
+	Option "DPMS" "true"
+				'';
+		deviceSection = ''
+Option "Monitor-HDMI1" "HDMI1"
+Option "Monitor-eDP1" "eDP1"
+'';
+		exportConfiguration = true;
+		displayManager.slim.theme = pkgs.fetchurl {
+			url = "https://github.com/vdemeester/slim-themes/raw/master/docker-nuage-theme-0.1.tar.xz";
+
+			sha256 = "1ds7p3d8dn21bankgs68i53hqrj4d2abpk437h6dbjz36q1ys839";
+		};
+	};
 }
