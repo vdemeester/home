21930da7fe5990427239b7e50a0277707c04617a
Vincent Demeester <vincent@sbr.pm>
2017-05-31 08:48:50 -0700

    Fix environment variables
    Signed-off-by: Vincent Demeester <vincent@sbr.pm>

--- hardware/thinkpad-t460s.nix
+++ hardware/thinkpad-t460s.nix
@@ -42,7 +42,9 @@ DISK_DEVICES="nvme0n1p3"
 	services.xserver = {
 		dpi = 144;
 	};
-	variables.QT_DEVICE_PIXEL_RATIO = "2";
-	variables.GDK_SCALE = "2";
-	variables.GDK_DPI_SCALE = "0.5";
+	environment = {
+		variables.QT_DEVICE_PIXEL_RATIO = "2";
+		variables.GDK_SCALE = "2";
+		variables.GDK_DPI_SCALE = "0.5";
+	};
 }
