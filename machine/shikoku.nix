6f1eb445dc441866baebe9426131c6096ad8c058
Vincent Demeester <vincent@sbr.pm>
2017-07-02 14:08:00 +0200

    Update shikoku theme :)
    Signed-off-by: Vincent Demeester <vincent@sbr.pm>

--- machine/shikoku.nix
+++ machine/shikoku.nix
@@ -28,8 +28,8 @@
 xrandr --output HDMI-0 --off --output DP-4 --auto --dpi 96 &
 '';
 				slim.theme = pkgs.fetchurl {
-					url = "https://github.com/vdemeester/slim-themes/raw/master/docker-paris-theme-0.1.tar.xz";
-					sha256 = "1kp30qbxiwv0g4z6gsy2hjacpmm96lr2id1cdwizzf2lrash2hsi";
+					url = "https://github.com/vdemeester/slim-themes/raw/master/docker-nuage-theme-0.1.tar.xz";
+					sha256 = "1ds7p3d8dn21bankgs68i53hqrj4d2abpk437h6dbjz36q1ys839";
 				};
 			};
 		};
--- machine/wakasu.nix
+++ machine/wakasu.nix
@@ -27,8 +27,8 @@
 	time.timeZone = "Europe/Paris";
 
 	services.xserver.displayManager.slim.theme = pkgs.fetchurl {
-						url = "https://github.com/vdemeester/slim-themes/raw/master/docker-key-theme-0.1.tar.xz";
-						sha256 = "127893l1nzqya0g68k8841g5lm3hlnx7b3b3h06axvplc54a1jd8";
-						};
-        networking.firewall.allowedTCPPorts = [ 8080 8000 ];
+		url = "https://github.com/vdemeester/slim-themes/raw/master/docker-key-theme-0.1.tar.xz";
+		sha256 = "127893l1nzqya0g68k8841g5lm3hlnx7b3b3h06axvplc54a1jd8";
+	};
+	networking.firewall.allowedTCPPorts = [ 8080 8000 ];
 }
