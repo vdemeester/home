98b7b4779a35acefa5ff5bfcd7aacddf2811dc66
Vincent Demeester <vincent@sbr.pm>
2017-07-25 14:32:08 +0200

    Make sure udiskie have the right rights...
    Signed-off-by: Vincent Demeester <vincent@sbr.pm>

--- profiles/desktop.nix
+++ profiles/desktop.nix
@@ -144,6 +144,28 @@ ${pkgs.pythonPackages.udiskie}/bin/udiskie -a -t -n -F &
 		subject.local && subject.active && subject.isInGroup("users")) {
 			return polkit.Result.YES;
 		}
+		var YES = polkit.Result.YES;
+		var permission = {
+			// required for udisks1:
+			"org.freedesktop.udisks.filesystem-mount": YES,
+			"org.freedesktop.udisks.luks-unlock": YES,
+			"org.freedesktop.udisks.drive-eject": YES,
+			"org.freedesktop.udisks.drive-detach": YES,
+			// required for udisks2:
+			"org.freedesktop.udisks2.filesystem-mount": YES,
+			"org.freedesktop.udisks2.encrypted-unlock": YES,
+			"org.freedesktop.udisks2.eject-media": YES,
+			"org.freedesktop.udisks2.power-off-drive": YES,
+			// required for udisks2 if using udiskie from another seat (e.g. systemd):
+			"org.freedesktop.udisks2.filesystem-mount-other-seat": YES,
+			"org.freedesktop.udisks2.filesystem-unmount-others": YES,
+			"org.freedesktop.udisks2.encrypted-unlock-other-seat": YES,
+			"org.freedesktop.udisks2.eject-media-other-seat": YES,
+			"org.freedesktop.udisks2.power-off-drive-other-seat": YES
+		};
+		if (subject.isInGroup("wheel")) {
+			return permission[action.id];
+		}
 	});
 	'';
 	# Auto commit some repositories
