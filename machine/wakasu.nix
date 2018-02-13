490d5df6d1aa761c079e4175b3e51e1e36a25679
Vincent Demeester <vincent@sbr.pm>
2018-02-13 16:42:47 +0100

    Remove port I didn't know where opened
    Signed-off-by: Vincent Demeester <vincent@sbr.pm>

--- machine/wakasu.nix
+++ machine/wakasu.nix
@@ -50,5 +50,4 @@
 		url = "https://github.com/vdemeester/slim-themes/raw/master/docker-key-theme-0.1.tar.xz";
 		sha256 = "127893l1nzqya0g68k8841g5lm3hlnx7b3b3h06axvplc54a1jd8";
 	};
-	networking.firewall.allowedTCPPorts = [ 8080 8000 ];
 }
