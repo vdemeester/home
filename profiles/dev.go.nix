c69435ab4f06a7e47da1f8782943b6d3c09c06b8
Vincent Demeester <vincent@sbr.pm>
2018-03-21 18:18:41 +0100

    Use go instead of go_1.9 and update to 1.10
    Signed-off-by: Vincent Demeester <vincent@sbr.pm>

--- profiles/dev.go.nix
+++ profiles/dev.go.nix
@@ -15,15 +15,15 @@
       goPkgs = import (fetchNixPkgs {
       owner = "NixOS";
       repo = "nixpkgs-channels";
-			rev = "cc4677c36ee8d880e881459ad114fd2224b3ac1c";
-			sha256 = "1rc1pjnvfi194gka45zc1nivzsncc819kvxlfv277l2c8ryhgbpc";
+			rev = "0e7c9b32817e5cbe61212d47a6cf9bcd71789322";
+			sha256 = "1dm777cmlhqcwlrq8zl9q2d87h3p70rclpvq36y43kp378f3pd0y";
     }) {};
 		in {
-			inherit (goPkgs) go_1_9 gotools golint godef gocode gotests gopkgs gomodifytags go-outline go-symbols goconvey delve vndr dep;
+			inherit (goPkgs) go gotools golint godef gocode gotests gopkgs gomodifytags go-outline go-symbols goconvey delve vndr dep;
 		};
 	};
   environment.systemPackages = with pkgs; [
-    go_1_9
+    go
     gcc
     # tools
     gotools
