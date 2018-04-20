2496a6479bdfb9565f99530a3fee9b695c9d00b6
Vincent Demeester <vincent@sbr.pm>
2018-04-20 23:32:45 +0200

    Move home-manager to base
    Signed-off-by: Vincent Demeester <vincent@sbr.pm>

--- envs/base.nix
+++ envs/base.nix
@@ -1,6 +1,14 @@
 { pkgs, prefix, ...}:
 
-{
+let home_directory = builtins.getEnv "HOME"; in
+
+rec {
+  programs = {
+    home-manager = {
+      enable = true;
+      path = "${home_directory}/src/nix/home-manager";
+    };
+  };
   home.packages = with pkgs; [
     jq
     htop
--- envs/remote-dev.nix
+++ envs/remote-dev.nix
@@ -1,14 +1,6 @@
 {...}:
 
-let home_directory = builtins.getEnv "HOME"; in
-
-rec {
-  programs = {
-    home-manager = {
-      enable = true;
-      path = "${home_directory}/src/nix/home-manager";
-    };
-  };
+{
   imports = [
     ./base.nix
     ./dev.nix
