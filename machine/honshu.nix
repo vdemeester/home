553db207d3f45cf31e84d51f30c51e0355dfcf6a
Vincent Demeester <vincent@sbr.pm>
2018-04-26 15:13:57 +0200

    Make honshu server :)
    Signed-off-by: Vincent Demeester <vincent@sbr.pm>

--- machine/honshu.nix
+++ machine/honshu.nix
@@ -4,15 +4,12 @@
 	imports =
 		[ # Include the results of the hardware scan.
 			../hardware-configuration.nix
-			../profiles/ssh.nix
-			../profiles/laptop.nix
+			../hardware/dell-latitude-e6540.nix
+			../profiles/server.nix
 			../profiles/dev.python.nix
 			../profiles/dev.go.nix
-			../profiles/virtualization.nix
 			../profiles/dockerization.nix
-			../profiles/office.nix
 			../location/home.nix
-			../hardware/dell-latitude-e6540.nix
 			../service/ssh-tunnel.nix
 		];
 
