6b7b400feb49a1ac935a579f433c3d0a54a8d263
Vincent Demeester <vincent@sbr.pm>
2018-04-28 15:12:32 +0200

    Move redshift to user config
    Signed-off-by: Vincent Demeester <vincent@sbr.pm>

--- envs/wakasu.nix
+++ envs/wakasu.nix
@@ -4,4 +4,11 @@
   programs.autorandr = {
     enable = true;
   };
+  services.redshift = {
+    enable = true;
+    brightness = { day = "1"; night = "0.9"; };
+    latitude = "48.3";
+    longitude = "7.5";
+    tray = true;
+  };
 }
