395a3bca26baf8129379e4287843c7ecda53fff8
Vincent Demeester <vincent@sbr.pm>
2018-04-29 21:22:40 +0200

    Update honshu (no X dependencies)
    Signed-off-by: Vincent Demeester <vincent@sbr.pm>

--- machine/honshu.nix
+++ machine/honshu.nix
@@ -23,7 +23,6 @@
 	services = {
 		openssh = {
 			enable = true;
-			forwardX11 = true;
 		};
 		ssh-tunnel = {
 			enable = true;
@@ -32,37 +31,6 @@
 			remotePort = 22;
 			remoteUser = "vincent";
 			bindPort = 2224;
-		};
-		printing = {
-			enable = true;
-			drivers = [ pkgs.gutenprint ];
-		};
-		xrdp.enable = true;
-		xrdp.defaultWindowManager = "${pkgs.i3}/bin/i3";
-		xserver = {
-			enable = true;
-			videoDrivers = [ "intel" ];
-			exportConfiguration = true;
-			displayManager.slim.theme = pkgs.fetchurl {
-				url = "https://github.com/vdemeester/slim-themes/raw/master/docker-penguins-theme-0.1.tar.xz";
-				sha256 = "1s0cfj1l9ay7y0ib68dnpdfkr1zwgr0b1s990ch786lxlajwwxpq";
-			};
-			monitorSection = ''
-	Modeline "2560x1080_60.00"  230.00  2560 2720 2992 3424  1080 1083 1093 1120 -hsync +vsync
-			'';
-			deviceSection = ''
-	Option "ModeValidation" "AllowNonEditModes"
-			'';
-			config = ''
-	Section "Monitor"
-		Identifier "eDP1"
-		Option	   "ignore"	"true"
-	EndSection
-	Section "Monitor"
-		Modeline "2560x1080_60.00"  230.00  2560 2720 2992 3424  1080 1083 1093 1120 -hsync +vsync
-		Identifier "HDMI1"
-	EndSection
-			'';
 		};
 	};
 	networking.firewall.allowedTCPPorts = [ 3389 2375 ];
