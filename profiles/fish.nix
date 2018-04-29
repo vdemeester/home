efdefbb90f248df087c1817fe949734226e4339e
Vincent Demeester <vincent@sbr.pm>
2018-04-29 21:56:59 +0200

    Actually share the prompt with envs
    Signed-off-by: Vincent Demeester <vincent@sbr.pm>

--- profiles/fish.nix
+++ profiles/fish.nix
@@ -3,6 +3,10 @@
 {
   programs.fish = {
     enable = true;
+    promptInit = ''
+      source /etc/fish/functions/fish_prompt.fish
+      source /etc/fish/functions/fish_right_prompt.fish
+    '';
   };
   environment.etc."fish/functions/fish_prompt.fish".source = ../envs/fish/fish_prompt.fish;
   environment.etc."fish/functions/fish_right_prompt.fish".source = ../envs/fish/fish_right_prompt.fish;
