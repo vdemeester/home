205b76b8f94f5284dd89b536467329fa37c87665
Vincent Demeester <vincent@sbr.pm>
2018-04-29 15:31:26 +0200

    Customize fish prompt
    Signed-off-by: Vincent Demeester <vincent@sbr.pm>

--- envs/fish.nix
+++ envs/fish.nix
@@ -9,4 +9,6 @@
     shellAliases = import ./aliases.nix;
   };
   xdg.configFile."fish/conf.d/nix-aliases.fish".source = ./fish/nix-aliases.fish;
+  xdg.configFile."fish/functions/fish_prompt.fish".source = ./fish/fish_prompt.fish;
+  xdg.configFile."fish/functions/fish_right_prompt.fish".source = ./fish/fish_right_prompt.fish;
 }
