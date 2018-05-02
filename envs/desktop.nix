1eff62cfd0cccf44c6c9178199f63092c1d2d28f
Vincent Demeester <vincent@sbr.pm>
2018-05-02 17:31:38 +0200

    Add some xorg specific configuration
    Signed-off-by: Vincent Demeester <vincent@sbr.pm>

--- envs/desktop.nix
+++ envs/desktop.nix
@@ -17,6 +17,11 @@
       name = "Vanilla-DMZ";
     };
   };
+  home.file.".XCompose".source = ./xorg/XCompose;
+  home.file.".Xmodmap".source = ./xorg/Xmodmap;
+  xdg.configFile."xorg/emoji.compose".source = ./xorg/emoji.compose;
+  xdg.configFile."xorg/parens.compose".source = ./xorg/parens.compose;
+  xdg.configFile."xorg/modletters.compose".source = ./xorg/modletters.compose;
   services.gpg-agent = {
     enable = true;
     enableSshSupport = true;
