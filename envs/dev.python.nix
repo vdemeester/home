c8021b280946555d8e4c160edef37f764c347ba3
Vincent Demeester <vincent@sbr.pm>
2018-05-31 14:05:07 +0200

    Update dev.python to add virtualenv and tox
    Signed-off-by: Vincent Demeester <vincent@sbr.pm>

--- envs/dev.python.nix
+++ envs/dev.python.nix
@@ -4,6 +4,9 @@
   imports = [ ./dev.nix ];
   home.packages = with pkgs; [
     python3
+    python36Packages.virtualenv
+    python36Packages.pip-tools
+    python36Packages.tox
     pipenv
   ];
 }
