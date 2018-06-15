bbd9229436f6d1839588dcabccef7d245dfe8668
Vincent Demeester <vincent@sbr.pm>
2018-06-15 10:05:41 +0200

    Update docker configuration
    Signed-off-by: Vincent Demeester <vincent@sbr.pm>

--- profiles/dockerization.nix
+++ profiles/dockerization.nix
@@ -2,28 +2,28 @@
 { config, pkgs, ...}:
 
 {
-	imports = [
-		../service/docker.nix
-		# Remove once containerd module is upstream
-		../service/containerd.nix
-	];
-	environment.systemPackages = with pkgs; [
-		python27Packages.docker_compose
-		docker-machine
+  imports = [
+    ../service/docker.nix
+    # Remove once containerd module is upstream
+    ../service/containerd.nix
+  ];
+  environment.systemPackages = with pkgs; [
+    python27Packages.docker_compose
+    docker-machine
     runc
     containerd
-	];
-	virtualisation = {
-		containerd = {
-			enable = true;
-		};
-		docker = {
-			enable = true;
+  ];
+  virtualisation = {
+    containerd = {
+      enable = true;
+    };
+    docker = {
+      enable = true;
       package = pkgs.docker-edge;
-			liveRestore = false;
-			storageDriver = "overlay2";
-			extraOptions = "--label=type=desktop --experimental --init --debug --add-runtime docker-runc=${pkgs.runc}/bin/runc --default-runtime=docker-runc --containerd=/run/containerd/containerd.sock";
-		};
-	};
-	networking.firewall.trustedInterfaces = [ "docker0" ];
+      liveRestore = false;
+      storageDriver = "overlay2";
+      extraOptions = "--label=type=desktop --experimental --init --debug --add-runtime docker-runc=${pkgs.runc}/bin/runc --default-runtime=docker-runc --containerd=/run/containerd/containerd.sock --insecure-registry 172.30.0.0/16";
+    };
+  };
+  networking.firewall.trustedInterfaces = [ "docker0" ];
 }
