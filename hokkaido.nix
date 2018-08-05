50cb2f6117b50af0102b38005e0e2c234a238e72
Vincent Demeester <vincent@sbr.pm>
2018-08-05 15:44:43 +0200

    Make hokkaido a laptop and move redshift there
    Signed-off-by: Vincent Demeester <vincent@sbr.pm>

--- hokkaido.nix
+++ hokkaido.nix
@@ -1,7 +1,8 @@
 { pkgs, ... }:
-with pkgs; {
+
+{
   imports = [
-    ./desktop.nix
+    ./laptop.nix
     ./devops.nix
     ./dev.go.nix
     ./dev.rust.nix
@@ -10,17 +11,6 @@ with pkgs; {
     ./dev.java.nix
     ./dev.haskell.nix
   ];
-  services.redshift = {
-    enable = true;
-    brightness = { day = "1"; night = "0.9"; };
-    latitude = "48.3";
-    longitude = "7.5";
-    tray = true;
-  };
-  xdg.configFile."fish/conf.d/docker.fish".text = ''
-    set -gx TESTKIT_AWS_KEYNAME "vdemeester-wakasu"
-    set -gx DOCKER_BUILDKIT 1
-  '';
   home.packages = with pkgs; [
     vscode
     weechat weechat-xmpp
--- laptop.nix
+++ laptop.nix
@@ -1,9 +1,16 @@
-{...}:
+{ ... }:
 
 {
   imports = [
     ./desktop.nix
   ];
+  services.redshift = {
+    enable = true;
+    brightness = { day = "1"; night = "0.9"; };
+    latitude = "48.3";
+    longitude = "7.5";
+    tray = true;
+  };
   programs.autorandr = {
     enable = true;
   };
