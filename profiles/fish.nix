195921456fea6a7adae6cbd523bd340e3f659ef9
Vincent Demeester <vincent@sbr.pm>
2018-08-05 15:04:34 +0200

    Fix fish profile
    Signed-off-by: Vincent Demeester <vincent@sbr.pm>

--- profiles/fish.nix
+++ profiles/fish.nix
@@ -8,6 +8,6 @@
       source /etc/fish/functions/fish_right_prompt.fish
     '';
   };
-  environment.etc."fish/functions/fish_prompt.fish".source = ../envs/fish/fish_prompt.fish;
-  environment.etc."fish/functions/fish_right_prompt.fish".source = ../envs/fish/fish_right_prompt.fish;
+  environment.etc."fish/functions/fish_prompt.fish".source = ../fish/fish_prompt.fish;
+  environment.etc."fish/functions/fish_right_prompt.fish".source = ../fish/fish_right_prompt.fish;
 }
