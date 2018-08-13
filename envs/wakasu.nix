70e00eae2d8bfa85abfcd01b76182c9dfec4b149
Vincent Demeester <vincent@sbr.pm>
2018-08-13 10:06:35 +0200

    Less is more : wakasu don't do rust or haskell (yet)
    Signed-off-by: Vincent Demeester <vincent@sbr.pm>

--- envs/wakasu.nix
+++ envs/wakasu.nix
@@ -4,11 +4,9 @@
   imports = [
     ./devops.nix
     ./dev.go.nix
-    ./dev.rust.nix
     ./dev.python.nix
     ./dev.js.nix
     ./dev.java.nix
-    ./dev.haskell.nix
   ];
   services.redshift = {
     enable = true;
