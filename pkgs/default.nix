ca2b2054bb10df5428ecd97ee8aa84cf6146d503
Vincent Demeester <vincent@sbr.pm>
2018-08-14 15:44:12 +0200

    Add op v0.5.1
    op is 1password cli tool 👼
    
    Signed-off-by: Vincent Demeester <vincent@sbr.pm>

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -6,10 +6,14 @@ in rec {
   dobi = import ./dobi {
     inherit (pkgs) stdenv lib fetchFromGitHub buildGoPackage;
   };
+  op = import ./op {
+    inherit (pkgs) stdenv lib fetchurl patchelf unzip file;
+  };
   prm = import ./prm {
     inherit (pkgs) stdenv lib fetchgit;
   };
   tmux-tpm = import ./tmux-tpm {
     inherit (pkgs) stdenv lib fetchFromGitHub;
   };
+
 }
