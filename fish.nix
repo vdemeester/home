33b4d4490efd57cac7f55394d998bbd043e3e509
Vincent Demeester <vincent@sbr.pm>
2018-08-16 11:42:55 +0200

    Import sudope behavior 👼
    See https://github.com/oh-my-fish/plugin-sudope
    
    Signed-off-by: Vincent Demeester <vincent@sbr.pm>

--- fish.nix
+++ fish.nix
@@ -10,6 +10,8 @@
     shellAliases = import ./aliases.nix;
   };
   xdg.configFile."fish/conf.d/nix-aliases.fish".source = ./fish/nix-aliases.fish;
+  xdg.configFile."fish/conf.d/sudope.fish".source = ./fish/sudope.fish;
+  xdg.configFile."fish/functions/sudope.fish".source = ./fish/sudope.function.fish;
   xdg.configFile."fish/functions/fish_prompt.fish".source = ./fish/fish_prompt.fish;
   xdg.configFile."fish/functions/fish_right_prompt.fish".source = ./fish/fish_right_prompt.fish;
 }
