3cd6cba37480d325a29f242f32905299f42a6a6c
Vincent Demeester <vincent@sbr.pm>
2018-08-16 11:16:02 +0200

    Add hokkaido ssh key
    Signed-off-by: Vincent Demeester <vincent@sbr.pm>

--- profiles/users.nix
+++ profiles/users.nix
@@ -1,18 +1,18 @@
 { config, pkgs, ... }:
 
 {
-	users = {
-		extraUsers = {
-			vincent = {
-				isNormalUser = true;
-				uid = 1000;
-				createHome = true;
-				extraGroups = [ "networkmanager" "wheel" "docker" "vboxusers" "libvirtd" "input" "audio" "video" "scanner" ];
-				shell = if config.programs.fish.enable then pkgs.fish else pkgs.bash;
-				initialPassword = "changeMe";
-				openssh.authorizedKeys.keys =
-					with import ../ssh-keys.nix; [ honshu wakasu kobe ];
-			};
-		};
-	};
+  users = {
+    extraUsers = {
+      vincent = {
+        isNormalUser = true;
+        uid = 1000;
+        createHome = true;
+        extraGroups = [ "networkmanager" "wheel" "docker" "vboxusers" "libvirtd" "input" "audio" "video" "scanner" ];
+        shell = if config.programs.fish.enable then pkgs.fish else pkgs.bash;
+        initialPassword = "changeMe";
+        openssh.authorizedKeys.keys =
+          with import ../ssh-keys.nix; [ honshu wakasu hokkaido kobe ];
+      };
+    };
+  };
 }
--- ssh-keys.nix
+++ ssh-keys.nix
@@ -1,5 +1,6 @@
 {
-  honshu = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJiGh6j1mSXXXId0iTtdsEewW7EqWVcRgtt8wC3ktFWV vincent@honshu";
-  wakasu = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILRmwzfaRAktaWbVpUG6+1F2wwTW1cDztC0FooQc6QZ/ vincent@wakasu";
   hokkaido = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFXtZRffTnh85lTatmeuYNNLmXsB02RI/ZuiDPdCAKIj vincent@hokkaido";
+  honshu = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJiGh6j1mSXXXId0iTtdsEewW7EqWVcRgtt8wC3ktFWV vincent@honshu";
+  kobe = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOqGw3BHWvCtVr1YPlsUSO2Hw8wJ67jdajnOlROX2H/Y vincent@kobe";
+  wakasu = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILRmwzfaRAktaWbVpUG6+1F2wwTW1cDztC0FooQc6QZ/ vincent@wakasu";
 }
