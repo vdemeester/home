531b4c0f1a8cef5f4a38315889049a808a32f40e
Vincent Demeester <vincent@sbr.pm>
2018-08-22 16:24:47 +0200

    Update wakasu
    Signed-off-by: Vincent Demeester <vincent@sbr.pm>

--- wakasu.nix
+++ wakasu.nix
@@ -2,6 +2,7 @@
 
 {
   imports = [
+    ./laptop.nix
     ./devops.nix
     ./dev.go.nix
     ./dev.rust.nix
@@ -10,17 +11,6 @@
     ./dev.java.nix
     ./dev.haskell.nix
   ];
-  services.redshift = {
-    enable = true;
-    brightness = { day = "1"; night = "0.9"; };
-    latitude = "48.3";
-    longitude = "7.5";
-    tray = true;
-  };
-  services.random-background = {
-    enable = true;
-    imageDirectory = "/home/vincent/desktop/pictures/wallpapers/Unsplashed";
-  };
   xdg.configFile."fish/conf.d/docker.fish".text = ''
     set -gx TESTKIT_AWS_KEYNAME "vdemeester-wakasu"
     set -gx DOCKER_BUILDKIT 1
