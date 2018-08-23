4a65733271535ff80846b75fccded269204fa0de
Vincent Demeester <vincent@sbr.pm>
2018-08-23 18:48:21 +0200

    Update shikoku

--- machine/shikoku.nix
+++ machine/shikoku.nix
@@ -9,10 +9,10 @@
 			../profiles/audio.nix
 			../profiles/dev.nix
 			../profiles/virtualization.nix
-			../profiles/dockerization.nix
+			#../profiles/dockerization.nix
+      ../profiles/containerd.nix
 			../profiles/office.nix
 			../profiles/gaming.nix
-			../profiles/synergy-server.nix
 			../location/home.nix
 			../service/ssh-tunnel.nix
 		];
@@ -36,25 +36,4 @@ xrandr --output DP-2 --auto --left-of DP-4 --output DP-4 --auto &
 	};
 
 	hardware.bluetooth.enable = true;
-
-	environment.etc."synergy-server.conf" = { text = ''
-section: screens
-	shikoku:
-	honshu:
-	wakasu:
-end
-section: links
-	shikoku:
-		left = wakasu
-	honshu:
-		right = wakasu
-        wakasu:
-		right = shikoku
-		left = honshu
-end
-section: options
-	keystroke(super+shift+left) = switchInDirection(left)
-	keystroke(super+shift+right) = switchInDirection(right)
-end
-''; };
 }
