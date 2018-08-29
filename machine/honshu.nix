b1524b9179ee0d2c90bbcd6c08dd8010031e7c10
Vincent Demeester <vincent@sbr.pm>
2018-08-29 11:27:51 +0200

    Mark honshu as dev too…
    … and re-indent the file
    
    Signed-off-by: Vincent Demeester <vincent@sbr.pm>

--- machine/honshu.nix
+++ machine/honshu.nix
@@ -1,40 +1,33 @@
 { config, pkgs, ... }:
 
 {
-	imports =
-		[ # Include the results of the hardware scan.
-		../hardware-configuration.nix
-		../hardware/dell-latitude-e6540.nix
-		../profiles/server.nix
-    ../profiles/gitconfig.nix
-		../profiles/fish.nix
+  imports =
+    [ # Include the results of the hardware scan.
+    ../hardware-configuration.nix
+    ../hardware/dell-latitude-e6540.nix
+    ../profiles/server.nix
+    ../profiles/dev.nix
+    ../profiles/fish.nix
     ../profiles/avahi.nix
-		../profiles/syncthing.nix
-		../service/ssh-tunnel.nix
+    ../profiles/syncthing.nix
+    ../service/ssh-tunnel.nix
     ../location/home.nix
-	];
+  ];
 
-	time.timeZone = "Europe/Paris";
+  time.timeZone = "Europe/Paris";
 
-  virtualisation.docker = {
-		enable = true;
-		package = pkgs.docker-edge;
-		storageDriver = "overlay2";
-		extraOptions = "--experimental --host=tcp://0.0.0.0:2375";
-	};
-	services = {
-
-	  logind.extraConfig = "HandleLidSwitch=ignore";
+  services = {
+    logind.extraConfig = "HandleLidSwitch=ignore";
     ssh-tunnel = {
-			enable = true;
-			localUser = "vincent";
-			remoteHostname = "95.85.58.158";
-			remotePort = 22;
-			remoteUser = "vincent";
-			bindPort = 2224;
-		};
-	};
-  
-	networking.enableIPv6 = false;
-	networking.firewall.allowedTCPPorts = [ 3389 2375 ];
+      enable = true;
+      localUser = "vincent";
+      remoteHostname = "95.85.58.158";
+      remotePort = 22;
+      remoteUser = "vincent";
+      bindPort = 2224;
+    };
+  };
+
+  networking.enableIPv6 = false;
+  networking.firewall.allowedTCPPorts = [ 3389 2375 ];
 }
