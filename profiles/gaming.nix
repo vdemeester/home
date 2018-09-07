2c65885d3cce351fd4d7e77e8150553215c48529
Vincent Demeester <vincent@sbr.pm>
2018-09-07 17:15:47 +0200

    Steam and discord should be installed by user :)
    Signed-off-by: Vincent Demeester <vincent@sbr.pm>

--- profiles/gaming.nix
+++ profiles/gaming.nix
@@ -1,12 +1,6 @@
 { config, pkgs, ... }:
 
 {
-  environment = {
-    systemPackages = with pkgs; [
-      steam
-      discord
-    ];
-  };
   hardware = {
     opengl = {
       driSupport32Bit = true;
