ca469bf8bc5f0839d2ea65b5b9654e301a6ae3f7
Vincent Demeester <vincent@sbr.pm>
2018-09-09 22:11:49 +0200

    Try to not fail in case of rsync error…
    Signed-off-by: Vincent Demeester <vincent@sbr.pm>

--- machine/honshu.nix
+++ machine/honshu.nix
@@ -33,6 +33,7 @@
 /home/vincent/desktop/pictures/screenshots/ vincent@synodine.local:/volumeUSB2/usbshare/pictures/screenshots/
 /home/vincent/desktop/pictures/wallpapers/ vincent@synodine.local:/volumeUSB2/usbshare/pictures/wallpapers/
 /home/vincent/desktop/pictures/photos/ vincent@synodine.local:/volumeUSB2/usbshare/pictures/photos/
+/home/vincent/desktop/documents/ vincent@synodine.local:/volume1/documents/
 /run/media/vincent/FcCuir/music/ vincent@synodine.local:/volumeUSB2/usbshare/music/
   '';
 
