98ed07083ff469108b7f7f45448e13b8b92c57d3
Vincent Demeester <vincent@sbr.pm>
2018-09-09 22:09:54 +0200

    Update honshu's vrsync…
    Signed-off-by: Vincent Demeester <vincent@sbr.pm>

--- machine/honshu.nix
+++ machine/honshu.nix
@@ -33,7 +33,6 @@
 /home/vincent/desktop/pictures/screenshots/ vincent@synodine.local:/volumeUSB2/usbshare/pictures/screenshots/
 /home/vincent/desktop/pictures/wallpapers/ vincent@synodine.local:/volumeUSB2/usbshare/pictures/wallpapers/
 /home/vincent/desktop/pictures/photos/ vincent@synodine.local:/volumeUSB2/usbshare/pictures/photos/
-/home/vincent/desktop/documents/ vincent@synodine.local:/volume1/documents/
 /run/media/vincent/FcCuir/music/ vincent@synodine.local:/volumeUSB2/usbshare/music/
   '';
 
@@ -42,7 +41,7 @@
     wantedBy = [ "multi-user.target" ];
     serviceConfig = {
       Type = "oneshot";
-      ExecStart = "${pkgs.vrsync}/bin/vrsync --dry-run";
+      ExecStart = "${pkgs.vrsync}/bin/vrsync";
       Environment = "PATH=/run/current-system/sw/bin";
     };
   };
