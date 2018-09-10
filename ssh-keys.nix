947ac86c9cab7f2cab2f403cbdfc80e293896ac1
Vincent Demeester <vincent@sbr.pm>
2018-09-10 08:56:06 +0200

    Add shikoku to ssh keys
    Signed-off-by: Vincent Demeester <vincent@sbr.pm>

--- ssh-keys.nix
+++ ssh-keys.nix
@@ -3,4 +3,5 @@
   honshu = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJiGh6j1mSXXXId0iTtdsEewW7EqWVcRgtt8wC3ktFWV vincent@honshu";
   kobe = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOqGw3BHWvCtVr1YPlsUSO2Hw8wJ67jdajnOlROX2H/Y vincent@kobe";
   wakasu = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILRmwzfaRAktaWbVpUG6+1F2wwTW1cDztC0FooQc6QZ/ vincent@wakasu";
+  shikoku = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGR4dqXwHwPpYgyk6yl9+9LRL3qrBZp3ZWdyKaTiXp0p vincent@shikoku";
 }
