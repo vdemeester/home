4b663504db1952f9e79c7ea9ca80427265d71489
Vincent Demeester <vincent@sbr.pm>
2018-09-14 12:11:42 +0200

    Add grv to git fish abbrs
    Signed-off-by: Vincent Demeester <vincent@sbr.pm>

--- fish/git.fish
+++ fish/git.fish
@@ -1,5 +1,5 @@
 function _def_git_nix_run_aliases
-    set -l packages git-annex:gitAndTools.git-annex
+    set -l packages git-annex:gitAndTools.git-annex grv:gitAndTools.grv
     for s in $packages
 	_nix_run_package $s nixpkgs
     end
