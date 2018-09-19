8063c340c64bba945fd2eb4954ac87429ce6c05e
Vincent Demeester <vincent@sbr.pm>
2018-09-19 17:33:28 +0200

    hokkaido: remove devops, java and haskell profiles
    Signed-off-by: Vincent Demeester <vincent@sbr.pm>

--- hokkaido.nix
+++ hokkaido.nix
@@ -3,13 +3,10 @@
 {
   imports = [
     ./laptop.nix
-    ./devops.nix
     ./dev.go.nix
     ./dev.rust.nix
     ./dev.python.nix
     ./dev.js.nix
-    ./dev.java.nix
-    ./dev.haskell.nix
   ];
   home.packages = with pkgs; [
     vscode
