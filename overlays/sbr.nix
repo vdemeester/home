1ae97649a4a9dc9eb0089374629499b2c38a1503
Vincent Demeester <vincent@sbr.pm>
2018-09-29 17:13:06 +0200

    vscode: add more extensions
    Signed-off-by: Vincent Demeester <vincent@sbr.pm>

--- overlays/sbr.nix
+++ overlays/sbr.nix
@@ -33,6 +33,9 @@ self: super:
   tmux-tpm = import ../pkgs/tmux-tpm {
     inherit (self) stdenv lib fetchFromGitHub;
   };
+  vscodeliveshare = import ../pkgs/vscodeliveshare {
+    inherit (self) stdenv vscode-utils autoPatchelfHook xorg gnome3 utillinux openssl icu zlib curl lttng-ust libsecret libkrb5 gcc libunwind binutils;
+  };
   vscode-with-extensions = super.vscode-with-extensions.override {
       # code --list-extensions --show-versions
       # ls ~/.vscode/extensions
@@ -48,20 +51,14 @@ self: super:
           {
             name = "vsc-material-theme";
             publisher = "Equinusocio";
-            version = "2.1.0";
-            sha256 = "1rhygrig9z1kxy0ldw28zm6xldjji53s1apxl2n9yi9vl0wn8np1";
+            version = "2.4.2";
+            sha256 = "1cl9xy9pg0jvgpz4ifvb9n6lv1xcsnrsh56xmz2hr6901n4gk70p";
           }
           {
             name = "material-icon-theme";
             publisher = "PKief";
-            version = "3.5.0";
-            sha256 = "0djr8dxhpf81y3x2haj7jriqsmi87may43myph3rgqmzw2s986sg";
-          }
-          {
-            name = "Nix";
-            publisher = "bbenoist";
-            version = "1.0.1";
-            sha256 = "0zd0n9f5z1f0ckzfjr38xw2zzmcxg1gjrava7yahg5cvdcw6l35b";
+            version = "3.6.0";
+            sha256 = "0jphqqs41pkyv11mq1a3wzx14sl6pifcfyz3lw5wany003fv5s9s";
           }
           {
             name = "fish-vscode";
@@ -69,9 +66,113 @@ self: super:
             version = "0.2.0";
             sha256 = "148r186y3h7n84fcyh6wa2qwl2q3pfi8aykwkc9dhfj3kwfcm5rb";
           }
+          {
+            name = "code-runner";
+            publisher = "formulahendry";
+            version = "0.9.4";
+            sha256 = "08qq21gaa7igklv9si35qxhs79na893vyp96hf7rvyv7c4fn1pvw";
+          }
+          {
+            name = "gitlens";
+            publisher = "eamodio";
+            version = "8.5.6";
+            sha256 = "1vn6fvxn4g3759pg9rp8hqdc58pgyvcdma1ylfwmdnipga37xfd3";
+          }
+          {
+            name = "vscode-direnv";
+            publisher = "Rubymaniac";
+            version = "0.0.2";
+            sha256 = "1gml41bc77qlydnvk1rkaiv95rwprzqgj895kxllqy4ps8ly6nsd";
+          }
+          {
+            name = "vscode-proto3";
+            publisher = "zxh404";
+            version = "0.2.1";
+            sha256 = "12yf66a9ws5hlyj38nmn91y8a1jrq8696fnmgk60w9anyfalbn4q";
+          }
+          {
+            name = "project-manager";
+            publisher = "alefragnani";
+            version = "9.0.1";
+            sha256 = "0aqyavgpaqvv62q15h4dkxcgj4khsgr1rlzr3wi9aflyncg7addb";
+          }
+          {
+            name = "tslint";
+            publisher = "eg2";
+            version = "1.0.39";
+            sha256 = "1al61xzz7p6rqgk7rplg3njj4hyiipx7w89pqfn8634skw7r32rl";
+          }
+          {
+            name = "vscode-npm-script";
+            publisher = "eg2";
+            version = "0.3.5";
+            sha256 = "1v4081siab0fm0zfn6vlvqlc4vx131q8y6f3h3l46mvpndsa2rck";
+          }
+          {
+            name = "vscode-pull-request-github";
+            publisher = "GitHub";
+            version = "0.1.6";
+            sha256 = "08r0i265q4gk6kmz3ynxglhssdk5020bifagl9jr8spfs5sacnsx";
+          }
+          {
+            name = "vscode-kubernetes-tools";
+            publisher = "ms-kubernetes-tools";
+            version = "0.1.14";
+            sha256 = "0ixs1cydbz6qizf9cs0jdqpxwfg7gs74jdy9hp2v9h8q7vq6503l";
+          }
+          # languages
+          {
+            name = "Go";
+            publisher = "ms-vscode";
+            version = "0.6.89";
+            sha256 = "05mzw4bwsa9wxldnkdgk0b4n4xm8gzhmrbqy6j8lbk3p360wdg8z";
+          }
+          {
+            name = "rust";
+            publisher = "rust-lang";
+            version = "0.4.10";
+            sha256 = "1y7sb3585knv2pbq7vf2cjf3xy1fgzrqzn2h3fx2d2bj6ns6vpy3";
+          }
+          {
+            name = "Kotlin";
+            publisher = "mathiasfrohlich";
+            version = "1.6.0";
+            sha256 = "04lqzn4pzwx6m936b9jv4nh3q3rs9p9jla8mpln0751jk1844y47";
+          }
+          {
+            name = "java";
+            publisher = "redhat";
+            version = "0.31.0";
+            sha256 = "1hzqiqkja4931k7rb4pmva6k80ss53nvyksiqvq6kxj25rg3kd1b";
+          }
+          {
+            name = "vscode-java-debug";
+            publisher = "vscjava";
+            version = "0.13.0";
+            sha256 = "11xvd1b0qsvrbm4yb0c7fm537p1gs6wz45xzl6v1a7k08dapi20i";
+          }
+          {
+            name = "vscode-java-test";
+            publisher = "vscjava";
+            version = "0.9.0";
+            sha256 = "0000v5qh100n3n529r1nzx79cawx83kvprrmhw6pb4j0m5b3p3p2";
+          }
+          {
+            name = "gradle-language";
+            publisher = "naco-siren";
+            version = "0.2.3";
+            sha256 = "15lzxvym0mkljjn57av1p4z6hqqwbsbn5idw2fn7nccgrl93aywf";
+          }
+          {
+            name = "vscode-yaml";
+            publisher = "redhat";
+            version = "0.0.16";
+            sha256 = "0v21qj65nrp850r0jmivmsm9y5i99ymxxm8ganzdia4vbv5hzx9r";
+          }
+        ] ++ [
+          super.vscode-extensions.bbenoist.Nix
+          super.vscode-extensions.ms-python.python
+          # self.vscodeliveshare
         ];
-        # ] ++ with super.vscode-extensions; [
-        #   bbenoist.Nix
-        # ];
     };
 }
