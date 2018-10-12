b02a59f91dede3bde1b6324e2f1e0aefb25119ea
Vincent Demeester <vincent@sbr.pm>
2018-10-12 18:32:40 +0200

    ssh: add system ssh_config
    Signed-off-by: Vincent Demeester <vincent@sbr.pm>

--- profiles/ssh.nix
+++ profiles/ssh.nix
@@ -5,6 +5,11 @@
     openssh = {
       enable = true;
       startWhenNeeded = false;
+      extraConfig = with import ../assets/machines.nix; ''
+Host kerkouane kerkouane.sbr.pm
+  Hostname kerkouane.sbr.pm
+  Port ${toString ssh.kerkouane.port}
+      '';
     };
   };
   programs.mosh.enable = true;
