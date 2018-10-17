972e5c758d564c4030c95962aad786b3bc2e684f
Vincent Demeester <vincent@sbr.pm>
2018-10-17 15:02:54 +0200

    massimo: disable lib sleep and fix file
    Signed-off-by: Vincent Demeester <vincent@sbr.pm>

--- machine/massimo.nix
+++ machine/massimo.nix
@@ -8,7 +8,6 @@
 
   boot.cleanTmpDir = true;
   networking.firewall.allowPing = true;
-  services.openssh.enable = true;
   programs.fish.enable = true;
   users.users.root.openssh.authorizedKeys.keys = [
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGR4dqXwHwPpYgyk6yl9+9LRL3qrBZp3ZWdyKaTiXp0p vincent@shikoku"
@@ -17,13 +16,13 @@
 
   services = {
     logind.extraConfig = "HandleLidSwitch=ignore";
-  };
-  
-  services.wireguard = with import ../assets/machines.nix; {
-    enable = true;
-    ips = [ "${wireguard.ips.massimo}/24" ];
-    endpoint = wg.endpointIP;
-    endpointPort = wg.listenPort;
-    endpointPublicKey = wireguard.kerkouane.publicKey;
+    openssh.enable = true;
+    wireguard = with import ../assets/machines.nix; {
+      enable = true;
+      ips = [ "${wireguard.ips.massimo}/24" ];
+      endpoint = wg.endpointIP;
+      endpointPort = wg.listenPort;
+      endpointPublicKey = wireguard.kerkouane.publicKey;
+    };
   };
 }
