4439a650a4abc68ebb726b3557593880b972feae
Vincent Demeester <vincent@sbr.pm>
2018-10-18 11:08:59 +0200

    openshift: no alias, direct access to the package (oc/openshift)
    Signed-off-by: Vincent Demeester <vincent@sbr.pm>

--- fish/openshift.fish
+++
@@ -1,9 +0,0 @@
-function _def_openshift_nix_run_aliases
-    set -l packages openshift oc:openshift
-    for s in $packages
-	_nix_run_package $s nixpkgs
-	# _nix_run_package $s unstable ~/.config/nixpkgs/channels.nix
-    end
-end
-
-_def_openshift_nix_run_aliases
--- openshift.nix
+++ openshift.nix
@@ -1,10 +1,10 @@
 { pkgs, ... }:
 
 {
-  xdg.configFile."fish/conf.d/openshift.fish".source = ./fish/openshift.fish;
   home.packages = with pkgs; [
     docker-machine-kvm
     docker-machine-kvm2
     s2i
+    openshift
   ];
 }
