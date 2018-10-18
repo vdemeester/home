a890bdbda3b68d6e21680bd441b9db3ded6fda45
Vincent Demeester <vincent@sbr.pm>
2018-10-18 15:01:29 +0200

    machine: update carthage (new droplet)
    Signed-off-by: Vincent Demeester <vincent@sbr.pm>

--- machine/carthage.nix
+++ machine/carthage.nix
@@ -1,42 +1,28 @@
 { config, pkgs, ... }: {
   imports = [
     ../hardware-configuration.nix
-    ../cloud/digitalocean/networking.nix # generated at runtime by nixos-infect
+    ../networking.nix # generated at runtime by nixos-infect
     ../profiles/server.nix
     ../profiles/gitconfig.nix
+    ../profiles/users.nix
   ];
-
-  environment = {
-    systemPackages = with pkgs; [
-      haskellPackages.git-annex
-    ];
-  };
 
   boot.cleanTmpDir = true;
+  boot.loader.grub.enable = true;
   networking.firewall.allowPing = true;
   services.openssh.enable = true;
+  # services.openssh.ports = with import ../assets/machines.nix; [ ssh.carthage.port ];
+  programs.fish.enable = true;
   users.users.root.openssh.authorizedKeys.keys = [
-    "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQCqaMAW+tsun4gRn/XZZ3evoEDlYOPKx+7h1O/PBzwbiJzrR+5XpP32p1n1krsbF1jHyxTK+3GO9BXfFq2ag/RoO6u6jbXJwzJX8+ZYlcRfhkPu/ixDkF0ADMTrtzw+OYiYU9uYnqY5bj2En2uOfZCIOtsvE4yHCgeXTR9Xo+owB5ci3d5lfF1URTg3dJNzQykBnM06Fu8fYth/5DBInEau4h5N8XrkWUU0K/zYdWl0ws6dAHZYo+JZoqmF+o/ptb00e9cegZHEtLfa/IXC/GwD4gcLrnIpETr+HQBHHJH5PXPuEnO73rDaRBVWHWAErkj1/3OoH/m71pyvc4rLPZs0N4B9vdCpblGj/IWivLLnQaZQFTNRPWe45WaqVpIR1VS5UpHElr0cjqroaqXI0RcBVQ8v6fMytITP1B9u2s0G07ScLMJLlWwA5GhySWULskzcYNqCLmbSnmEzsfFRzawDCHn0BWiwRdnMVo1HwbUwvAw7z2my5b83fauf45jrwuAElCSUBvibEeHHHmY84FeiJhAUIpeu9yXNpGnc1kSOibJQjZwlRH54bzMuiq5UvaaV+kH0gfNEQOvxHJBdf4r9gxb+JG4/VnurdgTLulBiGXDuDBvhOvCb3bPRpswZbm67EPgG5HMJtjeEdaLG/yV0mhZ7Jl6rk8dvh6IrXKkKew== vincent@honshu.local"
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGR4dqXwHwPpYgyk6yl9+9LRL3qrBZp3ZWdyKaTiXp0p vincent@shikoku"
   ];
   time.timeZone = "Europe/Paris";
-
-  # ape – sync git mirrors
-  systemd.user.services.ape = {
-    description = "Ape - sync git mirrors";
-    wantedBy = [ "multi-user.target" ];
-    serviceConfig = {
-      Type = "oneshot";
-      ExecStart = "${pkgs.ape}/bin/ape up /home/vincent/mirrors/";
-      Environment = "PATH=/run/current-system/sw/bin/";
-    };
+  
+  services.wireguard = with import ../assets/machines.nix; {
+    enable = true;
+    ips = [ "${wireguard.ips.carthage}/24" ];
+    endpoint = wg.endpointIP;
+    endpointPort = wg.listenPort;
+    endpointPublicKey = wireguard.kerkouane.publicKey;
   };
-  systemd.user.timers.ape = {
-    description = "Ape hourly";
-    wantedBy = [ "timers.target" ];
-    timerConfig = {
-      OnCalendar = "hourly";
-      Persistent = "true";
-    };
-  };
-  systemd.user.timers.ape.enable = true;
 }
