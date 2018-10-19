f4ddce70926f7d373ba9b92b3192160596d7cd73
Vincent Demeester <vincent@sbr.pm>
2018-10-19 12:02:59 +0200

    users: update ssh config and keys
    Signed-off-by: Vincent Demeester <vincent@sbr.pm>

--- profiles/users.nix
+++ profiles/users.nix
@@ -11,7 +11,7 @@
         shell = if config.programs.fish.enable then pkgs.fish else pkgs.bash;
         initialPassword = "changeMe";
         openssh.authorizedKeys.keys =
-          with import ../assets/ssh-keys.nix; [ honshu wakasu hokkaido california shikoku massimo carthage ];
+          with import ../assets/ssh-keys.nix; [ honshu kerkouane hokkaido california shikoku massimo carthage ];
           subUidRanges = [{ startUid = 100000; count = 65536; }];
           subGidRanges = [{ startGid = 100000; count = 65536; }];
       };
@@ -22,11 +22,21 @@ Host kerkouane kerkouane.sbr.pm
   Hostname kerkouane.sbr.pm
   Port ${toString ssh.kerkouane.port}
 Host kerkouane.vpn ${wireguard.ips.kerkouane}
+  Hostname ${wireguard.ips.kerkouane}
   Port ${toString ssh.kerkouane.port}
 Host carthage carthage.sbr.pm
   Hostname carthage.sbr.pm
   Port ${toString ssh.carthage.port}
-Host ${wireguard.ips.carthage}
+Host carthage.vpn ${wireguard.ips.carthage}
+  Hostname ${wireguard.ips.carthage}
   Port ${toString ssh.carthage.port}
+Host honshu.vpn ${wireguard.ips.honshu}
+  Hostname ${wireguard.ips.honshu}
+Host shikoku.vpn ${wireguard.ips.shikoku}
+  Hostname ${wireguard.ips.shikoku}
+Host hokkaido.vpn ${wireguard.ips.hokkaido}
+  Hostname ${wireguard.ips.hokkaido}
+Host massimo.vpn ${wireguard.ips.massimo}
+  Hostname ${wireguard.ips.massimo}
     '';
 }
