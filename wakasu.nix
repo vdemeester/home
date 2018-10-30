b5ef935013835e16fb9d1e1b7fb0df02758501e7
Vincent Demeester <vincent@sbr.pm>
2018-10-30 17:49:06 +0100

    wakasu: add some stuff back
    Signed-off-by: Vincent Demeester <vincent@sbr.pm>

--- wakasu.nix
+++ wakasu.nix
@@ -10,16 +10,20 @@
     ./dev.js.nix
     ./dev.java.nix
     ./dev.haskell.nix
+    ./openshift.nix
+    ./ssh.nix
+    ./vscode.nix
   ];
   xdg.configFile."fish/conf.d/docker.fish".text = ''
-    set -gx TESTKIT_AWS_KEYNAME "vdemeester-wakasu"
     set -gx DOCKER_BUILDKIT 1
   '';
   home.packages = with pkgs; [
-    vscode
-    jetbrains.idea-ultimate
-    zoom-us
-    debootstrap
-    weechat weechat-xmpp
+    google-chrome
+    obs-studio
+    mattermost-desktop
+    slack
+    virtmanager
+    #jetbrains.idea-ultimate
   ];
+  services.shairport-sync.enable = true;
 }
