d69fee13504f89bbf29ae96bd008f545aedf3e5c
Vincent Demeester <vincent@sbr.pm>
2018-11-01 18:43:17 +0100

    machines: create machines
    Signed-off-by: Vincent Demeester <vincent@sbr.pm>

--- machines/base.nix
+++ machines/base.nix
@@ -3,14 +3,14 @@
 let home_directory = builtins.getEnv "HOME"; in
 
 rec {
-  imports = [ modules/module-list.nix ];
+  imports = [ ../modules/module-list.nix ];
   programs = {
     home-manager = {
       enable = true;
       path = https://github.com/rycee/home-manager/archive/master.tar.gz;
     };
   };
-  home.file.".nix-channels".source = ./nix-channels;
+  home.file.".nix-channels".source = ../assets/nix-channels;
   home.packages = with pkgs; [
     aspell
     aspellDicts.en
