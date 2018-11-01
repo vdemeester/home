90396b7de4f4e1fa0dab293ea8201d3dc50c2233
Vincent Demeester <vincent@sbr.pm>
2018-11-01 21:27:42 +0100

    Fix ape.conf path
    Signed-off-by: Vincent Demeester <vincent@sbr.pm>

--- machines/honshu.nix
+++ machines/honshu.nix
@@ -6,5 +6,5 @@
   ];
   profiles.dev.go.enable = true;
   profiles.media.enable = true;
-  xdg.configFile."ape.conf".source = ./assets/ape.conf;
+  xdg.configFile."ape.conf".source = ../assets/ape.conf;
 }
