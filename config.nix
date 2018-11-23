9adbaa6061d95a9f2388efd04b3db1ff9877dbac
Vincent Demeester <vincent@sbr.pm>
2018-11-23 09:20:00 +0100

    config: fix indentation
    Signed-off-by: Vincent Demeester <vincent@sbr.pm>

--- config.nix
+++ config.nix
@@ -1,5 +1,5 @@
 {
-	allowUnfree = true;
+  allowUnfree = true;
   packageOverrides = pkgs: {
     nur = import (builtins.fetchTarball "https://github.com/nix-community/NUR/archive/master.tar.gz") {
       inherit pkgs;
