ba4fb5d2de4a477162b57cfae9b09f3b115419f5
Vincent Demeester <vincent@sbr.pm>
2018-11-23 19:05:01 +0100

    hokkaido: cleanup/fix configuration
    Signed-off-by: Vincent Demeester <vincent@sbr.pm>

--- machine/hokkaido.nix
+++ machine/hokkaido.nix
@@ -2,50 +2,43 @@
 
 {
   imports = [ ../hardware/thinkpad-x220.nix ];
-
-  profiles.laptop.enable = true;
-  profiles.ssh.enable = true;
-  profiles.dev.enable = true;
-  profiles.yubikey.enable = true;
-
   time.timeZone = "Europe/Paris";
-
+  profiles = {
+    dev.enable = true;
+    laptop.enable = true;
+    ssh.enable = true;
+    yubikey.enable = true;
+  };
+  programs.podman = {
+    enable = true;
+  };
+  services = {
+    autofs = {
+      enable = true;
+      debug = false;
+      autoMaster = let
+        mapConfSsh = pkgs.writeText "auto.sshfs"  ''
+        shikoku.local -fstype=fuse,allow_other :sshfs\#shikoku.local\:
+        '';
+        mapConf = pkgs.writeText "auto"  ''
+        synodine -fstype=nfs,rw 192.168.12.19:/
+        '';
+      in ''
+        /auto file:${mapConf}
+        /auto/sshfs file:${mapConfSsh} uid=1000,gid=100,--timeout=30,--ghost
+      '';
+    };
+    syncthing-edge.guiAddress = with import ../assets/machines.nix; "${wireguard.ips.hokkaido}:8384";
+    wireguard = with import ../assets/machines.nix; {
+      enable = true;
+      ips = [ "${wireguard.ips.hokkaido}/24" ];
+      endpoint = wg.endpointIP;
+      endpointPort = wg.listenPort;
+      endpointPublicKey = wireguard.kerkouane.publicKey;
+    };
+  };
   environment.systemPackages = with pkgs; [
     nfs-utils
     sshfs
   ];
-  
-  programs.podman = {
-    enable = true;
-  };
-
-  services.autofs = {
-    enable = true;
-    debug = false;
-    autoMaster = let
-      mapConfSsh = pkgs.writeText "auto.sshfs"  ''
-      shikoku.local -fstype=fuse,allow_other :sshfs\#shikoku.local\:
-      '';
-      mapConf = pkgs.writeText "auto"  ''
-      synodine -fstype=nfs,rw 192.168.12.19:/
-      '';
-    in ''
-      /auto file:${mapConf}
-      /auto/sshfs file:${mapConfSsh} uid=1000,gid=100,--timeout=30,--ghost
-    '';
-  };
-
-  services.xserver.displayManager.slim.theme = pkgs.fetchurl {
-    url = "https://github.com/vdemeester/slim-themes/raw/master/docker-key-theme-0.1.tar.xz";
-    sha256 = "127893l1nzqya0g68k8841g5lm3hlnx7b3b3h06axvplc54a1jd8";
-  };
-
-  services.syncthing-edge.guiAddress = with import ../assets/machines.nix; "${wireguard.ips.hokkaido}:8384";
-  services.wireguard = with import ../assets/wireguard.nix; {
-    enable = true;
-    ips = [ "${ips.hokkaido}/24" ];
-    endpoint = main.endpointIP;
-    endpointPort = main.listenPort;
-    endpointPublicKey = kerkouane.publicKey;
-  };
 }
