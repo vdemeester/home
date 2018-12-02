7be6762e7d8e8b16e38844a947c260772ae59014
Vincent Demeester <vincent@sbr.pm>
2018-12-02 17:17:54 +0100

    progfiles.docker: add docker and runc package customization
    Signed-off-by: Vincent Demeester <vincent@sbr.pm>

--- modules/profiles/docker.nix
+++ modules/profiles/docker.nix
@@ -12,16 +12,26 @@ in
         description = "Enable docker profile";
         type = types.bool;
       };
+      package = mkOption {
+        default = pkgs.docker-edge;
+        description = "docker package to be used";
+        type = types.package;
+      };
+      runcPackage = mkOption {
+        default = pkgs.nur.repos.vdemeester.runc;
+        description = "runc package to be used";
+        type = types.package;
+      };
     };
   };
   config = mkIf cfg.enable {
     virtualisation = {
       docker = {
         enable = true;
-        package = pkgs.docker-edge;
+        package = cfg.package;
         liveRestore = false;
         storageDriver = "overlay2";
-        extraOptions = "--label=type=desktop --experimental --init --debug --add-runtime docker-runc=${pkgs.runc-edge}/bin/runc --default-runtime=docker-runc --containerd=/run/containerd/containerd.sock --insecure-registry 172.30.0.0/16";
+        extraOptions = "--label=type=desktop --experimental --init --debug --add-runtime docker-runc=${cfg.runcPackage}/bin/runc --default-runtime=docker-runc --containerd=/run/containerd/containerd.sock --insecure-registry 172.30.0.0/16";
       };
     };
     networking.firewall.trustedInterfaces = [ "docker0" ];
