7870e7c82a395a494f86f11852a945cb8330a06e
Vincent Demeester <vincent@sbr.pm>
2018-12-09 16:56:53 +0100

    hokkaido: it's now a server…
    Signed-off-by: Vincent Demeester <vincent@sbr.pm>

--- machines/hokkaido.nix
+++ machines/hokkaido.nix
@@ -4,18 +4,10 @@
   imports = [
     ./base.nix
   ];
-  profiles.laptop.enable = true;
+  profiles.emacs = { daemonService = false; };
   profiles.dev = {
     go.enable = true;
-    js.enable = true;
-    java.enable = true;
-    python.enable = true;
-    rust.enable = true;
-    vscode.enable = true;
   };
   profiles.containers.enable = true;
   profiles.media.enable = true;
-  home.packages = with pkgs; [
-    google-chrome
-  ];
 }
