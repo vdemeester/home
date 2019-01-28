1329c1457167ae4af5a1ee25f7bda6093952cec6
Vincent Demeester <vincent@sbr.pm>
2019-01-28 16:04:49 +0100

    fish: add go2nix and dep2nix as nix run aliases
    Signed-off-by: Vincent Demeester <vincent@sbr.pm>

--- modules/profiles/fish.nix
+++ modules/profiles/fish.nix
@@ -37,7 +37,7 @@ in
     xdg.configFile."fish/functions/fish_right_prompt.fish".source = ./assets/fish/fish_right_prompt.fish;
     xdg.configFile."nr/default" = {
       text = builtins.toJSON [
-        {cmd = "ncdu";} {cmd = "sshfs";} {cmd = "gotop";} {cmd = "pandoc";}
+        {cmd = "ncdu";} {cmd = "sshfs";} {cmd = "gotop";} {cmd = "pandoc";} {cmd = "dep2nix";} {cmd = "go2nix";}
         {cmd = "lspci"; pkg = "pciutils";}
         {cmd = "lsusb"; pkg = "usbutils";}
         {cmd = "wakeonlan"; pkg = "python36Packages.wakeonlan";}
