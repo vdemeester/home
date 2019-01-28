5fbf40571ffd681bf72fdddc09f445a2e6211f0f
Vincent Demeester <vincent@sbr.pm>
2019-01-28 16:41:47 +0100

    Update .dir-locals.el with projectile variables
    Signed-off-by: Vincent Demeester <vincent@sbr.pm>

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,5 +1,7 @@
 ;;; Directory Local Variables
 ;;; For more information see (info "(emacs) Directory Variables")
 
-((nix-mode
+((nil (eval . (setq projectile-project-compilation-cmd "home-manager build"
+		    projectile-project-run-cmd "home-manager switch")))
+ (nix-mode
   (tab-width . 2)))
