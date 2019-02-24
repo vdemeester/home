9a4c50f0d6ec09086232e97d7a9b25fc8d560321
Vincent Demeester <vincent@sbr.pm>
2019-02-24 17:53:07 +0100

    mails: remove comments
    Signed-off-by: Vincent Demeester <vincent@sbr.pm>

--- modules/profiles/mails.nix
+++ modules/profiles/mails.nix
@@ -119,13 +119,10 @@ in
 
         [MailMover]
         folders = perso/Inbox redhat/Inbox 
-        # "perso/[Gmail]/All Mail" "redhat/[Gmail]/All Mail"
         rename = true
 
         perso/Inbox = 'NOT tag:Inbox':"perso/[Gmail]/All Mail"
         redhat/Inbox = 'NOT tag:Inbox':"redhat/[Gmail]/All Mail"
-        #"perso/[Gmail]/All Mail" = 'tag:Inbox':perso/Inbox
-        #"redhat/[Gmail]/All Mail" = 'tag:Inbox':redhat/Inbox
       '';
     };
     programs.notmuch.enable = true;
