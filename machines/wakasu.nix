84f8af781ddcd70b5077b2a7074cf7baf247e5bf
Vincent Demeester <vincent@sbr.pm>
2019-02-28 09:41:57 +0100

    wakasu: remove mattermost-desktop
    Signed-off-by: Vincent Demeester <vincent@sbr.pm>

--- machines/wakasu.nix
+++ machines/wakasu.nix
@@ -1,4 +1,4 @@
-{ pkgs, ...}:
+{ pkgs, ... }:
 
 {
   imports = [
@@ -29,7 +29,6 @@
   };
   home.packages = with pkgs; [
     obs-studio
-    mattermost-desktop
     slack
     virtmanager
     awscli
