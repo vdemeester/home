27a21fa0fba7b123dba72f5874ae3ee761d3c68c
Vincent Demeester <vincent@sbr.pm>
2019-03-15 14:50:48 +0100

    shikoku: clean and reorder file
    Signed-off-by: Vincent Demeester <vincent@sbr.pm>

--- machines/shikoku.nix
+++ machines/shikoku.nix
@@ -4,19 +4,6 @@
   imports = [
     ./base.nix
   ];
-  profiles.desktop.enable = true;
-  profiles.gaming.enable = true;
-  profiles.dev = {
-    go.enable = true;
-    haskell.enable = true;
-    java = { enable = true; idea = true; };
-    js.enable = true;
-    python.enable = true;
-    rust.enable = true;
-    vscode.enable = true;
-  };
-  profiles.emacs.withXwidgets = true;
-  profiles.media.enable = true;
   profiles.cloud.google.enable = true;
   profiles.containers = {
     enable = true;
@@ -25,15 +12,15 @@
     kubernetes = { enable = true; minikube.enable = true; };
     openshift = { enable = true; minishift.enable = true; };
   };
-  programs = {
-    google-chrome.enable = true;
+  profiles.desktop.enable = true;
+  profiles.dev = {
+    go.enable = true;
+    haskell.enable = true;
+    python.enable = true;
+    rust.enable = true;
+    vscode.enable = true;
   };
+  profiles.gaming.enable = true;
+  profiles.media.enable = true;
   services.shairport-sync.enable = true;
-  
-  home.file."src/github.com/knative/.envrc".source = ../projects/nix.envrc;
-  home.file."src/github.com/knative/default.nix".source = ../projects/knative/default.nix;
-
-  home.packages = with pkgs; [
-    obs-studio
-  ];
 }
