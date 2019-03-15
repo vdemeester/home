df79c8d55df5409075853569ad75e4df9105fdf6
Vincent Demeester <vincent@sbr.pm>
2019-03-15 14:53:03 +0100

    hokkaido: re-order (and small clean)
    Signed-off-by: Vincent Demeester <vincent@sbr.pm>

--- machines/hokkaido.nix
+++ machines/hokkaido.nix
@@ -4,13 +4,12 @@
   imports = [
     ./base.nix
   ];
-  profiles.laptop.enable = true;
+  profiles.containers.enable = true;
   profiles.dev = {
     go.enable = true;
     rust.enable = true;
   };
-  profiles.gpg.enable = true;
-  profiles.containers.enable = true;
+  profiles.laptop.enable = true;
   profiles.media.enable = true;
   profiles.mails.enable = true;
   programs.podman.enable = true;
