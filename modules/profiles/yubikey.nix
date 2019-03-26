b8845f018f71dc7db23afd3ac52e1c5b855abb16
Vincent Demeester <vincent@sbr.pm>
2019-03-26 17:14:59 +0100

    yubikey: fix the file..
    Signed-off-by: Vincent Demeester <vincent@sbr.pm>

--- modules/profiles/yubikey.nix
+++ modules/profiles/yubikey.nix
@@ -31,7 +31,7 @@ in
 KERNEL=="hidraw*", SUBSYSTEM=="hidraw", ATTRS{idVendor}=="1050", ATTRS{idProduct}=="0113|0114|0115|0116|0120|0402|0403|0406|0407|0410", TAG+="uaccess", MODE="0660", GROUP="wheel"
 # ACTION=="remove", ENV{ID_VENDOR_ID}=="1050", ENV{ID_MODEL_ID}=="0113|0114|0115|0116|0120|0402|0403|0406|0407|0410", RUN+="${pkgs.systemd}/bin/loginctl lock-sessions"
         '';
-      }
-    }
+      };
+    };
   };
 }
