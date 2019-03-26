5f8498f0823fea8c07b2510788d2b3bde66edc78
Vincent Demeester <vincent@sbr.pm>
2019-03-26 16:20:46 +0100

    yubikey: Add pcscd and update packaged for udev
    Signed-off-by: Vincent Demeester <vincent@sbr.pm>

--- modules/profiles/yubikey.nix
+++ modules/profiles/yubikey.nix
@@ -15,12 +15,6 @@ in
     };
   };
   config = mkIf cfg.enable {
-    services.udev.extraRules = ''
-      # Yubico YubiKey
-      KERNEL=="hidraw*", SUBSYSTEM=="hidraw", ATTRS{idVendor}=="1050", ATTRS{idProduct}=="0113|0114|0115|0116|0120|0402|0403|0406|0407|0410", TAG+="uaccess", MODE="0660", GROUP="wheel"
-      # ACTION=="remove", ENV{ID_VENDOR_ID}=="1050", ENV{ID_MODEL_ID}=="0113|0114|0115|0116|0120|0402|0403|0406|0407|0410", RUN+="${pkgs.systemd}/bin/loginctl lock-sessions"
-    '';
-  
     environment = {
       systemPackages = with pkgs; [
         yubico-piv-tool
@@ -28,5 +22,16 @@ in
         yubioath-desktop
       ];
     };
+    services = {
+      pcscd.enable = true;
+      udev = {
+        packages = with pkgs; [ yubikey-personalization ];
+        extraRules = ''
+# Yubico YubiKey
+KERNEL=="hidraw*", SUBSYSTEM=="hidraw", ATTRS{idVendor}=="1050", ATTRS{idProduct}=="0113|0114|0115|0116|0120|0402|0403|0406|0407|0410", TAG+="uaccess", MODE="0660", GROUP="wheel"
+# ACTION=="remove", ENV{ID_VENDOR_ID}=="1050", ENV{ID_MODEL_ID}=="0113|0114|0115|0116|0120|0402|0403|0406|0407|0410", RUN+="${pkgs.systemd}/bin/loginctl lock-sessions"
+        '';
+      }
+    }
   };
 }
