431725e81696c12543cdbed9b1b39fcfc7feb382
Vincent Demeester <vincent@sbr.pm>
2019-06-28 19:13:39 +0200

    nixpkgs: add naruhodo fedora machine
    Signed-off-by: Vincent Demeester <vincent@sbr.pm>

---
+++ machines/naruhodo.nix
@@ -0,0 +1,27 @@
+{ pkgs, ... }:
+
+{
+  imports = [
+    ./base.fedora.nix
+  ];
+  home.packages = with pkgs; [
+    kubectx
+    kustomize
+  ];
+  profiles.containers.kubernetes = {
+    enable = true;
+    containers = false;
+  };
+  profiles.zsh = {
+    enable = true;
+  };
+  profiles.dev = {
+    enable = true;
+  };
+  profiles.emacs = {
+    enable = true;
+    texlive = false;
+    daemonService = true;
+  };
+  xdg.configFile."user-dirs.dirs".source = ../modules/profiles/assets/xorg/user-dirs.dirs;
+}
