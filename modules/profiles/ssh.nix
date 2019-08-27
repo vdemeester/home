ff7a8195a468db47f42c4a2f1add5df820d83d14
Vincent Demeester <vincent@sbr.pm>
2019-08-27 10:59:02 +0200

    profiles.ssh: forward agent for local network
    Signed-off-by: Vincent Demeester <vincent@sbr.pm>

--- modules/profiles/ssh.nix
+++ modules/profiles/ssh.nix
@@ -48,7 +48,10 @@ in
         "*.redhat.com" = {
           user = "vdemeest";
         };
-      } //cfg.machines;
+        "192.168.1.*" = {
+          forwardAgent = true;
+        };
+      } // cfg.machines;
     };
   };
 }
