c93eabe77a4f2c8ba318112c9f59bd45157d1148
Vincent Demeester <vincent@sbr.pm>
2019-08-31 16:58:42 +0200

    machine/naruhodo: add ripgrep
    Signed-off-by: Vincent Demeester <vincent@sbr.pm>

--- machines/okinawa.nix
+++ machines/okinawa.nix
@@ -1,6 +1,11 @@
-{
+{ pkgs, ... }:
+
+with import ../assets/machines.nix; {
   imports = [
     ./base.nix
+  ];
+  home.packages = with pkgs; [
+    ripgrep
   ];
   profiles.gpg.enable = true;
   xdg.configFile."ape.conf".source = ../assets/ape.conf;
