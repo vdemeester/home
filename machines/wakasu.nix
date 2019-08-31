e7850ca596730860fc4375b16ff25af35abed27e
Vincent Demeester <vincent@sbr.pm>
2019-08-31 15:42:24 +0200

    machine/wakasu: remove lots of modules (as it is used as a server now)
    Signed-off-by: Vincent Demeester <vincent@sbr.pm>

--- machines/wakasu.nix
+++ machines/wakasu.nix
@@ -8,20 +8,9 @@
     enable = true;
   };
   profiles.cloud.google.enable = true;
-  profiles.containers = {
-    enable = true;
-    docker = true;
-    kubernetes = { enable = true; minikube.enable = true; };
-    openshift = { enable = true; minishift.enable = true; };
-  };
   profiles.dev = {
     go.enable = true;
-    java = { enable = true; idea = true; };
-    js.enable = true;
-    #haskell.enable = true;
-    #python.enable = true;
     rust.enable = true;
-    vscode.enable = true;
   };
   profiles.finances.enable = true;
   profiles.laptop.enable = true;
@@ -32,13 +21,6 @@
     podman.enable = true;
   };
   home.packages = with pkgs; [
-    obs-studio
-    slack
-    awscli
-    terraform
-    nur.repos.vdemeester.openshift-installer
-    nur.repos.vdemeester.operator-sdk
-    gnome3.vinagre
     openvpn
     krb5
   ];
