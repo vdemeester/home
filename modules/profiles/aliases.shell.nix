ba26abaed5791e2962c8f5f1efc8c3bd3b502884
Vincent Demeester <vincent@sbr.pm>
2019-09-25 13:25:11 +0200

    aliases: update chos4
    Signed-off-by: Vincent Demeester <vincent@sbr.pm>

--- modules/profiles/aliases.shell.nix
+++ modules/profiles/aliases.shell.nix
@@ -12,6 +12,6 @@
   t = ''exa --tree -L 2'';
   wget = ''wget -c'';
   map = ''xargs -n1'';
-  chos4 = ''curl -s chmouel.com/tmp/vincent.kubeconfig.gpg|gpg --decrypt > ~/.kube/config.os4 && export KUBECONFIG=~/.kube/config.os4'';
+  chos4 = ''curl -s chmouel.com/tmp/vincent.kubeadmin.password.gpg|gpg --decrypt > ~/.kube/kubeadmin.password.os4 && curl -s chmouel.com/tmp/vincent.webaccess.gpg|gpg --decrypt > ~/.kube/webaccess.os4 && curl -s chmouel.com/tmp/vincent.kubeconfig.gpg|gpg --decrypt > ~/.kube/config.os4 && export KUBECONFIG=~/.kube/config.os4'';
   m = ''m'';
 }
