52ad8f1171adff1126a62b7d66b61819d89ad78e
Vincent Demeester <vincent@sbr.pm>
2020-04-03 16:32:08 +0200

    machine/sakhalin: add nfs server
    Signed-off-by: Vincent Demeester <vincent@sbr.pm>

--- tmp/nixos-configuration/machine/sakhalin.nix
+++ tmp/nixos-configuration/machine/sakhalin.nix
@@ -24,7 +24,14 @@ with import ../assets/machines.nix; {
       listenTCP = true;
     };
   };
+  fileSystems."/export/gaia" = { device = "/mnt/gaia"; options = [ "bind" ]; };
   services = {
+    nfs.server = {
+      enable = true;
+      exports = ''
+        /export/gaia                 192.168.1.0/24(rw,fsid=0,no_subtree_check) 10.100.0.0/24(rw,fsid=0,no_subtree_check)
+      '';
+    };
     bind = {
       enable = true;
       forwarders = [ "8.8.8.8" "8.8.4.4" ];
@@ -67,77 +74,77 @@ with import ../assets/machines.nix; {
   };
   security.apparmor.enable = true;
   # -----------------------------------
-#   environment.etc."vrsync".text = ''
-# /home/vincent/desktop/pictures/screenshots/ vincent@synodine.home:/volumeUSB2/usbshare/pictures/screenshots/
-# /home/vincent/desktop/pictures/wallpapers/ vincent@synodine.home:/volumeUSB2/usbshare/pictures/wallpapers/
-# /home/vincent/desktop/documents/ vincent@synodine.home:/volume1/documents/
-# /mnt/Toshito/photos/ vincent@synodine.home:/volumeUSB2/usbshare/pictures/photos/
-# /mnt/Toshito/music/ vincent@synodine.home:/volumeUSB2/usbshare/music/
-#   '';
-#   systemd.services.vrsync = {
-#     description = "vrsync - sync folders to NAS";
-#     requires = [ "network-online.target" ];
-#     after    = [ "network-online.target" ];
-#
-#     unitConfig.X-StopOnRemoval = false;
-#     restartIfChanged = false;
-#
-#     path = with pkgs; [ rsync coreutils bash openssh ];
-#     script = ''
-#     ${pkgs.vrsync}/bin/vrsync
-#     '';
-#
-#     startAt = "hourly";
-#     serviceConfig = {
-#       Type = "oneshot";
-#       OnFailure = "status-email-root@%n.service";
-#     };
-#   };
-#   # ape – sync git mirrors
-#   systemd.services.ape = {
-#     description = "Ape - sync git mirrors";
-#     requires = [ "network-online.target" ];
-#     after    = [ "network-online.target" ];
-#
-#     restartIfChanged = false;
-#     unitConfig.X-StopOnRemoval = false;
-#
-#     serviceConfig = {
-#       Type = "oneshot";
-#       User = "vincent";
-#       OnFailure = "status-email-root@%n.service";
-#     };
-#
-#     path = with pkgs; [ git ];
-#     script = ''
-#     ${pkgs.nur.repos.vdemeester.ape}/bin/ape up /home/vincent/var/mirrors
-#     '';
-#
-#     startAt = "hourly";
-#   };
-#   # mr -i u daily
-#   systemd.services.mr = {
-#     description = "Update configs daily";
-#     requires = [ "network-online.target" ];
-#     after    = [ "network-online.target" ];
-#
-#     restartIfChanged = false;
-#     unitConfig.X-StopOnRemoval = false;
-#
-#     serviceConfig = {
-#       Type = "oneshot";
-#       User = "vincent";
-#       OnFailure = "status-email-root@%n.service";
-#     };
-#
-#     path = with pkgs; [ git mr ];
-#     script = ''
-#     set -e
-#      cd /mnt/synodine/volumeUSB2/usbshare/src/github.com/vdemeester/configs/
-#      mr -t run git reset --hard
-#      mr -t u
-#     '';
-#
-#     startAt = "daily";
-#   };
+  #   environment.etc."vrsync".text = ''
+  # /home/vincent/desktop/pictures/screenshots/ vincent@synodine.home:/volumeUSB2/usbshare/pictures/screenshots/
+  # /home/vincent/desktop/pictures/wallpapers/ vincent@synodine.home:/volumeUSB2/usbshare/pictures/wallpapers/
+  # /home/vincent/desktop/documents/ vincent@synodine.home:/volume1/documents/
+  # /mnt/Toshito/photos/ vincent@synodine.home:/volumeUSB2/usbshare/pictures/photos/
+  # /mnt/Toshito/music/ vincent@synodine.home:/volumeUSB2/usbshare/music/
+  #   '';
+  #   systemd.services.vrsync = {
+  #     description = "vrsync - sync folders to NAS";
+  #     requires = [ "network-online.target" ];
+  #     after    = [ "network-online.target" ];
+  #
+  #     unitConfig.X-StopOnRemoval = false;
+  #     restartIfChanged = false;
+  #
+  #     path = with pkgs; [ rsync coreutils bash openssh ];
+  #     script = ''
+  #     ${pkgs.vrsync}/bin/vrsync
+  #     '';
+  #
+  #     startAt = "hourly";
+  #     serviceConfig = {
+  #       Type = "oneshot";
+  #       OnFailure = "status-email-root@%n.service";
+  #     };
+  #   };
+  #   # ape – sync git mirrors
+  #   systemd.services.ape = {
+  #     description = "Ape - sync git mirrors";
+  #     requires = [ "network-online.target" ];
+  #     after    = [ "network-online.target" ];
+  #
+  #     restartIfChanged = false;
+  #     unitConfig.X-StopOnRemoval = false;
+  #
+  #     serviceConfig = {
+  #       Type = "oneshot";
+  #       User = "vincent";
+  #       OnFailure = "status-email-root@%n.service";
+  #     };
+  #
+  #     path = with pkgs; [ git ];
+  #     script = ''
+  #     ${pkgs.nur.repos.vdemeester.ape}/bin/ape up /home/vincent/var/mirrors
+  #     '';
+  #
+  #     startAt = "hourly";
+  #   };
+  #   # mr -i u daily
+  #   systemd.services.mr = {
+  #     description = "Update configs daily";
+  #     requires = [ "network-online.target" ];
+  #     after    = [ "network-online.target" ];
+  #
+  #     restartIfChanged = false;
+  #     unitConfig.X-StopOnRemoval = false;
+  #
+  #     serviceConfig = {
+  #       Type = "oneshot";
+  #       User = "vincent";
+  #       OnFailure = "status-email-root@%n.service";
+  #     };
+  #
+  #     path = with pkgs; [ git mr ];
+  #     script = ''
+  #     set -e
+  #      cd /mnt/synodine/volumeUSB2/usbshare/src/github.com/vdemeester/configs/
+  #      mr -t run git reset --hard
+  #      mr -t u
+  #     '';
+  #
+  #     startAt = "daily";
+  #   };
 }
