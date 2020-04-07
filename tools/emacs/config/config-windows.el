5ea53a56710081756fde56ed0e88d648299d1618
Vincent Demeester <vincent@sbr.pm>
2020-04-07 18:59:27 +0200

    config-windows.el: re-enable ace-window and windmove
    Signed-off-by: Vincent Demeester <vincent@sbr.pm>

--- tools/emacs/config/config-windows.el
+++ tools/emacs/config/config-windows.el
@@ -1,5 +1,35 @@
 ;;; -*- lexical-binding: t; -*-
 
+;; UseAceWindow
+(use-package ace-window
+  :commands (ace-window ace-swap-window)
+  :bind (("C-x o"   . ace-window)
+         ("C-c w w" . ace-window)
+         ("C-c w s" . ace-swap-window))
+  :config
+  (setq-default aw-keys '(?a ?u ?i ?e ?, ?c ?t ?r ?m)
+                aw-scope 'frame
+                aw-dispatch-always t
+                aw-dispatch-alist
+                '((?s aw-swap-window "Swap Windows")
+                  (?2 aw-split-window-vert "Split Window Vertically")
+                  (?3 aw-split-window-horz "Split Window Horizontally")
+                  (?? aw-show-dispatch-help))
+                aw-minibuffer-flag t
+                aw-ignore-current nil
+                aw-display-mode-overlay t
+                aw-background t))
+;; -UseAceWindow
+
+;; UseWindmove
+(use-package windmove
+  :commands (windmove-left windmove-right windmove-down windmove-up)
+  :bind (("M-<left>" . windmove-left)
+         ("M-<down>" . windmove-down)
+         ("M-<up>" . windmove-up)
+         ("M-<right>" . windmove-right)))
+;; -UseWindmove
+
 (defun vde/window-split-toggle ()
   "Toggle between horizontal and vertical split with two windows."
   (interactive)
@@ -25,31 +55,5 @@
    eyebrowse-mode-line-style 'always
    eyebrowse-new-workspace t
    eyebrowse-wrap-around t))
-
-(use-package ace-window                 ; Better movements between windows
-  :disabled
-  :custom
-  (aw-keys '(?a ?u ?i ?e ?, ?c ?t ?r ?m))
-  (aw-scope 'frame)
-  (aw-dispatch-always t)
-  (aw-dispatch-alist
-   '((?s aw-swap-window "Swap Windows")
-     (?2 aw-split-window-vert "Split Window Vertically")
-     (?3 aw-split-window-horz "Split Window Horizontally")
-     (?? aw-show-dispatch-help)))
-  (aw-minibuffer-flag t)
-  (aw-ignore-current nil)
-  (aw-display-mode-overlay t)
-  (aw-background t)
-  :bind (("C-x o"   . ace-window)
-         ("C-c w w" . ace-window)
-         ("C-c w s" . ace-swap-window)))
-
-(use-package windmove
-  :disabled
-  :bind (("M-<left>" . windmove-left)
-         ("M-<down>" . windmove-down)
-         ("M-<up>" . windmove-up)
-         ("M-<right>" . windmove-right)))
 
 (provide 'setup-windows)
