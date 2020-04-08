f6e92925d93f3f837e317dd1a1cdac1921a5ae33
Vincent Demeester <vincent@sbr.pm>
2020-04-08 16:50:22 +0200

    writing.el: re-enable markdown-mode
    Signed-off-by: Vincent Demeester <vincent@sbr.pm>

---
+++ tools/emacs/config/writing.el
@@ -0,0 +1,19 @@
+(use-package markdown-mode
+  :mode ("\\.md\\'" . markdown-mode)
+  :config
+  (setq markdown-fontify-code-blocks-natively t)
+
+  ;; Don't change font in code blocks
+  (set-face-attribute 'markdown-code-face nil
+                      :inherit nil)
+
+  ;; Process Markdown with Pandoc, using a custom stylesheet for nice output
+  (let ((stylesheet (expand-file-name
+                     (locate-user-emacs-file "etc/pandoc.css"))))
+    (setq markdown-command
+          (mapconcat #'shell-quote-argument
+                     `("pandoc" "--toc" "--section-divs"
+                       "--css" ,(concat "file://" stylesheet)
+                       "--standalone" "-f" "markdown" "-t" "html5")
+                     " ")))
+  (add-hook 'markdown-mode-hook #'auto-fill-mode))
