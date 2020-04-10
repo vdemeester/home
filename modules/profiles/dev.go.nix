8b4168373adceb50db6aa6f39c7456a04fe3b529
Vincent Demeester <vincent@sbr.pm>
2020-04-10 14:04:11 +0200

    nixpkgs-fmt: dev.go.nix and zsh.nix
    Signed-off-by: Vincent Demeester <vincent@sbr.pm>

--- modules/profiles/dev.go.nix
+++ modules/profiles/dev.go.nix
@@ -10,51 +10,55 @@ in
       enable = mkEnableOption "Enable go development profile";
     };
   };
-  config = mkIf cfg.enable (mkMerge [
-    {
-      home.sessionVariables = {
-        GOPATH = "${config.home.homeDirectory}";
-      };
-      profiles.dev.enable = true;
-      home.packages = with pkgs; [
-        gcc
-        go
-        godef
-        golangci-lint
-        golint
-        gopkgs
-        go-outline
-        go-symbols
-        delve
-        # vendoring tools
-        dep
-        nur.repos.vdemeester.dep-collector
-        # misc
-        protobuf
-        nur.repos.vdemeester.protobuild
-        nur.repos.vdemeester.ram
-        nur.repos.vdemeester.sec
-        nur.repos.vdemeester.goreturns
-        nur.repos.vdemeester.esc
-        nur.repos.vdemeester.yaspell
-      ];
-      xdg.configFile."nr/go" = {
-        text = builtins.toJSON [
-          {cmd = "pprof"; chan = "unstable";}
-          {cmd = "vndr"; chan = "unstable";}
-          {cmd = "go2nix"; }
-          {cmd = "dep2nix"; }
+  config = mkIf cfg.enable (
+    mkMerge [
+      {
+        home.sessionVariables = {
+          GOPATH = "${config.home.homeDirectory}";
+        };
+        profiles.dev.enable = true;
+        home.packages = with pkgs; [
+          gcc
+          go
+          godef
+          golangci-lint
+          golint
+          gopkgs
+          go-outline
+          go-symbols
+          delve
+          # vendoring tools
+          dep
+          nur.repos.vdemeester.dep-collector
+          # misc
+          protobuf
+          nur.repos.vdemeester.protobuild
+          nur.repos.vdemeester.ram
+          nur.repos.vdemeester.sec
+          nur.repos.vdemeester.goreturns
+          nur.repos.vdemeester.esc
+          nur.repos.vdemeester.yaspell
         ];
-        onChange = "${pkgs.nur.repos.vdemeester.nr}/bin/nr go";
-      };
-    }
-    (mkIf config.profiles.fish.enable {
-      xdg.configFile."fish/conf.d/go.fish".source = ./assets/fish/go.fish;
-      programs.fish.shellAbbrs = {
-        got = "go test -v";
-        gob = "go build -v";
-        gol = "golangci-lint run";
-      };
-    })
-  ]);
+        xdg.configFile."nr/go" = {
+          text = builtins.toJSON [
+            { cmd = "pprof"; chan = "unstable"; }
+            { cmd = "vndr"; chan = "unstable"; }
+            { cmd = "go2nix"; }
+            { cmd = "dep2nix"; }
+          ];
+          onChange = "${pkgs.nur.repos.vdemeester.nr}/bin/nr go";
+        };
+      }
+      (
+        mkIf config.profiles.fish.enable {
+          xdg.configFile."fish/conf.d/go.fish".source = ./assets/fish/go.fish;
+          programs.fish.shellAbbrs = {
+            got = "go test -v";
+            gob = "go build -v";
+            gol = "golangci-lint run";
+          };
+        }
+      )
+    ]
+  );
 }
