c3de4cf1b7ee6dc1b39f6a480f93dfd031c646b3
Vincent Demeester <vincent@sbr.pm>
2020-04-20 12:34:31 +0200

    machines: remove carthage, add kerkouane
    Signed-off-by: Vincent Demeester <vincent@sbr.pm>

--- machines/carthage.nix
+++
@@ -1,8 +0,0 @@
-{
-  imports = [ ./base.nix ];
-  profiles.containers = {
-    enable = true;
-    docker = false;
-    podman = true;
-  };
-}
---
+++ machines/kerkouane.nix
@@ -0,0 +1,3 @@
+{
+  imports = [ ./base.nixos.nix ];
+}
