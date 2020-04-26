9ff3f87a44ff343258d2d45dd6941bf159c13a04
Vincent Demeester <vincent@sbr.pm>
2020-04-26 16:35:10 +0200

    Add .mrconfig for sources
    Signed-off-by: Vincent Demeester <vincent@sbr.pm>

--- modules/profiles/dev.nix
+++ modules/profiles/dev.nix
@@ -10,38 +10,47 @@ in
       enable = mkEnableOption "Enable development profile";
     };
   };
-  config = mkIf cfg.enable (mkMerge [
-    {
-      profiles.git.enable = true;
-      profiles.emacs.enable = true;
-      home.file.".ignore".text = ''
-      *.swp
-      *~
-      **/VENDOR-LICENSE
-      '';
-      home.packages = with pkgs; [
-        binutils
-        cmake
-        fswatch
-        gnumake
-        jq
-        mercurial
-        niv
-        ripgrep
-        shfmt
-      ];
-      xdg.configFile."nr/dev" = {
-        text = builtins.toJSON [
-          {cmd = "yq";} {cmd = "lnav";} {cmd = "miniserve";}
-          { cmd = "licensor"; } { cmd = "nix-review"; }
-          {cmd = "yamllint"; pkg = "python37Packages.yamllint";}
-          {cmd = "nix-prefetch-git"; pkg = "nix-prefetch-scripts";}
-          {cmd = "nix-prefetch-hg"; pkg = "nix-prefetch-scripts";}
-          {cmd = "http"; pkg = "httpie"; }
-        ];
-        onChange = "${pkgs.nur.repos.vdemeester.nr}/bin/nr dev";
-      };
-      services.lorri.enable = true;
-    }
-  ]);
+  config = mkIf cfg.enable (
+    mkMerge [
+      {
+        profiles.git.enable = true;
+        profiles.emacs.enable = true;
+        home.file.".ignore".text = ''
+          *.swp
+          *~
+          **/VENDOR-LICENSE
+        '';
+        xdg.configFile."mr".source = ./assets/lib.mr;
+        home.file."src/.mrconfig".source = ./assets/src.mr;
+        home.file."src/github.com/.mrconfig".source = ./assets/src.github.mr;
+        home.packages = with pkgs;
+          [
+            binutils
+            cmake
+            fswatch
+            gnumake
+            jq
+            mercurial
+            niv
+            ripgrep
+            shfmt
+          ];
+        xdg.configFile."nr/dev" = {
+          text = builtins.toJSON [
+            { cmd = "yq"; }
+            { cmd = "lnav"; }
+            { cmd = "miniserve"; }
+            { cmd = "licensor"; }
+            { cmd = "nix-review"; }
+            { cmd = "yamllint"; pkg = "python37Packages.yamllint"; }
+            { cmd = "nix-prefetch-git"; pkg = "nix-prefetch-scripts"; }
+            { cmd = "nix-prefetch-hg"; pkg = "nix-prefetch-scripts"; }
+            { cmd = "http"; pkg = "httpie"; }
+          ];
+          onChange = "${pkgs.nur.repos.vdemeester.nr}/bin/nr dev";
+        };
+        services.lorri.enable = true;
+      }
+    ]
+  );
 }
