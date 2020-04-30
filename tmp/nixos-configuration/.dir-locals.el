0dd2534b18f9bd56a55f30d7ff8d232aeea80e7d
Vincent Demeester <vincent@sbr.pm>
2020-04-30 02:38:23 +0200

    Remove tmp/nixos-configuration 👼
    Signed-off-by: Vincent Demeester <vincent@sbr.pm>

--- tmp/nixos-configuration/.dir-locals.el
+++
@@ -1,7 +0,0 @@
-;;; Directory Local Variables
-;;; For more information see (info "(emacs) Directory Variables")
-
-((nil (eval . (setq projectile-project-compilation-cmd "nixos-rebuild dry-build"
-		    projectile-project-run-cmd "nixos-rebuild switch")))
- (nix-mode
-  (tab-width . 2)))
--- tmp/nixos-configuration/overlays/sbr.overlay.nix
+++
@@ -1,8 +0,0 @@
-self: super: {
-  vrsync = import ../pkgs/vrsync {
-    inherit (self) stdenv lib;
-  };
-  vde-thinkpad = import ../pkgs/vde-thinkpad {
-    inherit (self) stdenv lib;
-  };
-}
