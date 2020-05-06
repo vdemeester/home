c85a21184632bf5a9667591f5a46713b8b3bd889
Vincent Demeester <vincent@sbr.pm>
2020-05-06 10:14:14 +0200

    buildkit: fix package version name 😅
    Signed-off-by: Vincent Demeester <vincent@sbr.pm>

--- pkgs/buildkit/default.nix
+++ pkgs/buildkit/default.nix
@@ -1,7 +1,7 @@
 { stdenv, lib, fetchFromGitHub, buildGoPackage }:
 
 buildGoPackage rec {
-  name = "buildkit${version}";
+  name = "buildkit-${version}";
   version = "0.7.1";
   rev = "v${version}";
 
