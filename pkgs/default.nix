ae046e656e2a30123c6e43b8a275027ef3e4c96a
Vincent Demeester <vincent@sbr.pm>
2020-05-07 16:57:04 +0200

    pkgs: nixpkgs-fmt
    Signed-off-by: Vincent Demeester <vincent@sbr.pm>

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,73 +1,72 @@
-{ pkgs ? import <nixpkgs> {} }:
+{ pkgs ? import <nixpkgs> { } }:
 
 rec {
   # pre nur-packages import
-  scripts = pkgs.callPackage ./scripts {};
-  tmux-tpm = pkgs.callPackage ./tmux-tpm {};
-  vrsync = pkgs.callPackage ./vrsync {};
-  vde-thinkpad = pkgs.callPackage ./vde-thinkpad {};
+  scripts = pkgs.callPackage ./scripts { };
+  tmux-tpm = pkgs.callPackage ./tmux-tpm { };
+  vrsync = pkgs.callPackage ./vrsync { };
+  vde-thinkpad = pkgs.callPackage ./vde-thinkpad { };
 
   # Mine
-  ape = pkgs.callPackage ./ape {};
-  fhs-std = pkgs.callPackage ./fhs/std.nix {};
-  nr = pkgs.callPackage ./nr {};
-  ram = pkgs.callPackage ./ram {};
-  sec = pkgs.callPackage ./sec {};
-  systemd-email = pkgs.callPackage ./systemd-email {};
-  yak = pkgs.callPackage ./yak {};
+  ape = pkgs.callPackage ./ape { };
+  fhs-std = pkgs.callPackage ./fhs/std.nix { };
+  nr = pkgs.callPackage ./nr { };
+  ram = pkgs.callPackage ./ram { };
+  sec = pkgs.callPackage ./sec { };
+  systemd-email = pkgs.callPackage ./systemd-email { };
+  yak = pkgs.callPackage ./yak { };
 
   # Maybe upstream
-  athens = pkgs.callPackage ./athens {};
-  dep-collector = pkgs.callPackage ./dep-collector {};
-  envbox = pkgs.callPackage ./envbox {};
-  esc = pkgs.callPackage ./esc {};
-  gogo-protobuf = pkgs.callPackage ./gogo-protobuf {};
-  goreturns = pkgs.callPackage ./goreturns {};
-  gorun = pkgs.callPackage ./gorun {};
-  govanityurl = pkgs.callPackage ./govanityurl {};
-  ko = pkgs.callPackage ./ko {};
-  kss = pkgs.callPackage ./kss {};
-  kubernix = pkgs.callPackage ./kubernix {};
-  krew = pkgs.callPackage ./krew {};
-  prm = pkgs.callPackage ./prm {};
-  protobuild = pkgs.callPackage ./protobuild {};
-  rmapi = pkgs.callPackage ./rmapi {};
-  toolbox = pkgs.callPackage ./toolbox {};
-  yaspell = pkgs.callPackage ./yaspell {};
+  athens = pkgs.callPackage ./athens { };
+  envbox = pkgs.callPackage ./envbox { };
+  esc = pkgs.callPackage ./esc { };
+  gogo-protobuf = pkgs.callPackage ./gogo-protobuf { };
+  goreturns = pkgs.callPackage ./goreturns { };
+  gorun = pkgs.callPackage ./gorun { };
+  govanityurl = pkgs.callPackage ./govanityurl { };
+  ko = pkgs.callPackage ./ko { };
+  kss = pkgs.callPackage ./kss { };
+  kubernix = pkgs.callPackage ./kubernix { };
+  krew = pkgs.callPackage ./krew { };
+  prm = pkgs.callPackage ./prm { };
+  protobuild = pkgs.callPackage ./protobuild { };
+  rmapi = pkgs.callPackage ./rmapi { };
+  toolbox = pkgs.callPackage ./toolbox { };
+  yaspell = pkgs.callPackage ./yaspell { };
 
   # OpenShift
-  inherit (pkgs.callPackage ./oc {})
+  inherit (pkgs.callPackage ./oc { })
     oc_4_1
     oc_4_2
     oc_4_3
     oc_4_4
     ;
   oc = oc_4_4;
-  openshift-install = pkgs.callPackage ./openshift-install {};
+  openshift-install = pkgs.callPackage ./openshift-install { };
 
-  inherit (pkgs.callPackage ./crc {})
+  inherit (pkgs.callPackage ./crc { })
     crc_1_9
     crc_1_10
     ;
   crc = crc_1_10;
 
   # Operator SDK
-  inherit (pkgs.callPackage ./operator-sdk {})
+  inherit (pkgs.callPackage ./operator-sdk { })
     operator-sdk_0_16
     operator-sdk_0_17
     ;
   operator-sdk = operator-sdk_0_17;
 
   # Tekton
-  inherit (pkgs.callPackage ./tkn {})
+  inherit (pkgs.callPackage ./tkn { })
     tkn_0_8
     tkn_0_9
     ;
   tkn = tkn_0_9;
 
   # Upstream
-  buildkit = pkgs.callPackage ./buildkit {};
-  inherit (pkgs.callPackage ./containerd {})
+  buildkit = pkgs.callPackage ./buildkit { };
+  inherit (pkgs.callPackage ./containerd { })
     containerd_1_2
     containerd_1_3
     ;
