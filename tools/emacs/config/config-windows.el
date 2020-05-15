ea71d35433d5b4ae97b4b1c1c11dda093ab2bcf4
Vincent Demeester <vincent@sbr.pm>
2020-05-15 14:04:15 +0200

    config-window.el: remove unused func
    Signed-off-by: Vincent Demeester <vincent@sbr.pm>

--- tools/emacs/config/config-windows.el
+++ tools/emacs/config/config-windows.el
@@ -41,21 +41,5 @@
          ("C-s-<left>" . windmove-left)))
 ;; -UseWindmove
 
-(defun vde/window-split-toggle ()
-  "Toggle between horizontal and vertical split with two windows."
-  (interactive)
-  (if (> (length (window-list)) 2)
-      (error "Can't toggle with more than 2 windows!")
-    (let ((func (if (window-full-height-p)
-                    #'split-window-vertically
-                  #'split-window-horizontally)))
-      (delete-other-windows)
-      (funcall func)
-      (save-selected-window
-        (other-window 1)
-        (switch-to-buffer (other-buffer))))))
-
-;;(bind-key "C-c w t" #'vde/window-split-toggle)
-
 (provide 'config-windows)
 ;;; config-windows ends here
