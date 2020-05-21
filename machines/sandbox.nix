cb9e352f9b2ed6df04bbd4fc2a92e3a5dcc0b723
Vincent Demeester <vincent@sbr.pm>
2020-05-21 15:37:24 +0200

    machines/sandbox: do the same as other machines…
    … for now. The goal will be to de-duplicate all this, but let's start
    similar and slowly move away.
    
    Signed-off-by: Vincent Demeester <vincent@sbr.pm>

--- machines/sandbox.nix
+++ machines/sandbox.nix
@@ -1,22 +1,3 @@
-{ config, pkgs, ... }:
-
 {
-  networking = {
-    firewall.enable = false; # we are in safe territory :D
-    networkmanager = {
-      dns = "dnsmasq";
-    };
-  };
-  profiles = {
-    dev.enable = true;
-    nix-config.buildCores = 4;
-    ssh = {
-      enable = true;
-      forwardX11 = true;
-    };
-  };
-  home-manager.users.vincent = import ./kerkouane.nix;
-  home-manager.users.root = { pkgs, ... }: {
-    home.packages = with pkgs; [ htop ];
-  };
+  imports = [ ./nixos-base.nix ];
 }
