5f27671f5a93f20b70c0651b77088e9a20a9e455
Vincent Demeester <vincent@sbr.pm>
2020-05-25 17:38:38 +0200

    profiles.home: fix with import 🙃
    Signed-off-by: Vincent Demeester <vincent@sbr.pm>

--- modules/profiles/home.nixos.nix
+++ modules/profiles/home.nixos.nix
@@ -3,8 +3,6 @@
 with lib;
 let
   cfg = config.profiles.home;
-  machines = import ../../assets/machines.nix;
-  home = machines.home;
 in
 {
   options = {
@@ -15,27 +13,29 @@ in
     networking.domain = "home";
     time.timeZone = "Europe/Paris";
     # To mimic autofs on fedora
-    fileSystems."/net/synodine.home/" = {
-      device = "${home.ips.synodine}:/";
-      fsType = "nfs";
-      options = [ "x-systemd.automount" "noauto" ];
-    };
-    # FIXME(vdemeester): I think it acts like this because there is only one export
-    fileSystems."/net/sakhalin.home/export/" = {
-      device = "${home.ips.sakhalin}:/";
-      fsType = "nfs";
-      options = [ "x-systemd.automount" "noauto" ];
-    };
-    # Deprecated
-    fileSystems."/mnt/synodine" = {
-      device = "${home.ips.synodine}:/";
-      fsType = "nfs";
-      options = [ "x-systemd.automount" "noauto" ];
-    };
-    fileSystems."/mnt/sakhalin" = {
-      device = "${home.ips.sakhalin}:/";
-      fsType = "nfs";
-      options = [ "x-systemd.automount" "noauto" ];
+    fileSystems = with import ../../assets/machines.nix; {
+      "/net/synodine.home/" = {
+        device = "${home.ips.synodine}:/";
+        fsType = "nfs";
+        options = [ "x-systemd.automount" "noauto" ];
+      };
+      # FIXME(vdemeester): I think it acts like this because there is only one export
+      "/net/sakhalin.home/export/" = {
+        device = "${home.ips.sakhalin}:/";
+        fsType = "nfs";
+        options = [ "x-systemd.automount" "noauto" ];
+      };
+      # Deprecated
+      "/mnt/synodine" = {
+        device = "${home.ips.synodine}:/";
+        fsType = "nfs";
+        options = [ "x-systemd.automount" "noauto" ];
+      };
+      "/mnt/sakhalin" = {
+        device = "${home.ips.sakhalin}:/";
+        fsType = "nfs";
+        options = [ "x-systemd.automount" "noauto" ];
+      };
     };
   };
 }
