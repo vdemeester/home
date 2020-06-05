2042a10f7c532c3bd1fe107620ddad8f8b9e67be
Vincent Demeester <vincent@sbr.pm>
2020-06-05 15:38:47 +0200

    profiles.home: add hosts from machines secrets
    Signed-off-by: Vincent Demeester <vincent@sbr.pm>

--- modules/profiles/home.nixos.nix
+++ modules/profiles/home.nixos.nix
@@ -13,7 +13,27 @@ in
   };
   config = mkIf cfg {
     boot.kernelParams = [ "nfs.nfs4_disable_idmapping=0" "nfsd.nfs4_disable_idmapping=0" ];
-    networking.domain = "home";
+    networking = {
+      domain = "home";
+      hosts = with machines; {
+        "${home.ips.honshu}" = [ "honshu.home" ];
+        "${wireguard.ips.honshu}" = [ "honshu.vpn" ];
+        "${home.ips.shikoku}" = [ "shikoku.home" ];
+        "${wireguard.ips.shikoku}" = [ "shikoku.vpn" ];
+        "${home.ips.wakasu}" = [ "wakasu.home" ];
+        "${wireguard.ips.wakasu}" = [ "wakasu.vpn" ];
+        "${home.ips.hokkaido}" = [ "hokkaido.home" ];
+        "${wireguard.ips.hokkaido}" = [ "hokkaido.vpn" ];
+        "${home.ips.sakhalin}" = [ "sakhalin.home" ];
+        "${wireguard.ips.sakhalin}" = [ "sakhalin.vpn" ];
+        "${wireguard.ips.massimo}" = [ "massimo.vpn" ];
+        "${home.ips.synodine}" = [ "synodine.home" ];
+        "${home.ips.okinawa}" = [ "okinawa.home" "cache.home" "svc.home" "nix.cache.home" "go.cache.home" ];
+        "${wireguard.ips.okinawa}" = [ "okinawa.vpn" ];
+        "${wireguard.ips.carthage}" = [ "carthage.vpn" ];
+        "${wireguard.ips.kerkouane}" = [ "kerkouane.vpn" ];
+      };
+    };
     time.timeZone = "Europe/Paris";
     # To mimic autofs on fedora
     fileSystems = mkIf secretCondition {
