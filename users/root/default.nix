2c23010722f985bf16af42a845e572ef3e4e177b
Vincent Demeester <vincent@sbr.pm>
2020-06-05 15:16:45 +0200

    users.root: set zsh as shell if enabled
    Signed-off-by: Vincent Demeester <vincent@sbr.pm>

--- users/root/default.nix
+++ users/root/default.nix
@@ -1,6 +1,9 @@
 { config, lib, pkgs, ... }:
 
-{
+with lib; {
+  users.users.root = {
+    shell = mkIf config.programs.zsh.enable pkgs.zsh;
+  };
   home-manager.users.root = lib.mkMerge (
     [ (import ../vincent/core) ]
   );
