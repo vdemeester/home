2be1317acbac813c0807be3199b06d9991eba089
Vincent Demeester <vincent@sbr.pm>
2020-06-06 17:04:05 +0200

    shell.nix: update NIX_PATH to add…
    … nixos and nixos-unstable (just in case)
    
    Signed-off-by: Vincent Demeester <vincent@sbr.pm>

--- shell.nix
+++ shell.nix
@@ -1,6 +1,8 @@
 let
   sources = import ./nix;
   pkgs = sources.nixpkgs { };
+  nixos-unstable = sources.pkgs-unstable { };
+  nixos = sources.pkgs { };
 in
 pkgs.mkShell {
   name = "nix-config";
@@ -11,6 +13,6 @@ pkgs.mkShell {
     nixpkgs-fmt
   ];
   shellHook = ''
-    export NIX_PATH="nixpkgs=${pkgs.path}"
+    export NIX_PATH="nixpkgs=${pkgs.path}:nixos=${nixos.path}:nixos-unstable=${nixos-unstable.path}"
   '';
 }
