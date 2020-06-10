74bf8891402d8f6e27a3444b3876c29525d59d1f
Vincent Demeester <vincent@sbr.pm>
2020-06-10 12:48:36 +0200

    users/vincent: add gcloud package instead of nr
    Signed-off-by: Vincent Demeester <vincent@sbr.pm>

--- users/vincent/containers/gcloud.nix
+++ users/vincent/containers/gcloud.nix
@@ -1,11 +1,5 @@
 { pkgs, ... }:
 
 {
-  xdg.configFile."nr/gcloud" = {
-    text = builtins.toJSON [
-      { cmd = "gcloud"; pkg = "google-cloud-sdk"; }
-      { cmd = "gcsfuse"; }
-    ];
-    onChange = "${pkgs.my.nr}/bin/nr gcloud";
-  };
+  home.packages = with pkgs; [ google-cloud-sdk ];
 }
