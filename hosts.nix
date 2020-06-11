0a250ead84c17367b26b0d406d07cb255be1dae2
Vincent Demeester <vincent@sbr.pm>
2020-06-11 15:21:21 +0200

    systems: add support for building VMs
    … and add foo as a VM (to try things out)
    
    Signed-off-by: Vincent Demeester <vincent@sbr.pm>

--- hosts.nix
+++ hosts.nix
@@ -4,5 +4,7 @@
   kerkouane = { arch = "x86_64-linux"; };
   okinawa = { arch = "x86_64-linux"; };
   sakhalin = { arch = "x86_64-linux"; };
+  # Test VM
+  foo = { arch = "x86_64-linux"; type = "unstable"; vm = true; };
   # mypi = { arch = "aarch64-linux" };
 }
