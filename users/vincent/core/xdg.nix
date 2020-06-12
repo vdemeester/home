379057b4553ae9fee80a89af8016a88bb9a9ae7b
Vincent Demeester <vincent@sbr.pm>
2020-06-12 11:05:05 +0200

    Revert "users/vincent: fix downloads xdg dir"
    This reverts commit 88911edd5eeeff94ed28f796863af4a4aa80d7cd.

--- users/vincent/core/xdg.nix
+++ users/vincent/core/xdg.nix
@@ -5,7 +5,7 @@
       enable = true;
       desktop = "$HOME/desktop";
       documents = "$HOME/desktop/documents";
-      downloads = "$HOME/desktop/downloads";
+      download = "$HOME/desktop/downloads";
       music = "$HOME/desktop/music";
       pictures = "$HOME/desktop/pictures";
       publicShare = "$HOME/desktop/sites";
