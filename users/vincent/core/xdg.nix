88911edd5eeeff94ed28f796863af4a4aa80d7cd
Vincent Demeester <vincent@sbr.pm>
2020-06-12 11:01:36 +0200

    users/vincent: fix downloads xdg dir
    Signed-off-by: Vincent Demeester <vincent@sbr.pm>

--- users/vincent/core/xdg.nix
+++ users/vincent/core/xdg.nix
@@ -5,7 +5,7 @@
       enable = true;
       desktop = "$HOME/desktop";
       documents = "$HOME/desktop/documents";
-      download = "$HOME/desktop/downloads";
+      downloads = "$HOME/desktop/downloads";
       music = "$HOME/desktop/music";
       pictures = "$HOME/desktop/pictures";
       publicShare = "$HOME/desktop/sites";
