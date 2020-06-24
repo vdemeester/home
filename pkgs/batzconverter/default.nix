0f0ba58729ca3ebd2f0c6cf55f27da807d377cde
Vincent Demeester <vincent@sbr.pm>
2020-06-24 12:24:13 +0200

    pkgs/batzconverter: -git -> 1.0
    Signed-off-by: Vincent Demeester <vincent@sbr.pm>

--- pkgs/batzconverter/default.nix
+++ pkgs/batzconverter/default.nix
@@ -2,13 +2,13 @@
 
 stdenv.mkDerivation rec {
   pname = "batzconverter";
-  version = "git";
+  version = "1.0";
   name = "${pname}-${version}";
 
   src = fetchFromGitHub {
     owner = "chmouel";
     repo = "batzconverter";
-    rev = "907a1a6f97c590c5d6f9427c7a92e6be1d18a697";
+    rev = "${version}";
     sha256 = "0alrxqhbi3ivx6a2n443rmry3y9b896lansmvfw6xwq7p1h7x22y";
   };
 
