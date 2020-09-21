3606d82193305c23b79f5dec9b0df6f27935b998
Vincent Demeester <vincent@sbr.pm>
2020-09-21 09:34:27 +0200

    pkgs/ram: 0.3.1 -> 0.3.2
    Signed-off-by: Vincent Demeester <vincent@sbr.pm>

--- pkgs/ram/default.nix
+++ pkgs/ram/default.nix
@@ -2,16 +2,15 @@
 
 buildGoModule rec {
   name = "ram-${version}";
-  version = "0.3.1";
+  version = "0.3.2";
   rev = "v${version}";
 
   src = fetchgit {
     inherit rev;
     url = "https://git.sr.ht/~vdemeester/ram";
-    sha256 = "1xmx4dc30din9cwl7c0zz9pvdzk9mirvps8m89xfrkmcj5h8k8rr";
+    sha256 = "1zjyw699cxylvgh9zakqyylmjrwxwq36g0jls5iwwm75admgqnfr";
   };
-  vendorSha256 = "16b2061x0z83c4j59fxm1hhzr9akq1y2lbpiqdh139sqy6l6h7df";
-  modSha256 = "${vendorSha256}";
+  vendorSha256 = null;
 
   meta = {
     description = "A golang opiniated continuous testing tool 🐏";
