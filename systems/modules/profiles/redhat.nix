57edb016e6b57c4ee698ac06d1c0f5d58ed213ea
Vincent Demeester <vincent@sbr.pm>
2020-10-12 11:43:07 +0200

    systems/modules: add kerberos to redhat profile
    Signed-off-by: Vincent Demeester <vincent@sbr.pm>

--- systems/modules/profiles/redhat.nix
+++ systems/modules/profiles/redhat.nix
@@ -11,6 +11,7 @@ in
     };
   };
   config = mkIf cfg.enable {
+    environment.systemPackages = with pkgs; [ krb5 ];
     # NetworkManager
     environment.etc."NetworkManager/system-connections/1-RHVPN.ovpn" = {
       source = pkgs.mkSecret ../../../secrets/etc/NetworkManager/system-connections/1-RHVPN.ovpn;
