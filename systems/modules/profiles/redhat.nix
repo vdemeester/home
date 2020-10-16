9069cf7154824ad6d62083976f946b2639ff5887
Vincent Demeester <vincent@sbr.pm>
2020-10-16 15:40:28 +0200

    systems: add libnotify to redhat profile
    Signed-off-by: Vincent Demeester <vincent@sbr.pm>

--- systems/modules/profiles/redhat.nix
+++ systems/modules/profiles/redhat.nix
@@ -11,7 +11,7 @@ in
     };
   };
   config = mkIf cfg.enable {
-    environment.systemPackages = with pkgs; [ krb5 google-chrome ];
+    environment.systemPackages = with pkgs; [ krb5 google-chrome libnotify ];
     # NetworkManager
     environment.etc."NetworkManager/system-connections/1-RHVPN.ovpn" = {
       source = pkgs.mkSecret ../../../secrets/etc/NetworkManager/system-connections/1-RHVPN.ovpn;
