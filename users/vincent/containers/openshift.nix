888de1394e89fcaf53ac446b83c04e7cdf0e45cf
Vincent Demeester <vincent@sbr.pm>
2020-10-16 15:40:39 +0200

    systems: re-enable oc and openshift-install
    Signed-off-by: Vincent Demeester <vincent@sbr.pm>

--- users/vincent/containers/openshift.nix
+++ users/vincent/containers/openshift.nix
@@ -4,8 +4,8 @@
   home.file.".local/share/applications/chos4.desktop".source = ./chos4.desktop;
   home.packages = with pkgs; [
     #my.crc
-    #my.oc
-    #my.openshift-install
+    my.oc
+    my.openshift-install
     #my.operator-sdk
   ];
 }
