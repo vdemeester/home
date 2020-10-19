e22974450257c3f1339f9d9f019b9046b6e00265
Vincent Demeester <vincent@sbr.pm>
2020-10-19 16:23:53 +0200

    users/vincent: enable spotifyd…
    … and add spotify-tui
    
    Signed-off-by: Vincent Demeester <vincent@sbr.pm>

--- users/vincent/desktop/spotify.nix
+++ users/vincent/desktop/spotify.nix
@@ -1,5 +1,17 @@
 { pkgs, ... }:
 
 {
-  home.packages = with pkgs; [ spotify ];
+  home.packages = with pkgs; [ spotify spotify-tui ];
+  services.spotifyd = {
+    enable = true;
+    package = (pkgs.spotifyd.override { withKeyring = true; withPulseAudio = true; withMpris = true; });
+    settings = {
+      global = {
+        username = "vdemeester";
+        use_keyring = "true";
+        backend = "pulseaudio";
+        device_name = "naruhodo"; # FIXME(vdemeester) change this
+      };
+    };
+  };
 }
