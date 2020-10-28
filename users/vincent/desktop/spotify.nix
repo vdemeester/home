d8a0ce9903be70b07313a420c95148dd456d4072
Vincent Demeester <vincent@sbr.pm>
2020-10-28 11:50:17 +0100

    users/vincent: remove spotifyd…
    … let's use the UI from time to time. But most of my time, I use my
    phone or a table for spotify so…
    
    Signed-off-by: Vincent Demeester <vincent@sbr.pm>

--- users/vincent/desktop/spotify.nix
+++ users/vincent/desktop/spotify.nix
@@ -1,17 +1,5 @@
 { pkgs, ... }:
 
 {
-  home.packages = with pkgs; [ spotify spotify-tui ];
-  services.spotifyd = {
-    enable = true;
-    package = (pkgs.spotifyd.override { withKeyring = true; withPulseAudio = true; withMpris = true; });
-    settings = {
-      global = {
-        username = "vdemeester";
-        use_keyring = "true";
-        backend = "pulseaudio";
-        device_name = "naruhodo"; # FIXME(vdemeester) change this
-      };
-    };
-  };
+  home.packages = with pkgs; [ spotify ];
 }
