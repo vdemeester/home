f52beb29aae448940720e8f1aa10ed79abdf7073
Vincent Demeester <vincent@sbr.pm>
2020-11-17 18:08:22 +0100

    users/vincent: add openshift-pipelines mr file
    Signed-off-by: Vincent Demeester <vincent@sbr.pm>

--- users/vincent/dev/mr.nix
+++ users/vincent/dev/mr.nix
@@ -8,6 +8,7 @@
   home.file."src/knative.dev/.mrconfig".source = ./mr/src.knative.dev.mr;
   home.file."src/github.com/.mrconfig".source = ./mr/src.github.mr;
   home.file."src/github.com/openshift/.mrconfig".source = ./mr/src.github.openshift.mr;
+  home.file."src/github.com/openshift-pipelines/.mrconfig".source = ./mr/src.github.openshift-pipelines.mr;
   home.file."src/github.com/tektoncd/.mrconfig".source = ./mr/src.github.tektoncd.mr;
   home.file."src/pkgs.devel.redhat.com/.mrconfig".source = ./mr/src.pkgs.devel.redhat.mr;
 }
