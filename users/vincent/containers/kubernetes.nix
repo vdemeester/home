e7fcf4103d4853115066f8f340116bb625ae2cf2
Vincent Demeester <vincent@sbr.pm>
2020-11-20 11:13:51 +0100

    users/vincent: add command to kubernetes
    - knd to delete namespaces
    
    Signed-off-by: Vincent Demeester <vincent@sbr.pm>

--- users/vincent/containers/kubernetes.nix
+++ users/vincent/containers/kubernetes.nix
@@ -1,5 +1,12 @@
-{ pkgs, ... }:
+{ config, lib, pkgs, ... }:
 
+with lib;
+let
+  knd = pkgs.writeScriptBin "knd" ''
+    #!${pkgs.stdenv.shell}
+    ${pkgs.kubectl}/bin/kubectl get namespaces -o name | ${pkgs.fzf}/bin/fzf --multi | xargs kubectl delete
+  '';
+in
 {
   home.packages = with pkgs; [
     #cri-tools
@@ -12,5 +19,7 @@
     my.ko
     my.krew
     my.kss
+    # our own scripts
+    knd
   ];
 }
