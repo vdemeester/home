57828534cc9bceec3f401ca664107d3603821b3e
Vincent Demeester <vincent@sbr.pm>
2020-12-04 18:12:46 +0100

    pkgs/buildkit: 0.7.1 -> 0.8.0
    Signed-off-by: Vincent Demeester <vincent@sbr.pm>

--- pkgs/buildkit/default.nix
+++ pkgs/buildkit/default.nix
@@ -1,12 +1,13 @@
-{ stdenv, lib, fetchFromGitHub, buildGoPackage }:
+{ stdenv, lib, fetchFromGitHub, buildGoModule }:
 
-buildGoPackage rec {
+buildGoModule rec {
   name = "buildkit-${version}";
-  version = "0.7.1";
+  version = "0.8.0";
   rev = "v${version}";
 
   goPackagePath = "github.com/moby/buildkit";
   subPackages = [ "cmd/buildctl" "cmd/buildkitd" ];
+
   buildFlagsArray = let t = "${goPackagePath}/version"; in
     ''
       -ldflags=
@@ -17,8 +18,10 @@ buildGoPackage rec {
     inherit rev;
     owner = "moby";
     repo = "buildkit";
-    sha256 = "048h69ffgmpir2ix9ldi6zrzlwxa5yz3idg5ajspz2dihmzmnwws";
+    sha256 = "0qcgq93wj77i912xqhwrzkzaqz608ilczfn5kcsrf9jk2m1gnx7m";
   };
+  vendorSha256 = null;
+  doCheck = false;
 
   meta = {
     description = "concurrent, cache-efficient, and Dockerfile-agnostic builder toolkit";
