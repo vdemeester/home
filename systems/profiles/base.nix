59d483408f7ee6e974b39ab55a856ea57e433160
Vincent Demeester <vincent@sbr.pm>
2020-12-16 17:11:33 +0100

    systems: fix profiles/base.nix
    for flakes.
    
    Signed-off-by: Vincent Demeester <vincent@sbr.pm>

--- systems/profiles/base.nix
+++ systems/profiles/base.nix
@@ -1,14 +1,15 @@
 { config, lib, pkgs, ... }:
 let
-  inherit (lib) mkEnableOption;
+  inherit (lib) mkEnableOption mkIf;
   cfg = config.profiles.base;
 in
 {
   options = {
-    enable = mkEnableOption "base configuration";
+    config.profiles.base = {
+      enable = mkEnableOption "base configuration";
+    };
   };
-  config = {
-
+  config = mkIf cfg.enable {
     # `nix-daemon` will hit the stack limit when using `nixFlakes`.
     systemd.services.nix-daemon.serviceConfig."LimitSTACK" = "infinity";
   };
