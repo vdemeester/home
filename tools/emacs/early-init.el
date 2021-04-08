8ffacf443a7ee6eba1541fe47e539399c39d1510
Vincent Demeester <vincent@sbr.pm>
2021-04-08 16:07:11 +0200

    Emacs: remove theme customization 🙃
    Signed-off-by: Vincent Demeester <vincent@sbr.pm>

--- tools/emacs/early-init.el
+++ tools/emacs/early-init.el
@@ -54,16 +54,17 @@ This is used internally by `vde/modus-themes-toggle'."
 
 (defun vde/modus-operandi-custom ()
   "Customize modus-operandi theme"
-  (if (member 'modus-operandi custom-enabled-themes)
-      (modus-operandi-theme-with-color-variables ; this macro allows us to access the colour palette
-        (custom-theme-set-faces
-         'modus-operandi
-         `(whitespace-tab ((,class (:background "#ffffff" :foreground "#cccccc"))))
-         `(whitespace-space ((,class (:background "#ffffff" :foreground "#cccccc"))))
-         `(whitespace-hspace ((,class (:background "#ffffff" :foreground "#cccccc"))))
-         `(whitespace-newline ((,class (:background "#ffffff" :foreground "#cccccc"))))
-         `(whitespace-indentation ((,class (:background "#ffffff" :foreground "#cccccc"))))
-         ))))
+  ;; (if (member 'modus-operandi custom-enabled-themes)
+  ;;     (modus-operandi-theme-with-color-variables ; this macro allows us to access the colour palette
+  ;;      (custom-theme-set-faces
+  ;;       'modus-operandi
+  ;;       `(whitespace-tab ((,class (:background "#ffffff" :foreground "#cccccc"))))
+  ;;       `(whitespace-space ((,class (:background "#ffffff" :foreground "#cccccc"))))
+  ;;       `(whitespace-hspace ((,class (:background "#ffffff" :foreground "#cccccc"))))
+  ;;       `(whitespace-newline ((,class (:background "#ffffff" :foreground "#cccccc"))))
+  ;;       `(whitespace-indentation ((,class (:background "#ffffff" :foreground "#cccccc"))))
+  ;;       )))
+  )
 
 (add-hook 'contrib/after-load-theme-hook 'vde/modus-operandi-custom)
 (vde/modus-operandi)
