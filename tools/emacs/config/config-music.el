25d4aa87c2bb728bbc016c1ffc5426b0d15dffe2
Vincent Demeester <vincent@sbr.pm>
2021-04-21 12:06:35 +0200

    emacs: add m4e to bongo audio file names
    Signed-off-by: Vincent Demeester <vincent@sbr.pm>

--- tools/emacs/config/config-music.el
+++ tools/emacs/config/config-music.el
@@ -4,7 +4,9 @@
 ;;; Code:
 
 (use-package bongo
-  :commands (bongo bongo-show))
+  :commands (bongo bongo-show)
+  :config
+  (setq-default bongo-audio-file-name-extensions '("669" "aac" "amf" "apun" "au" "dsm" "far" "flac" "g18" "g36" "gdm" "imf" "it" "mdz" "med" "mid" "midi" "mka" "mod" "m4a" "mp2" "mp3" "mtm" "ogg" "okt" "r36" "ra" "rcp" "rmi" "s3m" "spx" "stm" "stx" "ult" "umx" "uni" "vqf" "wav" "wma" "xm")))
 
 (provide 'config-music)
 ;;; config-music ends here
