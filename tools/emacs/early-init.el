28683536ad708f49b6355ce847e571f02e85fbf9
Vincent Demeester <vincent@sbr.pm>
2021-04-23 18:45:46 +0200

    emacs: update early-init with comments.
    Signed-off-by: Vincent Demeester <vincent@sbr.pm>

--- tools/emacs/early-init.el
+++ tools/emacs/early-init.el
@@ -1,7 +1,47 @@
-;;; early-init.el -*- lexical-binding: t; -*-
+;;; early-init.el --- Early init configuration file -*- lexical-binding: t; -*-
 
+;; Copyright (c) 2020-2021  Vincent Demeester <vincent@sbr.pm>
+
+;; Author: Vincent Demeester <vincent@sbr.pm>
+;; URL: https://git.sr.ht/~vdemeester/home
+;; Version: 0.1.0
+;; Package-Requires: ((emacs "27.1"))
+
+;; This file is NOT part of GNU Emacs.
+
+;; This file is free software: you can redistribute it and/or modify it
+;; under the terms of the GNU General Public License as published by the
+;; Free Software Foundation, either version 3 of the License, or (at
+;; your option) any later version.
+;;
+;; This file is distributed in the hope that it will be useful, but
+;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+;; General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with this file.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; Prior to Emacs 27, the `init.el' was supposed to handle the
+;; initialisation of the package manager, by means of calling
+;; `package-initialize'.  Starting with Emacs 27, the default
+;; behaviour is to start the package manager before loading the init
+;; file.
+;;
+
+;; See my dotfiles: https://git.sr.ht/~vdemeester/home
+
+;;; Code:
+
+;; Do not initialize installed packages
 (setq package-enable-at-startup nil)
+
+;; Do not resize the frame at this early stage
 (setq frame-inhibit-implied-resize t)
+
+;; Disable GUI elements
 (menu-bar-mode -1)
 (tool-bar-mode -1)
 (scroll-bar-mode -1)
