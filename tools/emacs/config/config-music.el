2fb58d8acd79a5ea7382c46f801bdd05f36493eb
Vincent Demeester <vincent@sbr.pm>
2021-07-05 11:50:35 +0200

    emacs: add support for m4a and opus (with mpv)
    Signed-off-by: Vincent Demeester <vincent@sbr.pm>

--- tools/emacs/config/config-music.el
+++ tools/emacs/config/config-music.el
@@ -6,7 +6,10 @@
 (use-package bongo
   :commands (bongo bongo-show)
   :config
-  (setq-default bongo-audio-file-name-extensions '("669" "aac" "amf" "apun" "au" "dsm" "far" "flac" "g18" "g36" "gdm" "imf" "it" "mdz" "med" "mid" "midi" "mka" "mod" "m4a" "mp2" "mp3" "mtm" "ogg" "okt" "r36" "ra" "rcp" "rmi" "s3m" "spx" "stm" "stx" "ult" "umx" "uni" "vqf" "wav" "wma" "xm")))
+  (setq-default bongo-audio-file-name-extensions '("669" "aac" "amf" "apun" "au" "dsm" "far" "flac" "g18" "g36" "gdm" "imf" "it" "mdz" "med" "mid" "midi" "mka" "mod" "m4a" "mp2" "mp3" "mtm" "ogg" "okt" "r36" "ra" "rcp" "rmi" "s3m" "spx" "stm" "stx" "ult" "umx" "uni" "vqf" "wav" "wma" "xm"))
+  (setq-default bongo-custom-backend-matchers
+                '((mpv local-file "m4a")
+                  (mpv local-file "opus"))))
 
 (provide 'config-music)
 ;;; config-music ends here
