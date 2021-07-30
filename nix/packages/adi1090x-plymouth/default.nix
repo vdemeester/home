f2b3fd5fc74852b4825cc4f1c923e24a53ff1124
Vincent Demeester <vincent@sbr.pm>
2021-07-30 16:54:22 +0200

    nix/packages: copy only a subset of plymouth themes
    Signed-off-by: Vincent Demeester <vincent@sbr.pm>

--- nix/packages/adi1090x-plymouth/default.nix
+++ nix/packages/adi1090x-plymouth/default.nix
@@ -20,10 +20,9 @@ pkgs.stdenv.mkDerivation rec {
   '';
 
   installPhase = ''
-    cp -r pack_1/* $out/share/plymouth/themes
-    cp -r pack_2/* $out/share/plymouth/themes
-    cp -r pack_3/* $out/share/plymouth/themes
-    cp -r pack_4/* $out/share/plymouth/themes
+    cp -r pack_1/cuts $out/share/plymouth/themes
+    cp -r pack_2/{hexagon,green_loader,deus_ex} $out/share/plymouth/themes
+    cp -r pack_4/{spinner_alt,sphere} $out/share/plymouth/themes
     for p in $out/share/plymouth/themes/*; do
       theme=$(basename $p)
       sed -i "s@\/usr\/@$out\/@" $out/share/plymouth/themes/$theme/$theme.plymouth
