bb5d18faeb20f1e67f852c26f74d9bff004072f0
Vincent Demeester <vincent@sbr.pm>
2021-08-03 09:12:39 +0200

    tools/emacs: add support for opus in bongo…
    … playlist.. the backend is already configured but it was not in the
    file-name-extensions list allowed in playlists.
    
    Signed-off-by: Vincent Demeester <vincent@sbr.pm>

--- tools/emacs/config/config-music.el
+++ tools/emacs/config/config-music.el
@@ -16,8 +16,8 @@
   (setq-default bongo-audio-file-name-extensions
                 '("669" "aac" "amf" "apun" "au" "dsm" "far" "flac" "g18" "g36" "gdm"
                   "imf" "it" "mdz" "med" "mid" "midi" "mka" "mod" "m4a" "mp2" "mp3"
-                  "mtm" "ogg" "okt" "r36" "ra" "rcp" "rmi" "s3m" "spx" "stm" "stx"
-                  "ult" "umx" "uni" "vqf" "wav" "wma" "xm")
+                  "mtm" "ogg" "opus" "okt" "r36" "ra" "rcp" "rmi" "s3m" "spx" "stm"
+                  "stx" "ult" "umx" "uni" "vqf" "wav" "wma" "xm")
                 bongo-custom-backend-matchers
                 '((mpv local-file "m4a")
                   (mpv local-file "opus"))
