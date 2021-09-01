12a3da0bfdf80c4c8c29bcb73a55478595789d41
Vincent Demeester <vincent@sbr.pm>
2021-09-01 17:25:05 +0200

    Remove *.flake.nix …
    It is "history" for now
    
    Signed-off-by: Vincent Demeester <vincent@sbr.pm>

--- systems/modules/virtualisation/default.flake.nix
+++
@@ -1,9 +0,0 @@
-# Virtualisation is grouping modules related to virtualisation, such
-# as containers (podman, docker, …), vm (qemu, libvirt, …).
-{ lib, ... }:
-
-{
-  imports = [
-    ./libvirt.nix
-  ];
-}
