bdcc7c377bfb5113499309347f5fce17995399b4
Vincent Demeester <vincent@sbr.pm>
2021-09-15 17:30:27 +0200

    users/vincent: update dev mrconfig(s)
    Signed-off-by: Vincent Demeester <vincent@sbr.pm>

--- users/vincent/dev/mr.nix
+++ users/vincent/dev/mr.nix
@@ -7,6 +7,7 @@
   home.file."src/go.sbr.pm/.mrconfig".source = ./mr/src.go.sbr.pm.mr;
   home.file."src/k8s.io/.mrconfig".source = ./mr/src.k8s.io.mr;
   home.file."src/knative.dev/.mrconfig".source = ./mr/src.knative.dev.mr;
+  home.file."src/osp/forks/.mrconfig".source = ./mr/src.osp.forks.mr;
   home.file."src/osp/.mrconfig".source = ./mr/src.osp.mr;
   # Old setup, migrate this slowly
   home.file."src/github.com/.mrconfig".source = ./mr/src.github.mr;
