9beef75ab3672735128ccd0baf464854cbec7f04
Vincent Demeester <vincent@sbr.pm>
2021-09-15 16:29:34 +0200

    users/vincent: rework =mr= a bit
    Signed-off-by: Vincent Demeester <vincent@sbr.pm>

--- users/vincent/dev/mr.nix
+++ users/vincent/dev/mr.nix
@@ -7,8 +7,7 @@
   home.file."src/go.sbr.pm/.mrconfig".source = ./mr/src.go.sbr.pm.mr;
   home.file."src/k8s.io/.mrconfig".source = ./mr/src.k8s.io.mr;
   home.file."src/knative.dev/.mrconfig".source = ./mr/src.knative.dev.mr;
+  home.file."src/osp/.mrconfig".source = ./mr/src.osp.mr;
+  # Old setup, migrate this slowly
   home.file."src/github.com/.mrconfig".source = ./mr/src.github.mr;
-  home.file."src/github.com/openshift/.mrconfig".source = ./mr/src.github.openshift.mr;
-  home.file."src/github.com/openshift-pipelines/.mrconfig".source = ./mr/src.github.openshift-pipelines.mr;
-  home.file."src/pkgs.devel.redhat.com/.mrconfig".source = ./mr/src.pkgs.devel.redhat.mr;
 }
