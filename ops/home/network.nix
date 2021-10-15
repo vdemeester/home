8ade46283b99ba634617acb63d75993950e42d95
Vincent Demeester <vincent@sbr.pm>
2021-10-15 11:00:41 +0200

    ops: clean home network
    - Remove hokkaido
    - Add some tags to "bare metal" hosts
    - Add a simple healthChecks on k8sn1, probably more to come, it's fun
    
    Signed-off-by: Vincent Demeester <vincent@sbr.pm>

--- ops/home/network.nix
+++ ops/home/network.nix
@@ -13,6 +13,12 @@ in
     deployment.targetHost = "192.168.1.130";
     # deployment.targetHost = "k8sn1.home";
     deployment.tags = [ "kubernetes" "master" ];
+    deployment.healthChecks = {
+      cmd = [{
+        cmd = [ "kubectl" "--kubeconfig=/etc/kubernetes/cluster-admin.kubeconfig" "version" ];
+        description = "Validating that kubectl is available and a cluster is running";
+      }];
+    };
     imports = [ ../../systems/hosts/k8sn1.nix ];
   };
   "k8sn2" = { config, pkgs, lib, ... }: {
@@ -32,21 +38,19 @@ in
   "wakasu" = { config, pkgs, lib, ... }: {
     deployment.targetUser = "root";
     deployment.targetHost = "wakasu.home";
+    deployment.tags = [ "baremetal" "wakasu" ];
     imports = [ ../../systems/hosts/wakasu.nix ];
   };
   "sakhalin" = { config, pkgs, lib, ... }: {
     deployment.targetUser = "root";
     deployment.targetHost = "sakhalin.home";
+    deployment.tags = [ "baremetal" "sakhalin" ];
     imports = [ ../../systems/hosts/sakhalin.nix ];
   };
   "okinawa" = { config, pkgs, lib, ... }: {
     deployment.targetUser = "root";
     deployment.targetHost = "okinawa.home";
+    deployment.tags = [ "baremetal" "okinawa" ];
     imports = [ ../../systems/hosts/okinawa.nix ];
-  };
-  "hokkaido" = { config, pkgs, lib, ... }: {
-    deployment.targetUser = "root";
-    deployment.targetHost = "hokkaido.home";
-    imports = [ ../../systems/hosts/hokkaido.nix ];
   };
 }
