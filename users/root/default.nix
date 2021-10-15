9eee282f90c8bbdf281e820f1114f0a837284277
Vincent Demeester <vincent@sbr.pm>
2021-10-15 15:57:50 +0200

    users: remove home.nix
    Signed-off-by: Vincent Demeester <vincent@sbr.pm>

--- users/root/default.nix
+++ users/root/default.nix
@@ -21,10 +21,9 @@ in
     [
       (import ../vincent/core/zsh.nix)
       (import ../vincent/core/ssh.nix)
-      (import ./home.nix)
     ]
     ++ optionals (versionOlder config.system.nixos.release "21.11") [{
-      # FIXME manpages are broken on 21.05 and home-manager (for some reason..)
+      # manpages are broken on 21.05 and home-manager (for some reason..)
       manual.manpages.enable = false;
     }]
   );
