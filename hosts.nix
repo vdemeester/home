3de09f14d194f501348d8cf12002dce7c5817a9f
Vincent Demeester <vincent@sbr.pm>
2021-10-22 18:59:31 +0200

    clean hosts.nix
    Signed-off-by: Vincent Demeester <vincent@sbr.pm>

--- hosts.nix
+++ hosts.nix
@@ -1,11 +1,4 @@
 {
   naruhodo = { arch = "x86_64-linux"; type = "unstable"; };
   aomi = { arch = "x86_64-linux"; type = "unstable"; };
-  # servers
-  # FIXME(vdemeester) move this away
-  hokkaido = { arch = "x86_64-linux"; };
-  wakasu = { arch = "x86_64-linux"; };
-  kerkouane = { arch = "x86_64-linux"; };
-  # okinawa = { arch = "x86_64-linux"; }; # okinawa died
-  sakhalin = { arch = "x86_64-linux"; };
 }
