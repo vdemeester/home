26358247114a1572e4043bc365ac967e0c276b73
Vincent Demeester <vincent@sbr.pm>
2021-11-18 10:39:49 +0100

    systems/modules/core: add experimental-features configuration
    Signed-off-by: Vincent Demeester <vincent@sbr.pm>

--- systems/modules/core/nix.nix
+++ systems/modules/core/nix.nix
@@ -55,14 +55,13 @@ in
       daemonIONiceLevel = 5;
       daemonNiceLevel = 10;
       # if hydra is down, don't wait forever
-      # package = pkgs.nixFlakes;
-      # experimental-features = flakes nix-command
       extraOptions = ''
         connect-timeout = 20
         build-cores = 0
         keep-outputs = true
         keep-derivations = true
         builders-use-substitutes = true
+        experimental-features = flakes nix-command
       '';
       gc = {
         automatic = true;
