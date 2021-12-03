393a8ad6095a05e973a370bf69a538f29dcd0a7e
Vincent Demeester <vincent@sbr.pm>
2021-12-03 12:26:55 +0100

    nix/packages: manifest-tools unstable -> 2.0.0-rc.1
    Signed-off-by: Vincent Demeester <vincent@sbr.pm>

--- nix/packages/manifest-tool/default.nix
+++ nix/packages/manifest-tool/default.nix
@@ -2,23 +2,24 @@
 
 buildGoModule rec {
   name = "manifest-tool-${version}";
-  version = "unstable-2020-10-26";
-  #rev = "v${version}";
-  rev = "bae5531170d45955c2d72d1b29d77ce1b0c9dedb";
+  version = "2.0.0-rc.1";
+  rev = "v${version}";
+  # rev = "bae5531170d45955c2d72d1b29d77ce1b0c9dedb";
 
   subPackages = [ "cmd/manifest-tool" ];
+  modRoot = "./v2";
 
   src = fetchFromGitHub {
     inherit rev;
     owner = "estesp";
     repo = "manifest-tool";
-    sha256 = "066ls7sxacfy236c9kjbhhqsmpbiy8wx4ishyw6nxi4n5zvzs3kl";
+    sha256 = "sha256-IMXeAjFBAtGShsn/EmScIubJ88CZiS9A6VeTp0NV91M=";
   };
   vendorSha256 = null;
 
   meta = {
     description = "";
-    homepage = "https://github.com/estesp/manifest-tool";
+    homepage = https://github.com/estesp/manifest-tool;
     license = lib.licenses.asl20;
     maintainers = with lib.maintainers; [ vdemeester ];
   };
