6ee02bd20ec6244ca79fbeecc9efac815c71ddb5
Vincent Demeester <vincent@sbr.pm>
2022-03-22 11:13:08 +0100

    nix/packages: update sugarjazy to 0.5.1
    Signed-off-by: Vincent Demeester <vincent@sbr.pm>

--- nix/packages/sugarjazy/default.nix
+++ nix/packages/sugarjazy/default.nix
@@ -1,8 +1,8 @@
-{ stdenv, lib, fetchFromGitHub, poetry2nix, python310 }:
+{ stdenv, lib, fetchFromGitHub, poetry2nix, python310, python310Packages }:
 
 poetry2nix.mkPoetryApplication rec {
   pname = "sugarjazy";
-  version = "0.3.0";
+  version = "0.5.1";
   python = python310;
 
   projectDir = ./.;
@@ -10,9 +10,11 @@ poetry2nix.mkPoetryApplication rec {
     owner = "chmouel";
     repo = "sugarjazy";
     rev = "${version}";
-    sha256 = "sha256-stGHznSTsEOmd/tGnO+Tg/FixyKvRkOpv54GTyvmQJg=";
+    sha256 = "sha256-GkfPpCRXta2aDUQT4c/tU+T1b/LoPM+k1GlopvgIpzQ=";
   };
   doCheck = false;
+
+  buildInputs = [ python310Packages.dateutil ];
 
   meta = with lib; {
     homepage = "https://github.com/chmouel/sugarjazy";
