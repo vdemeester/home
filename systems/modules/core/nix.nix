41bf5657c4b4fcb8e35def26a7b878d6be7db4f9
Vincent Demeester <vincent@sbr.pm>
2022-04-06 02:48:28 +0200

    systems/modules: remove nix.cache.home local cache
    It gets more in the way than anything..
    
    Signed-off-by: Vincent Demeester <vincent@sbr.pm>

--- systems/modules/core/nix.nix
+++ systems/modules/core/nix.nix
@@ -31,7 +31,7 @@ in
         '';
       };
       localCaches = mkOption {
-        default = [ "http://nix.cache.home" ];
+        default = [ ];
         description = "List of local nix caches";
         type = types.listOf types.str;
       };
