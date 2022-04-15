a23dd26214698041338ff79821d11ddae3ecb16f
Vincent Demeester <vincent@sbr.pm>
2022-04-15 17:12:15 +0200

    nix/packages: move adi1090x-plymouth to fetchFromGithub
    Signed-off-by: Vincent Demeester <vincent@sbr.pm>
    (cherry picked from commit 5fde030bc3c98b959a7aaf3347d222fbf7777b75)

--- nix/packages/adi1090x-plymouth/default.nix
+++ nix/packages/adi1090x-plymouth/default.nix
@@ -1,11 +1,14 @@
-{ pkgs }:
+{ pkgs, fetchFromGitHub }:
 
 pkgs.stdenv.mkDerivation rec {
   pname = "adi1090x-plymouth";
   version = "0.0.1";
 
-  src = builtins.fetchGit {
-    url = "https://github.com/adi1090x/plymouth-themes";
+  src = fetchFromGitHub {
+    owner = "adi1090x";
+    repo = "plymouth-themes";
+    rev = "bf2f570bee8e84c5c20caac353cbe1d811a4745f";
+    sha256 = "sha256-VNGvA8ujwjpC2rTVZKrXni2GjfiZk7AgAn4ZB4Baj2k=";
   };
 
   buildInputs = [
