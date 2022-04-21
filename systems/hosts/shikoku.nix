34be6a5c5136e283aaa558f4980f382297f0c6b6
Vincent Demeester <vincent@sbr.pm>
2022-04-21 19:06:11 +0200

    systems/shikoku: add HDD1 and enable zfs
    Signed-off-by: Vincent Demeester <vincent@sbr.pm>

--- systems/hosts/shikoku.nix
+++ systems/hosts/shikoku.nix
@@ -20,6 +20,9 @@ in
     (import ../../users).root
   ];
 
+  boot.supportedFilesystems = [ "zfs" ];
+  networking.hostId = "$(head -c 8 /etc/machine-id)";
+
   networking = {
     bridges.br1.interfaces = [ "enp0s31f6" ];
     firewall.enable = false; # we are in safe territory :D
@@ -54,21 +57,32 @@ in
   # TODO: check if it's done elsewhere
   hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 
-  fileSystems."/" =
-    {
-      device = "/dev/disk/by-uuid/73fd8864-f6af-4fdd-b826-0dfdeacd3c19";
-      fsType = "ext4";
-      options = [ "noatime" "discard" ];
-    };
+  fileSystems."/" = {
+    device = "/dev/disk/by-uuid/73fd8864-f6af-4fdd-b826-0dfdeacd3c19";
+    fsType = "ext4";
+    options = [ "noatime" "discard" ];
+  };
 
-  fileSystems."/boot" =
-    {
-      device = "/dev/disk/by-uuid/829D-BFD1";
-      fsType = "vfat";
-    };
+  fileSystems."/boot" = {
+    device = "/dev/disk/by-uuid/829D-BFD1";
+    fsType = "vfat";
+  };
 
-  swapDevices =
-    [{ device = "/dev/disk/by-uuid/a9ec44e6-0c1d-4f60-9f5c-81a7eaa8e8fd"; }];
+  # Extra data
+  # HDD:   b58e59a4-92e7-4278-97ba-6fe361913f50
+  filesystems."/data" = {
+    device = "/dev/disk/by-uuid/b58e59a4-92e7-4278-97ba-6fe361913f50";
+    fsType = "ext4";
+    options = [ "noatime" ];
+  };
+  # ZFS Pool
+  # SSD1:  469077df-049f-4f5d-a34f-1f5449d782ec
+  # SSD2:  e11a3b63-791c-418b-9f4b-5ae0199f1f97
+  # NVME2: 3d2dff80-f2b1-4c48-8e76-12b01fdf4137
+
+  swapDevices = [{
+    device = "/dev/disk/by-uuid/a9ec44e6-0c1d-4f60-9f5c-81a7eaa8e8fd";
+  }];
 
   profiles = {
     home = true;
