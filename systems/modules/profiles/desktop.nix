88fd857013ce1a1b7778cd6b56ae9fcdd07ba5a9
Vincent Demeester <vincent@sbr.pm>
2022-06-02 10:24:25 +0200

    profiles/desktop: use fullpath for nmcli and grep
    Signed-off-by: Vincent Demeester <vincent@sbr.pm>

--- systems/modules/profiles/desktop.nix
+++ systems/modules/profiles/desktop.nix
@@ -71,11 +71,11 @@ in
 
           enable_disable_wifi ()
           {
-              result=$(nmcli dev | grep "ethernet" | grep -w "connected")
+              result=$(${pkgs.networkmanager}/bin/nmcli dev | ${pkgs.gnugrep}/bin/grep "ethernet" | ${pkgs.gnugrep}/bin/grep -w "connected")
               if [ -n "$result" ]; then
-                  nmcli radio wifi off
+                  ${pkgs.networkmanager}/bin/nmcli radio wifi off
               else
-                  nmcli radio wifi on
+                  ${pkgs.networkmanager}/bin/nmcli radio wifi on
               fi
           }
 
