8b11ccf33a6dc1d0c065ebcfe61ae111d878c5cf
Vincent Demeester <vincent@sbr.pm>
2022-07-18 11:50:00 +0200

    nix/packages: batzconverter 1.2.1 -> 2.0.0
    Signed-off-by: Vincent Demeester <vincent@sbr.pm>

--- nix/packages/batzconverter/default.nix
+++ nix/packages/batzconverter/default.nix
@@ -2,14 +2,14 @@
 
 stdenv.mkDerivation rec {
   pname = "batzconverter";
-  version = "1.2.1";
+  version = "2.0.0";
   name = "${pname}-${version}";
 
   src = fetchFromGitHub {
     owner = "chmouel";
     repo = "batzconverter";
     rev = "${version}";
-    sha256 = "sha256-YAcCtek4pYvhgUaS8EZT0kFBLZQOFPCaxKh8bNr+ru4=";
+    sha256 = "sha256-z6ue3cGX0NyUVauZF3X5688Rocuk9gCETUsZA6PYyT0=";
   };
 
   installPhase = ''
