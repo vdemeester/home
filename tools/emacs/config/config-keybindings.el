b549cc568bb1e821e51feef733851e0f1f7dcdc3
Vincent Demeester <vincent@sbr.pm>
2022-08-17 16:40:01 +0200

    tools/emacs: remap dabbrev-expand to hippie-expand
    Signed-off-by: Vincent Demeester <vincent@sbr.pm>

--- tools/emacs/config/config-keybindings.el
+++ tools/emacs/config/config-keybindings.el
@@ -6,5 +6,9 @@
 ;; Disable C-x C-n to avoid the disabled command buffer
 (unbind-key "C-x C-n" global-map)
 
+;; Remap dynamic-abbrev to hippie-expand
+;; See https://www.masteringemacs.org/article/text-expansion-hippie-expand
+(global-set-key [remap dabbrev-expand] 'hippie-expand)
+
 (provide 'config-keybindings)
 ;;; config-keybindings.el ends here
