a511980bf9044898f9b7644f60d057f50e26aa8c
Vincent Demeester <vincent@sbr.pm>
2022-08-19 10:05:12 +0000

    systems/modules: modules.hardware.laptop takes over
    Signed-off-by: Vincent Demeester <vincent@sbr.pm>

--- systems/modules/profiles/laptop.nix
+++ systems/modules/profiles/laptop.nix
@@ -11,23 +11,8 @@ in
     };
   };
   config = mkIf cfg.enable {
-    boot.kernel.sysctl = {
-      "vm.swappiness" = 10;
-      "vm.dirty_ratio" = 25;
-      "vm.dirty_background_ratio" = 10;
-      "vm.dirty_writeback_centisecs" = 5000;
-      "vm.dirty_expire_centisecs" = 5000;
-    };
-    environment.systemPackages = with pkgs; [
-      lm_sensors
-      powertop
-      acpi
-    ];
-    systemd.services.nix-gc.unitConfig.ConditionACPower = true;
-    services = {
-      logind.extraConfig = ''
-        HandleLidSwitchDocked=ignore
-      '';
-    };
+    warnings = [ "The option 'profiles.laptop' is deprecated, use modules.hardware.laptop instead" ];
+    # Use modules.hardware.enable instead
+    modules.hardware.laptop.enable = true;
   };
 }
