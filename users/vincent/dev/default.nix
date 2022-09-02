287ead3a0e3edf7ba5a8fc54cc777b88bfe2e483
Vincent Demeester <vincent@sbr.pm>
2022-09-02 18:22:07 +0200

    users/vincent: remove dev/src unused folder.
    All is either inlined or elsewhere.
    
    Signed-off-by: Vincent Demeester <vincent@sbr.pm>

--- users/vincent/dev/default.nix
+++ users/vincent/dev/default.nix
@@ -42,9 +42,13 @@
 
   # FIXME move this to a "work.redhat" configuration.
   # Upstream Tekton
-  home.file."src/tektoncd/.envrc".source = ./src/tektoncd.envrc;
+  home.file."src/tektoncd/.envrc".text = ''
+    eval "$(lorri direnv)"
+  '';
   home.file."src/tektoncd/shell.nix".source = ../../../shells/tekton.nix;
   # Downstream OSP
-  home.file."src/osp/.envrc".source = ./src/osp.envrc;
+  home.file."src/osp/.envrc".text = ''
+    eval "$(lorri direnv)"
+  '';
   home.file."src/osp/shell.nix".source = ../../../shells/osp.nix;
 }
--- users/vincent/dev/src/tektoncd.shell.nix
+++
@@ -1,6 +0,0 @@
-{ system ? builtins.currentSystem }:
-
-# Use flake.nix devshell, similar to "nix develop"
-(builtins.getFlake (toString /home/vincent/src/home)).devShells.${system}.tekton
-
-
