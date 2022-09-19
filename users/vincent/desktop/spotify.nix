52fc7af70acd4f73a91fa135b6054205a88d1d2d
Vincent Demeester <vincent@sbr.pm>
2022-09-19 15:57:22 +0200

    users/vincent: wrap spotify to support wayland
    Signed-off-by: Vincent Demeester <vincent@sbr.pm>

--- users/vincent/desktop/spotify.nix
+++ users/vincent/desktop/spotify.nix
@@ -1,5 +1,12 @@
 { pkgs, ... }:
 
+let
+  my-spotify = pkgs.writeShellScriptBin "spotify" ''
+    exec ${pkgs.spotify}/bin/spotify --enable-features=UseOzonePlatform --ozone-platform=wayland
+  '';
+in
 {
-  home.packages = with pkgs; [ spotify ];
+  home.packages = [
+    my-spotify
+  ];
 }
