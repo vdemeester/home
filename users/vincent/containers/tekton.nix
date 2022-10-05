035ebb858ee594d568b495fabcddd0dc75fe69c0
Vincent Demeester <vincent@sbr.pm>
2022-10-05 14:43:48 +0200

    users/vincent: use upstream tektoncd/cli package
    Signed-off-by: Vincent Demeester <vincent@sbr.pm>

--- users/vincent/containers/tekton.nix
+++ users/vincent/containers/tekton.nix
@@ -2,11 +2,11 @@
 
 {
   home.packages = with pkgs; [
-    # tektoncd-cli
-    my.tkn
+    tektoncd-cli
+    # my.tkn
     my.tkn-pac
     my.tkn-local
-    my.sugarjazy
+    # my.sugarjazy
     rekor-cli
   ];
 }
