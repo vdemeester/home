e70e8de61a29928de7d68e2e06d7b6f1f000a8c0
Vincent Demeester <vincent@sbr.pm>
2022-10-05 14:43:35 +0200

    systems/modules: migrate nix to new config
    Signed-off-by: Vincent Demeester <vincent@sbr.pm>

--- systems/modules/core/nix.nix
+++ systems/modules/core/nix.nix
@@ -41,17 +41,19 @@ in
     environment.systemPackages = [ pkgs.git ];
     nix = {
       allowedUsers = [ "@wheel" ];
-      binaryCaches = cfg.localCaches ++ [
-        "https://cache.nixos.org/"
-        "https://r-ryantm.cachix.org"
-        "https://shortbrain.cachix.org"
-      ];
-      binaryCachePublicKeys = [
-        "r-ryantm.cachix.org-1:gkUbLkouDAyvBdpBX0JOdIiD2/DP1ldF3Z3Y6Gqcc4c="
-        "shortbrain.cachix.org-1:dqXcXzM0yXs3eo9ChmMfmob93eemwNyhTx7wCR4IjeQ="
-        "mic92.cachix.org-1:gi8IhgiT3CYZnJsaW7fxznzTkMUOn1RY4GmXdT/nXYQ="
-      ];
-      buildCores = cfg.buildCores;
+      settings = {
+        cores = cfg.buildCores;
+        substituters = cfg.localCaches ++ [
+          "https://cache.nixos.org/"
+          "https://r-ryantm.cachix.org"
+          "https://shortbrain.cachix.org"
+        ];
+        trusted-public-keys = [
+          "r-ryantm.cachix.org-1:gkUbLkouDAyvBdpBX0JOdIiD2/DP1ldF3Z3Y6Gqcc4c="
+          "shortbrain.cachix.org-1:dqXcXzM0yXs3eo9ChmMfmob93eemwNyhTx7wCR4IjeQ="
+          "mic92.cachix.org-1:gi8IhgiT3CYZnJsaW7fxznzTkMUOn1RY4GmXdT/nXYQ="
+        ];
+      };
       # FIXME handle this depending on the version
       # 21.05 has the following
       # daemonIONiceLevel, daemonNiceLevel
