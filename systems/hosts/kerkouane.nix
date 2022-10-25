559c50e116526c43e4ed8cdb8b65c73677777640
Vincent Demeester <vincent@sbr.pm>
2022-10-25 17:17:01 +0200

    systems/kerkouane: fix indentation
    Signed-off-by: Vincent Demeester <vincent@sbr.pm>

--- systems/hosts/kerkouane.nix
+++ systems/hosts/kerkouane.nix
@@ -223,11 +223,11 @@ in
         locations."/" = {
           proxyPass = "http://127.0.0.1:3333";
           extraConfig = ''
-              proxy_buffering off;
-              proxy_cache off;
-              proxy_set_header Host            $host;
-              proxy_set_header X-Forwarded-For $remote_addr;
-              proxy_set_header Connection "";
+            proxy_buffering off;
+            proxy_cache off;
+            proxy_set_header Host            $host;
+            proxy_set_header X-Forwarded-For $remote_addr;
+            proxy_set_header Connection "";
             proxy_http_version 1.1;
             chunked_transfer_encoding off;
           '';
