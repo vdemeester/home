004cd3a9eac5474871648d80c2874ffebeef8f53
Vincent Demeester <vincent@sbr.pm>
2022-10-31 16:52:09 +0100

    systems/k3s: rying k3s
    Signed-off-by: Vincent Demeester <vincent@sbr.pm>

--- systems/hosts/shikoku.nix
+++ systems/hosts/shikoku.nix
@@ -95,6 +95,15 @@ in
     };
   };
 
+  # Single node k3s
+  networking.firewall.allowedTCPPorts = [ 6443 ];
+  services.k3s.enable = true;
+  services.k3s.role = "server";
+  services.k3s.extraFlags = toString [
+    # "--kubelet-arg=v=4" # Optionally add additional args to k3s
+  ];
+  environment.systemPackages = [ pkgs.k3s ];
+
   programs.ssh.setXAuthLocation = true;
   profiles = {
     bind.enable = true;
