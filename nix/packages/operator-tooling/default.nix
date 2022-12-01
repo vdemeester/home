4fb94a9b1fa680adbe573a3b6a1a8095c3b9d40b
Vincent Demeester <vincent@sbr.pm>
2022-12-01 13:42:10 +0100

    nix/packages: update operator-tooling to 0.0.3…
    … next is to move it to chapeau-rouge
    
    Signed-off-by: Vincent Demeester <vincent@sbr.pm>

--- nix/packages/operator-tooling/default.nix
+++ nix/packages/operator-tooling/default.nix
@@ -2,14 +2,14 @@
 
 buildGoModule rec {
   name = "operator-tool-${version}";
-  version = "0.0.1";
+  version = "0.0.3";
   rev = "v${version}";
 
   src = fetchFromGitHub {
     inherit rev;
     owner = "openshift-pipelines";
     repo = "operator-tooling";
-    sha256 = "sha256-QCtX1fgJ5HVafn3FmiEjah+AF+po3ZQh7kODrjc2On4=";
+    sha256 = "sha256-S2EKdv/Y3ha+wMMK90jr5RPq3+2IJyX63c/xEEvrxrE=";
   };
   vendorSha256 = null;
 
