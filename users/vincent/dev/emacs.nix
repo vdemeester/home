303830380cedb1703799115a28e34fe05ae8af60
Vincent Demeester <vincent@sbr.pm>
2023-03-03 16:15:18 +0100

    users/vincent: fix packages names
    Signed-off-by: Vincent Demeester <vincent@sbr.pm>

--- users/vincent/dev/emacs.nix
+++ users/vincent/dev/emacs.nix
@@ -162,7 +162,7 @@ in
   programs.emacs = {
     enable = true;
     # FIXME: choose depending on the enabled modules
-    package = (pkgs.emacsPgtkNativeComp.override { withGTK3 = true; withGTK2 = false; });
+    package = (pkgs.emacsPgtk.override { withGTK3 = true; withGTK2 = false; });
     # package = (pkgs.emacsNativeComp.override { withGTK3 = true; withGTK2 = false; withX = true; withXwidgets = true; });
     extraPackages = myExtraPackages;
   };
--- users/vincent/dev/python.nix
+++ users/vincent/dev/python.nix
@@ -4,6 +4,6 @@
   home.packages = with pkgs; [
     python3
     pipenv
-    python-language-server
+    python-lsp-server
   ];
 }
