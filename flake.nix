a802a5d3c33f10991ca6bc36dc1a78566bb10662
Vincent Demeester <vincent@sbr.pm>
2023-10-19 18:19:57 +0200

    flake: clean flake and some unused files
    No more flake-utils, flake-utils-plus or flake-parts. I am only
    relying on basic, normal flake. This is all I need for now as I only
    rely on `nixosConfiguration`.
    
    Signed-off-by: Vincent Demeester <vincent@sbr.pm>

--- flake.nix
+++ flake.nix
@@ -4,25 +4,28 @@
   inputs = {
     # Flake for compatibility with non-flake commands
     flake-compat = { type = "github"; owner = "edolstra"; repo = "flake-compat"; flake = false; };
-    flake-utils = { type = "github"; owner = "numtide"; repo = "flake-utils"; };
-    flake-utils-plus = {
-      type = "github";
-      owner = "gytis-ivaskevicius";
-      repo = "flake-utils-plus";
-      ref = "v1.3.1";
-      inputs.flake-utils.follows = "flake-utils";
+
+    devenv = {
+      url = "github:cachix/devenv/latest";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
-    devshell = { type = "github"; owner = "numtide"; repo = "devshell"; };
-    devenv.url = "github:cachix/devenv/latest";
+    hyprland = {
+      url = "github:hyprwm/Hyprland";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
 
 
-    # Flake Dependencies
+    # nixpkgs
+    nixpkgs = { type = "github"; owner = "NixOS"; repo = "nixpkgs"; ref = "nixos-unstable"; };
+    nixpkgs-23_05 = { type = "github"; owner = "NixOS"; repo = "nixpkgs"; ref = "nixos-23.05"; };
+    # Home Manager
     home-manager = { type = "github"; owner = "nix-community"; repo = "home-manager"; inputs.nixpkgs.follows = "nixpkgs"; };
-    home-manager-23_05 = { type = "github"; owner = "nix-community"; repo = "home-manager"; ref = "release-23.05"; inputs.nixpkgs.follows = "nixos-23_05"; };
+    home-manager-23_05 = { type = "github"; owner = "nix-community"; repo = "home-manager"; ref = "release-23.05"; inputs.nixpkgs.follows = "nixpkgs-23_05"; };
+
     impermanence = { type = "github"; owner = "nix-community"; repo = "impermanence"; };
+
     nixpkgs-wayland = { type = "github"; owner = "nix-community"; repo = "nixpkgs-wayland"; inputs.nixpkgs.follows = "nixpkgs"; };
 
-    nur.url = "github:nix-community/NUR";
     emacs-overlay.url = "github:nix-community/emacs-overlay";
 
     sops-nix = {
@@ -40,58 +43,19 @@
 
     # WSL
     nixos-wsl = { type = "github"; owner = "nix-community"; repo = "NixOS-WSL"; inputs.nixpkgs.follows = "nixpkgs"; };
-    nix-hardware = { type = "github"; owner = "NixOS"; "repo" = "nixos-hardware"; };
+    nixos-hardware = { type = "github"; owner = "NixOS"; "repo" = "nixos-hardware"; };
 
     # Red Hat
     chapeau-rouge = {
       type = "github";
       owner = "vdemeester";
       repo = "chapeau-rouge";
-      inputs.flake-utils.follows = "flake-utils";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-
-    # Channels
-    # FIXME: is it needed or should I just alias nixos-unstable instead
-    nixpkgs = { type = "github"; owner = "NixOS"; repo = "nixpkgs"; ref = "nixos-unstable"; };
-    nixos-23_05 = { type = "github"; owner = "NixOS"; repo = "nixpkgs"; ref = "nixos-23.05"; };
-    nixos-unstable = { type = "github"; owner = "NixOS"; repo = "nixpkgs"; ref = "nixos-unstable"; };
-    nixpkgs-unstable = { type = "github"; owner = "NixOS"; repo = "nixpkgs"; ref = "nixpkgs-unstable"; };
   };
 
-  outputs =
-    { self
-    , nixpkgs
-    , flake-utils-plus
-    , flake-utils
-    , home-manager
-    , home-manager-23_05
-    , emacs-overlay
-    , chapeau-rouge
-    , nur
-    , sops-nix
-    , envfs
-    , nixos-wsl
-    , nixos-hardware
-    , devshell
-    , devenv
-    , ...
-    } @ inputs:
+  outputs = { self, ... } @ inputs:
     let
-      mkApp = flake-utils.lib.mkApp;
-
-      nixosModules = flake-utils-plus.lib.exportModules [
-        ./systems/modules/virtualisation/buildkit.nix
-      ];
-
-      stableModules_23_05 = [
-        home-manager-23_05.nixosModules.home-manager
-        ./systems/modules/profiles/docker.nix
-      ];
-      unstableModules = [
-        home-manager.nixosModules.home-manager
-        ./systems/modules/profiles/docker.nix
-      ];
       wayland = { pkgs, config, ... }: {
         config = {
           nix = {
@@ -107,173 +71,122 @@
           nixpkgs.overlays = [ inputs.nixpkgs-wayland.overlay ];
         };
       };
-    in
-    flake-utils-plus.lib.mkFlake {
-      inherit self inputs nixosModules;
-
-      supportedSystems = [ "aarch64-linux" "x86_64-linux" ];
-      channels-config.allowUnfree = true;
-
-      sharedOverlays = [
-        (import ./nix/overlays).tekton
-        (import ./nix/overlays/sbr.nix)
-        emacs-overlay.overlay
-        nur.overlay
-        devshell.overlays.default
-        chapeau-rouge.overlays.openshift
-        (_: prev: {
-          inherit (devenv.packages.${prev.system}) devenv;
-        })
+      stableModules = [
+        inputs.home-manager-23_05.nixosModules.home-manager
       ];
+      unstableModules = [
+        inputs.home-manager.nixosModules.home-manager
+      ];
+      commonModules = [
+        {
+          config.nixpkgs.overlays = [
+            (import ./nix/overlays).tekton
+            (import ./nix/overlays/sbr.nix)
+            inputs.emacs-overlay.overlay
+            # inputs.nur.overlay
+            # inputs.devshell.overlays.default
+            inputs.chapeau-rouge.overlays.openshift
+            (_: prev: {
+              inherit (inputs.devenv.packages.${prev.system}) devenv;
+            })
+          ];
+        }
+        ./systems/modules/core/default.nix
+        ./systems/modules/shell/default.nix
+        ./systems/modules/desktop/default.nix
+        ./systems/modules/dev/default.nix
+        ./systems/modules/editors/default.nix
+        ./systems/modules/hardware/default.nix
+        ./systems/modules/profiles/default.flake.nix # TODO: rename
+        ./systems/modules/virtualisation/default.nix
+        ./systems/modules/virtualisation/buildkit.nix
+        ./systems/modules/services/default.nix
+        # FIXME: migrate this to elsewhere, or at least split it
+        # Profiles probably need to go away
+        ./systems/modules/profiles/base.nix
+        ./systems/modules/profiles/builder.nix
+        ./systems/modules/profiles/desktop.nix
+        ./systems/modules/profiles/dev.nix
+        ./systems/modules/profiles/dns.nix
+        ./systems/modules/profiles/docker.nix
+        ./systems/modules/profiles/git.nix
+        ./systems/modules/profiles/gnome.nix
+        ./systems/modules/profiles/home.nix
+        ./systems/modules/profiles/i18n.nix
+        ./systems/modules/profiles/i3.nix
+        ./systems/modules/profiles/ipfs.nix
+        ./systems/modules/profiles/kubernetes.nix
+        ./systems/modules/profiles/laptop.nix
+        ./systems/modules/profiles/mail.nix
+        ./systems/modules/profiles/printing.nix
+        ./systems/modules/profiles/qemu.nix
+        ./systems/modules/profiles/redhat.nix
+        ./systems/modules/profiles/scanning.nix
+        ./systems/modules/profiles/virtualization.nix
+        ./systems/modules/profiles/wireguard.server.nix
+        ./systems/modules/profiles/zsh.nix
+        inputs.sops-nix.nixosModules.sops
+        inputs.envfs.nixosModules.envfs
+        {
+          # config.nix.generateRegistryFromInputs = true;
+          config.home-manager.useGlobalPkgs = true;
+          config.home-manager.useUserPackages = true;
+          # Import custom home-manager modules (NixOS)
+          config.home-manager.sharedModules = import ./users/modules/modules.nix;
+          # Default SopsFile
+          config.sops.defaultSopsFile = ./secrets/secrets.yaml;
+        }
+      ];
+    in
+    {
+      nixosConfigurations =
+        {
+          # Work laptop (unstable)
+          wakasu = inputs.nixpkgs.lib.nixosSystem {
+            system = "x86_64-linux";
+            modules = commonModules ++ unstableModules ++ [
+              wayland
+              inputs.nixos-hardware.nixosModules.lenovo-thinkpad-x1-9th-gen
+              ./systems/hosts/wakasu.nix
+            ];
+          };
+          # Work workstation (unstable)
+          aomi = inputs.nixpkgs.lib.nixosSystem {
+            system = "x86_64-linux";
+            modules = commonModules ++ unstableModules ++ [
+              inputs.nixos-hardware.nixosModules.lenovo-thinkpad-p1-gen3
+              inputs.nixos-hardware.nixosModules.common-pc-laptop-ssd
+              ./systems/hosts/aomi.nix
+            ];
+          };
+          # Servers (stable)
+          shikoku = inputs.nixpkgs-23_05.lib.nixosSystem {
+            system = "x86_64-linux";
+            modules = commonModules ++ stableModules ++ [
+              ./systems/hosts/shikoku.nix
+            ];
+          };
+          sakhalin = inputs.nixpkgs-23_05.lib.nixosSystem {
+            system = "x86_64-linux";
+            modules = commonModules ++ stableModules ++ [
+              inputs.nixos-hardware.nixosModules.common-pc-ssd
+              ./systems/hosts/sakhalin.nix
+            ];
+          };
+          kerkouane = inputs.nixpkgs-23_05.lib.nixosSystem {
+            system = "x86_64-linux";
+            modules = commonModules ++ stableModules ++ [
+              ./systems/modules/services/govanityurl.nix
+              ./systems/hosts/kerkouane.nix
+            ];
+          };
+        };
 
-      hostDefaults = {
-        system = "x86_64-linux";
-        channelName = "nixos-unstable";
-        extraArgs = {
-          # nixos/profiles/core.nix requires self parameter
-          inherit self;
-        };
-        modules = with nixosModules; [
-          { nix.generateRegistryFromInputs = true; }
-          # Exported modules
-          buildkit
-          # Common modules
-          ./systems/modules/core/default.nix
-          ./systems/modules/shell/default.nix
-          ./systems/modules/desktop/default.nix
-          ./systems/modules/dev/default.nix
-          ./systems/modules/editors/default.nix
-          ./systems/modules/hardware/default.nix
-          ./systems/modules/profiles/default.flake.nix # TODO: rename
-          ./systems/modules/virtualisation/default.nix
-          ./systems/modules/services/default.nix
-          # ./systems/modules/hardware/sane-extra-config.nixos.nix
-          # FIXME: migrate this to elsewhere, or at least split it
-          # Profiles probably need to go away
-          ./systems/modules/profiles/base.nix
-          ./systems/modules/profiles/builder.nix
-          ./systems/modules/profiles/desktop.nix
-          ./systems/modules/profiles/dev.nix
-          ./systems/modules/profiles/dns.nix
-          ./systems/modules/profiles/git.nix
-          ./systems/modules/profiles/gnome.nix
-          ./systems/modules/profiles/home.nix
-          ./systems/modules/profiles/i18n.nix
-          ./systems/modules/profiles/i3.nix
-          ./systems/modules/profiles/ipfs.nix
-          ./systems/modules/profiles/kubernetes.nix
-          ./systems/modules/profiles/laptop.nix
-          ./systems/modules/profiles/mail.nix
-          ./systems/modules/profiles/printing.nix
-          ./systems/modules/profiles/qemu.nix
-          ./systems/modules/profiles/redhat.nix
-          ./systems/modules/profiles/scanning.nix
-          ./systems/modules/profiles/virtualization.nix
-          ./systems/modules/profiles/wireguard.server.nix
-          ./systems/modules/profiles/zsh.nix
-          sops-nix.nixosModules.sops
-          envfs.nixosModules.envfs
-          {
-            config.nix.generateRegistryFromInputs = true;
-            config.home-manager.useGlobalPkgs = true;
-            config.home-manager.useUserPackages = true;
-            # Import custom home-manager modules (NixOS)
-            config.home-manager.sharedModules = import ./users/modules/modules.nix;
-            # Default SopsFile
-            config.sops.defaultSopsFile = ./secrets/secrets.yaml;
-          }
-        ];
-      };
-
-      hosts = {
-        # Main laptop
-        naruhodo = {
-          modules = unstableModules ++ [
-            nixos-hardware.nixosModules.lenovo-thinkpad-t480s
-            ./systems/hosts/naruhodo.nix
-          ];
-        };
-        wakasu = {
-          modules = unstableModules ++ [
-            wayland
-            nixos-hardware.nixosModules.lenovo-thinkpad-x1-9th-gen
-            ./systems/hosts/wakasu.nix
-          ];
-        };
-        # WSL setup
-        # FIXME okinawa doesn't have openssh
-        okinawa = {
-          modules = unstableModules ++ [
-            nixos-wsl.nixosModules.wsl
-            ./systems/hosts/okinawa.nix
-          ];
-        };
-        # Work "workstation"
-        aomi = {
-          modules = unstableModules ++ [
-            nixos-hardware.nixosModules.lenovo-thinkpad-p1-gen3
-            nixos-hardware.nixosModules.common-pc-laptop-ssd
-            ./systems/hosts/aomi.nix
-          ];
-        };
-        # Servers
-        shikoku = {
-          channelName = "nixos-23_05";
-          modules = stableModules_23_05 ++ [
-            ./systems/hosts/shikoku.nix
-          ];
-        };
-        sakhalin = {
-          channelName = "nixos-23_05";
-          modules = stableModules_23_05 ++ [
-            nixos-hardware.nixosModules.common-pc-ssd
-            ./systems/hosts/sakhalin.nix
-          ];
-        };
-        kerkouane = {
-          channelName = "nixos-23_05";
-          modules = stableModules_23_05 ++ [
-            ./systems/modules/services/govanityurl.nix
-            ./systems/hosts/kerkouane.nix
-          ];
-        };
-      };
-
-      # checks = builtins.mapAttrs (system: deployLib: deployLib.deployChecks self.deploy) deploy-rs.lib;
+      # TODO: expose some packages ?
+      # This is probably not gonna happen, instead I should move any internal package here outside, in their
+      # own repository and flake. If they are useful upstream.
 
       overlays = import ./nix/overlays;
-      outputsBuilder = channels:
-        let
-          pkgs = channels.nixpkgs;
-        in
-        {
-          # overlays = import ./nix/overlays;
-
-          packages = with channels.nixpkgs; {
-            inherit
-              bekind
-              tkn
-              tkn_0_23
-              tkn_0_22
-              tkn-pac
-              tkn-pac_0_8
-              tkn-pac_0_7
-              tkn-pac_0_6
-              tkn-local
-              tkn-local_0_4
-              tkn-local_0_3
-              ;
-          };
-
-          # `nix develop`
-          devShells =
-            let
-              ls = builtins.readDir ./shells;
-              files = builtins.filter (name: ls.${name} == "regular") (builtins.attrNames ls);
-              shellNames = builtins.map (filename: builtins.head (builtins.split "\\." filename)) files;
-              nameToValue = name: import (./shells + "/${name}.nix") { inherit pkgs inputs; };
-            in
-            builtins.listToAttrs (builtins.map (name: { inherit name; value = nameToValue name; }) shellNames);
-        };
     };
+
 }
