8b482ef82e875649ee38dec2d20b47bb77cf244a
Vincent Demeester <vincent@sbr.pm>
2023-10-24 17:57:17 +0200

    tools/emacs: remove old-config
    Signed-off-by: Vincent Demeester <vincent@sbr.pm>

--- tools/emacs/old-config/programming-elisp.el
+++
@@ -1,7 +0,0 @@
-;;; programming-elisp.el --- -*- lexical-binding: t -*-
-;;; Commentary:
-;;; Emacs Lisp configurations
-;;; Code:
-
-(provide 'programming-elisp)
-;;; programming-elisp.el ends here
