d6cf6f44d49c5879324d5f244af9134f0d417576
Vincent Demeester <vincent@sbr.pm>
2023-10-24 13:39:16 +0200

    tools/emacs: add project-x
    To be able to "save state" of a project.
    
    Signed-off-by: Vincent Demeester <vincent@sbr.pm>

--- tools/emacs/config/config-projects.el
+++ tools/emacs/config/config-projects.el
@@ -73,5 +73,17 @@ switch to it. Otherwise, create a new vterm shell."
       (vde-project-vterm (read-string "Command: "))))
   )
 
+(use-package project-x
+  :after project
+  :custom
+  (project-x-window-list-file
+   (expand-file-name
+    (concat "auto-save-list/project-window-list") user-emacs-directory))
+  :init
+  (require 'project-x)
+  (setq project-x-save-interval 600)    ;Save project state every 10 min
+  (project-x-mode 1))
+
+
 (provide 'config-projects)
 ;;; config-projects.el ends here
