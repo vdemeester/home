d5b5f1f05af849149fcd837b2540157442039ff9
Vincent Demeester <vincent@sbr.pm>
2023-11-02 15:23:11 +0100

    nix/packages: update gcr-nuke
    Signed-off-by: Vincent Demeester <vincent@sbr.pm>

--- nix/packages/my/scripts/bin/gcr-nuke.sh
+++ nix/packages/my/scripts/bin/gcr-nuke.sh
@@ -6,10 +6,10 @@ for repository in $(gcloud container images list --format='get(name)');
 do
     echo ">> Cleaning ${repository}…"
     while true; do
-	DIGEST=$(gcloud container images list-tags ${repository} --format='get(digest)' --limit=1)
+	DIGEST=$(gcloud container images list-tags ${repository} --filter='-tags:*' --format='get(digest)' --limit=1)
 	if [ -z "${DIGEST}" ]; then
 	    break
 	fi
-	gcloud container images delete ${repository}@${DIGEST} --force-delete-tags --quiet
+	gcloud container images delete ${repository}@${DIGEST} --force-delete-tags --quiet || break
     done
 done
