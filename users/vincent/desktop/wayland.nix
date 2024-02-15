8eaec52d12877a00c75421c2a05c8b7746865d7c
Vincent Demeester <vincent@sbr.pm>
2024-02-15 15:18:15 +0100

    users/vincent: configure kitty a bit more…
    … the idea is to remove the use of tmux locally.
    
    Signed-off-by: Vincent Demeester <vincent@sbr.pm>

--- users/vincent/desktop/wayland.nix
+++ users/vincent/desktop/wayland.nix
@@ -104,12 +104,35 @@
     };
     kitty = {
       enable = true;
+      # shellIntegration.enableZshIntegration = true;
       settings = {
         term = "xterm-256color";
         close_on_child_death = "yes";
         font_family = "Ubuntu Mono";
+        tab_bar_edge = "top";
+        listen_on = "unix:/tmp/my kitty";
+        allow_remote_control = "yes";
+        macos_option_as_alt = "yes";
+        copy_on_select = "yes";
+
+        active_tab_foreground = "#C6A0F6";
+        active_tab_background = "#0c0c0c";
+        inactive_tab_foreground = "#6E738D";
+        inactive_tab_background = "#0c0c0c";
+      };
+      font = {
+        name = "Ubuntu Mono";
+        size = 12;
+      };
+      keybindings = {
+        "shift+left" = "neighboring_window left";
+        "shift+right" = "neighboring_window right";
+        "shift+up" = "neighboring_window up";
+        "shift+down" = "neighboring_window down";
       };
       theme = "Tango Light";
+      # action_alias mkh kitten hints --alphabet asdfghjklqwertyuiopzxcvbnmASDFGHJKLQWERTYUIOPZXCVBNM 
+      # map kitty_mod+n    mkh --type=linenum emacsclient -c -nw +{line} {path}
     };
   };
   services = {
