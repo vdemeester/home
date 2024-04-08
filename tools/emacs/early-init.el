1dba7a281b90be6b05003a79f0a9f54047c45321
Vincent Demeester <vincent@sbr.pm>
2024-04-08 03:13:38 +0200

    tools/emacs: effectively disable more gui element in early-init
    Signed-off-by: Vincent Demeester <vincent@sbr.pm>

--- tools/emacs/early-init.el
+++ tools/emacs/early-init.el
@@ -42,6 +42,10 @@
 (setq frame-inhibit-implied-resize t)
 
 ;; Disable GUI elements
+(push '(menu-bar-lines . 0) default-frame-alist)
+(push '(tool-bar-lines . 0) default-frame-alist)
+(push '(vertical-scroll-bars) default-frame-alist)
+
 (menu-bar-mode -1)
 (tool-bar-mode -1)
 (scroll-bar-mode -1)
