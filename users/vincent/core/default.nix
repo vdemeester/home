72ecc060ae72e0613a1b4b0c106812e58e76eff9
Vincent Demeester <vincent@sbr.pm>
2024-05-13 16:21:20 +0200

    users/vincent: remove latest ref of NUR
    Signed-off-by: Vincent Demeester <vincent@sbr.pm>

--- users/vincent/core/default.nix
+++ users/vincent/core/default.nix
@@ -46,11 +46,6 @@ in
   xdg.configFile."nixpkgs/config.nix".text = ''
     {
       allowUnfree = true;
-      packageOverrides = pkgs: {
-        nur = import (builtins.fetchTarball "https://github.com/nix-community/NUR/archive/master.tar.gz") {
-          inherit pkgs;
-        };
-      };
     }
   '';
 }
