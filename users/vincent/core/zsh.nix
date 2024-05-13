e645bcc9c04d5412ec9735d2c35cd301e1d70733
Vincent Demeester <vincent@sbr.pm>
2024-05-13 16:21:38 +0200

    users/vincent/zsh: update autosuggestion settings
    Signed-off-by: Vincent Demeester <vincent@sbr.pm>

--- users/vincent/core/zsh.nix
+++ users/vincent/core/zsh.nix
@@ -18,7 +18,7 @@ in
 
   programs.zsh = {
     enable = true;
-    enableAutosuggestions = true;
+    autosuggestion.enable = true;
     enableCompletion = true;
     autocd = true;
     dotDir = ".config/zsh";
