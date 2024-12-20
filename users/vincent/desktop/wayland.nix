f3cdb64c890e50fadc74be6997188413c1bd8a8c
Vincent Demeester <vincent@sbr.pm>
2024-12-20 07:48:44 +0100

    users/vincent: re-enable swayidle in wayland
    Signed-off-by: Vincent Demeester <vincent@sbr.pm>

--- users/vincent/desktop/wayland.nix
+++ users/vincent/desktop/wayland.nix
@@ -57,14 +57,14 @@
     kanshi = {
       enable = true;
       settings = [
-				# Make it configurable per host
-				{
-					profile.name = "aomi";
-					profile.outputs = [
+        # Make it configurable per host
+        {
+          profile.name = "aomi";
+          profile.outputs = [
             { criteria = "eDP-1"; status = "disable"; }
             { criteria = "DP-1"; status = "enable"; position = "0,0"; mode = "3440x1440"; scale = 1.0; }
-					];
-				}
+          ];
+        }
         {
           profile.name = "home-undocked";
           profile.outputs = [
@@ -93,68 +93,67 @@
       groupBy = "app-name,summary";
       # FIXME: hide pulseaudio notifications (maybe they don't show up without pasystray)
       extraConfig = ''
-width=400
-on-button-left=dismiss
-on-button-middle=invoke-default-action
-on-button-right=dismiss
-border-radius=6
-border-size=3
-border-color=#db7508
-format=<b>%s</b>\n%b\n<i>%a</i>
-icon-path=/run/current-system/sw/share/icons/Qogir-dark:/run/current-system/sw/share/icons/hicolor
+        width=400
+        on-button-left=dismiss
+        on-button-middle=invoke-default-action
+        on-button-right=dismiss
+        border-radius=6
+        border-size=3
+        border-color=#db7508
+        format=<b>%s</b>\n%b\n<i>%a</i>
+        icon-path=/run/current-system/sw/share/icons/Qogir-dark:/run/current-system/sw/share/icons/hicolor
 
-[urgency=low]
-background-color=#282c30
-text-color=#888888
-default-timeout=2000
+        [urgency=low]
+        background-color=#282c30
+        text-color=#888888
+        default-timeout=2000
 
-[urgency=normal]
-background-color=#282c30
-text-color=#ffffff
-default-timeout=5000
+        [urgency=normal]
+        background-color=#282c30
+        text-color=#ffffff
+        default-timeout=5000
 
-[urgency=high]
-background-color=#900000
-text-color=#ffffff
-border-color=#ff0000
+        [urgency=high]
+        background-color=#900000
+        text-color=#ffffff
+        border-color=#ff0000
 
-[app-name="pa-notify"]
-background-color=#0080ff
-text-color=#333333
-anchor=bottom-right
-format=<b>%s</b>\n%b
+        [app-name="pa-notify"]
+        background-color=#0080ff
+        text-color=#333333
+        anchor=bottom-right
+        format=<b>%s</b>\n%b
 
-[category="build"]
-anchor=bottom-right
-format=<b>%s</b>\n%b
+        [category="build"]
+        anchor=bottom-right
+        format=<b>%s</b>\n%b
 
-[category="recording"]
-anchor=bottom-right
-format=<b>%s</b>\n%b
+        [category="recording"]
+        anchor=bottom-right
+        format=<b>%s</b>\n%b
 
-[category="info"]
-anchor=center
-format=<b>%s</b> %b
+        [category="info"]
+        anchor=center
+        format=<b>%s</b> %b
 
-#[app-name="Google Chrome" body~="calendar.google.com.*"]
-#max-visible=2
+        #[app-name="Google Chrome" body~="calendar.google.com.*"]
+        #max-visible=2
 
-[mode=do-not-disturb]
-invisible=1
+        [mode=do-not-disturb]
+        invisible=1
       '';
     };
     swayidle = {
-      enable = false;
+      enable = true;
       events = [
         { event = "before-sleep"; command = "${pkgs.swaylock}/bin/swaylock --daemonize -i $HOME/desktop/pictures/lockscreen"; }
         { event = "lock"; command = "${pkgs.swaylock}/bin/swaylock --daemonize -i $HOME/desktop/pictures/lockscreen"; }
       ];
       timeouts = [
-        { timeout = 600; command = "${pkgs.swaylock}/bin/swaylock --daemonize -i $HOME/desktop/pictures/lockscreen"; }
         {
-          timeout = 1200;
-          command = ''${pkgs.sway}/bin/swaymsg "output * dpms off"'';
-          resumeCommand = ''${pkgs.sway}/bin/swaymsg "output * dpms on"'';
+          timeout = 600;
+          command = ''${pkgs.sway}/bin/swaymsg "output * power off"'';
+          resumeCommand = ''${pkgs.sway}/bin/swaymsg "output * power on"'';
         }
       ];
     };
