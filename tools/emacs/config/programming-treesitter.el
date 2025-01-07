200c5da479856967eb1f68991254bee4371ca33e
Vincent Demeester <vincent@sbr.pm>
2025-01-07 19:00:48 +0100

    tools/emacs: cleaning up treesit file
    Signed-off-by: Vincent Demeester <vincent@sbr.pm>

--- tools/emacs/config/programming-treesitter.el
+++ tools/emacs/config/programming-treesitter.el
@@ -3,10 +3,6 @@
 ;;; Treesitter configuration
 ;;; Code:
 
-(use-package treesit-auto
-  :config
-  (setq treesit-auto-install 'prompt)
-  (global-treesit-auto-mode))
 
 (use-package indent-bars
   :if (eq system-type 'gnu/linux)
@@ -21,32 +17,6 @@
   (indent-bars-treesit-ignore-blank-lines-types '("module"))
   (indent-bars-treesit-scope '((python function_definition class_definition for_statement
 	                               if_statement with_statement while_statement))))
-
-;; (unless (package-installed-p 'combobulate)
-;;   (package-vc-install '(combobulate :url "https://github.com/mickeynp/combobulate"
-;; 				    :branch "development")))
-
-;; (use-package combobulate
-;;   :load-path "~/.config/emacs/elpa/combobulate"
-;;   :config
-;;   ;; You can customize Combobulate's key prefix here.
-;;   ;; Note that you may have to restart Emacs for this to take effect!
-;;   (setq combobulate-key-prefix "C-c o")
-;; 
-;;   ;; Optional, but recommended.
-;;   ;;
-;;   ;; You can manually enable Combobulate with `M-x
-;;   ;; combobulate-mode'.
-;;   :hook
-;;   (python-ts-mode . combobulate-mode)
-;;   (js-ts-mode . combobulate-mode)
-;;   (html-ts-mode . combobulate-mode)
-;;   (css-ts-mode . combobulate-mode)
-;;   (yaml-ts-mode . combobulate-mode)
-;;   (typescript-ts-mode . combobulate-mode)
-;;   (json-ts-mode . combobulate-mode)
-;;   (tsx-ts-mode . combobulate-mode)
-;;   (go-ts-mode . combobulate-mode))
 
 (provide 'programming-treesitter)
 ;;; programming-treesitter.el ends here
