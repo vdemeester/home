68c6c752e077a2064b8c971dc82a973b14038189
Vincent Demeester <vincent@sbr.pm>
2025-01-10 00:23:07 +0100

    tools/emacs: start mu4e configuration
    Signed-off-by: Vincent Demeester <vincent@sbr.pm>

--- tools/emacs/config/config-mu4e.el
+++ tools/emacs/config/config-mu4e.el
@@ -2,7 +2,13 @@
 ;;; Commentary:
 ;;; Code:
 
+(use-package mu4e
+  :commands (mu4e)
+  :config
+  (setopt mu4e-completing-read-function completing-read-function))
 
+(use-package consult-mu
+  :commands (consult-mu))
 
 (provide 'config-mu4e)
 ;;; config-mu4e.el ends here
