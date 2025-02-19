23af6499e6add56eda3792ffcaf39fb4d46c8b33
Vincent Demeester <vincent@sbr.pm>
2025-02-19 08:12:10 +0100

    tools/emacs: move vde-project-run-in-vterm directly in config

--- tools/emacs/lisp/project-func.el
+++ tools/emacs/lisp/project-func.el
@@ -18,26 +18,5 @@
     (project-root (project-current)))
    (t default-directory)))
 
-;;;##autoload
-(defun vde-project-run-in-vterm (command &optional directory)
-  "Run the given `COMMAND' in a new vterm buffer in `project-root' or the
-given `DIRECTORY'.
-
-This is similar to `compile' but with vterm.
-One reason for this is to be able to run commands that needs a TTY."
-  (interactive "sCommand: ")
-  (let* ((cwd (or directory (vde-project--project-root-or-default-directory)))
-	 (default-directory cwd)
-	 (buffer-name (format "*vterm %s: %s*" cwd command))
-         (buffer (get-buffer buffer-name))
-         (vterm-kill-buffer-on-exit nil)
-	 (vterm-shell (concat "bash -c '" command ";exit'")))
-    (when buffer
-      (kill-buffer buffer))
-    (let ((buffer (generate-new-buffer buffer-name)))
-      (pop-to-buffer buffer)
-      (with-current-buffer buffer
-        (vterm-mode)))))
-
 (provide 'project-func)
 ;;; project-func.el ends here
