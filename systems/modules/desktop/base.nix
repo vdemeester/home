219cb1cf52650d53e91a1ca194ef6722d918237f
Vincent Demeester <vincent@sbr.pm>
2025-03-05 12:10:58 +0100

    Add nix-ld and envfs 🙃
    Signed-off-by: Vincent Demeester <vincent@sbr.pm>

--- systems/modules/desktop/base.nix
+++ systems/modules/desktop/base.nix
@@ -22,6 +22,82 @@ in
     };
   };
   config = mkIf cfg.enable {
+    # Putting this in desktop for now
+    programs = {
+      nix-ld = {
+        enable = true;
+        # put whatever libraries you think you might need
+        # nix-ld includes a strong sane-default as well
+        # in addition to these
+        libraries = with pkgs; [
+          acl
+          alsa-lib
+          at-spi2-atk
+          at-spi2-core
+          atk
+          attr
+          bzip2
+          cairo
+          curl
+          dbus
+          expat
+          fontconfig
+          freetype
+          fuse3
+          gdk-pixbuf
+          glib
+          glibc
+          gtk3
+          icu
+          libGL
+          libappindicator-gtk3
+          libdrm
+          libglvnd
+          libnotify
+          libpulseaudio
+          libsecret
+          libsodium
+          libssh
+          libunwind
+          libusb1
+          libuuid
+          libxkbcommon
+          mesa
+          nspr
+          nss
+          openssl
+          pango
+          pipewire
+          systemd
+          stdenv.cc.cc # .lib
+          util-linux
+          # vulkan-loader
+          # xorg.libX11
+          # xorg.libXScrnSaver
+          # xorg.libXcomposite
+          # xorg.libXcursor
+          # xorg.libXdamage
+          # xorg.libXext
+          # xorg.libXfixes
+          # xorg.libXi
+          # xorg.libXrandr
+          # xorg.libXrender
+          # xorg.libXtst
+          # xorg.libxcb
+          # xorg.libxkbfile
+          # xorg.libxshmfence
+          zlib
+          zstd
+        ];
+      };
+    };
+
+    services = {
+      envfs = {
+        enable = true;
+      };
+    };
+
     modules.services.avahi.enable = true;
     # Enable netbootxyz if systemd-boot is enabled
     boot = {
