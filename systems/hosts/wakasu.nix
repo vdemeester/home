7a280262e0eb49f6df9c0e2d7eb4efb120677c66
Vincent Demeester <vincent@sbr.pm>
2025-04-04 12:17:37 +0200

    systems/wakasu: update udev extra rules…
    … for zsa keyboards.
    
    Signed-off-by: Vincent Demeester <vincent@sbr.pm>

--- systems/hosts/wakasu.nix
+++ systems/hosts/wakasu.nix
@@ -86,16 +86,21 @@ in
   ];
   services.udev.packages = [ pkgs.sane-airscan ];
   services.udev.extraRules = ''
+        KERNEL=="hidraw*", ATTRS{idVendor}=="16c0", MODE="0664", GROUP="wheel"
+        KERNEL=="hidraw*", ATTRS{idVendor}=="3297", MODE="0664", GROUP="wheel"
         # STM32 rules for the Moonlander and Planck EZ
-        SUBSYSTEMS=="usb", ATTRS{idVendor}=="0483", ATTRS{idProduct}=="df11", \
-            MODE:="0666", \
-            SYMLINK+="stm32_dfu"
+    		# All zsa keyboards
+        SUBSYSTEM=="usb", ATTR{idVendor}=="3297", GROUP="wheel"
+    		# zsa moonlander
+        SUBSYSTEM=="usb", ATTR{idVendor}=="3297", ATTR{idProduct}=="1969", GROUP="wheel"
+        SUBSYSTEMS=="usb", ATTRS{idVendor}=="0483", ATTRS{idProduct}=="df11", MODE:="0666", SYMLINK+="stm32_dfu
+        SUBSYSTEMS=="usb", ATTRS{idVendor}=="3297", MODE:="0666", SYMLINK+="ignition_dfu"
 
         # Suspend the system when battery level drops to 5% or lower
         SUBSYSTEM=="power_supply", ATTR{status}=="Discharging", ATTR{capacity}=="[0-5]", RUN+="${pkgs.systemd}/bin/systemctl hibernate"
 
         #Flipper Zero serial port
-    		SUBSYSTEMS==“usb”, ATTRS{idVendor}==“0483”, ATTRS{idProduct}==“5740”, ATTRS{manufacturer}==“Flipper Devices Inc.”, TAG+=“uaccess”, GROUP=“wheel”
+        SUBSYSTEMS==“usb”, ATTRS{idVendor}==“0483”, ATTRS{idProduct}==“5740”, ATTRS{manufacturer}==“Flipper Devices Inc.”, TAG+=“uaccess”, GROUP=“wheel”
         #Flipper Zero DFU
         SUBSYSTEMS==“usb”, ATTRS{idVendor}==“0483”, ATTRS{idProduct}==“df11”, ATTRS{manufacturer}==“STMicroelectronics”, TAG+=“uaccess”, GROUP=“wheel”
   '';
