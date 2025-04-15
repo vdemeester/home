4276ba9d12c55c434f456c5620ee8f46f99cd778
Vincent Demeester <vincent@sbr.pm>
2025-04-15 23:26:18 +0200

    systems/aomi: port wakasu configurations
    Signed-off-by: Vincent Demeester <vincent@sbr.pm>

--- systems/hosts/aomi.nix
+++ systems/hosts/aomi.nix
@@ -191,28 +191,51 @@ in
     home = true;
   };
 
+  # virtualisation.podman.dockerSocket.enable = true;
+  virtualisation.podman.autoPrune.enable = true;
   environment.systemPackages = with pkgs; [
     virt-manager
     catt
     go-org-readwise
     vscode
     age-plugin-tpm
+    keymapp
+    kontroll
   ];
 
+  services.udev.packages = [ pkgs.sane-airscan ];
   services.udev.extraRules = ''
-    # STM32 rules for the Moonlander and Planck EZ
-    SUBSYSTEMS=="usb", ATTRS{idVendor}=="0483", ATTRS{idProduct}=="df11", \
-        MODE:="0666", \
-        SYMLINK+="stm32_dfu"
+        KERNEL=="hidraw*", ATTRS{idVendor}=="16c0", MODE="0664", GROUP="wheel"
+        KERNEL=="hidraw*", ATTRS{idVendor}=="3297", MODE="0664", GROUP="wheel"
+        # STM32 rules for the Moonlander and Planck EZ
+    		# All zsa keyboards
+        SUBSYSTEM=="usb", ATTR{idVendor}=="3297", GROUP="wheel"
+    		# zsa moonlander
+        SUBSYSTEM=="usb", ATTR{idVendor}=="3297", ATTR{idProduct}=="1969", GROUP="wheel"
+        SUBSYSTEMS=="usb", ATTRS{idVendor}=="0483", ATTRS{idProduct}=="df11", MODE:="0666", SYMLINK+="stm32_dfu
+        SUBSYSTEMS=="usb", ATTRS{idVendor}=="3297", MODE:="0666", SYMLINK+="ignition_dfu"
 
-    # Suspend the system when battery level drops to 5% or lower
-    SUBSYSTEM=="power_supply", ATTR{status}=="Discharging", ATTR{capacity}=="[0-5]", RUN+="${pkgs.systemd}/bin/systemctl hibernate"
+        # Suspend the system when battery level drops to 5% or lower
+        SUBSYSTEM=="power_supply", ATTR{status}=="Discharging", ATTR{capacity}=="[0-5]", RUN+="${pkgs.systemd}/bin/systemctl hibernate"
+
+        #Flipper Zero serial port
+        SUBSYSTEMS==“usb”, ATTRS{idVendor}==“0483”, ATTRS{idProduct}==“5740”, ATTRS{manufacturer}==“Flipper Devices Inc.”, TAG+=“uaccess”, GROUP=“wheel”
+        #Flipper Zero DFU
+        SUBSYSTEMS==“usb”, ATTRS{idVendor}==“0483”, ATTRS{idProduct}==“df11”, ATTRS{manufacturer}==“STMicroelectronics”, TAG+=“uaccess”, GROUP=“wheel”
   '';
 
   services = {
     ollama = {
       enable = true;
       # acceleration = "cuda"; # no nivida :D
+    };
+    dictd = {
+      enable = true;
+      DBs = with pkgs.dictdDBs; [ wiktionary wordnet fra2eng eng2fra ];
+    };
+    locate = {
+      enable = true;
+      pruneBindMounts = true;
     };
     geoclue2.enable = true;
     # automatic login is "safe" as we ask for the encryption passphrase anyway..
@@ -238,6 +261,41 @@ in
       endpoint = endpointIP;
       endpointPort = endpointPort;
       endpointPublicKey = endpointPublicKey;
+    };
+    gitea-actions-runner = {
+      instances = {
+        "aomi-codeberg" = {
+          name = "aomi";
+          enable = true;
+          url = "https://codeberg.org";
+          # tokenFile = "/home/vincent/sync/codeberg.token";
+          token = "ysW0pn2ss2zhhKx1C6Ely6pCsSlpGXR8N8qXPtE4";
+          labels = [
+            # "local:host"
+            "nixos-${pkgs.system}:host"
+            "native:host"
+            "docker:docker://gitea/runner-images:ubuntu-latest"
+            "ubuntu-latest:docker://gitea/runner-images:ubuntu-latest"
+            "ubuntu-24.04:docker://gitea/runner-images:ubuntu-24.04"
+            "ubuntu-22.04:docker://gitea/runner-images:ubuntu-22.04"
+            "ubuntu-20.04:docker://gitea/runner-images:ubuntu-20.04"
+            # "nix:docker://localhost:5921/nix-runner"
+          ];
+          hostPackages = with pkgs; [
+            bash
+            direnv
+            coreutils
+            curl
+            gawk
+            nixVersions.stable
+            gitFull
+            gnused
+            docker
+            openssh
+            wget
+          ];
+        };
+      };
     };
   };
 
