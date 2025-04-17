efb1e13fe312810a5abc1548511eefcef20258c4
Vincent Demeester <vincent@sbr.pm>
2025-04-17 11:40:25 +0200

    Fix install.sh quotes
    Signed-off-by: Vincent Demeester <vincent@sbr.pm>

--- install.sh
+++ install.sh
@@ -5,4 +5,4 @@ SYSTEM=$1
 shift
 
 nix --extra-experimental-features "nix-command flakes" run \
-    'github:nix-community/disko/latest#disko-install' -- --flake '.#${SYSTEM}' $@
+    'github:nix-community/disko/latest#disko-install' -- --flake ".#${SYSTEM}" $@
