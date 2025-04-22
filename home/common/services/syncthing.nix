0a3e7e308d88479fa57cdfe2d6622ea423ddf0c6
Vincent Demeester <vincent@sbr.pm>
2025-04-22 16:35:44 +0200

    home: temporary configuration for syncthing…
    … for kyushu only, will enhance this as I switch hosts to the new setup.
    
    Signed-off-by: Vincent Demeester <vincent@sbr.pm>

--- home/common/services/syncthing.nix
+++ home/common/services/syncthing.nix
@@ -8,8 +8,7 @@ let
   isCurrentHost = n: v: n != config.networking.hostName;
   # Folders list
   folders = [
-    # TODO maybe deprecate for documents
-    { label = "sync"; id = "7dshg-r8zr6"; }
+    { label = "sync"; id = "7dshg-r8zr6"; } # TODO maybe deprecate for documents
     { label = "documents"; id = "oftdb-t5anv"; }
     { label = "org"; id = "sjpsr-xfwdu"; }
     { label = "screenshots"; id = "prpsz-azlz9"; }
@@ -64,6 +63,62 @@ in
     enable = true;
     extraOptions = [ "--no-default-folder" ];
     # guiAddress = cfg.guiAddress;
-    settings = { };
+    # TODO This is only for kyushu, will need to migrate this later
+    settings = {
+      devices = {
+        aomi = {
+          id = "XCR6WWB-OZUDGFB-LQPFW73-MV5SPJK-4IGOMA4-IAXON3I-C6OFETL-TPK5FQS";
+          addresses = [ "tcp://aomi.vpn" "tcp://aomi.light" "tcp://aomi.sbr.pm" ];
+        };
+        sakhalin = {
+          id = "4TYYG7V-A67D5SN-HMEJCI7-POOZRLL-RNCIE4U-ZYVGTOB-JQ5DOSV-ZCGWUAL";
+          addresses = [ "tcp://sakhalin.light" "tcp://sakhalin.vpn" "tcp://sakhalin.sbr.pm" ];
+        };
+        shikoku = {
+          id = "KZMMXRR-UINDQTS-H3TV2W7-EIGOUDI-3LW4ZDG-7PRKDFV-MJ5KUTJ-YG5Y5AI";
+          addresses = [ "tcp://shikoku.light" "tcp://shikoku.vpn" "tcp://shikoku.sbr.pm" ];
+        };
+        kerkouane = {
+          id = "IFVRRQ7-KMIOQXP-5YDJXQU-UJXUKHB-7THCSY6-B3NHRNA-ED7IRI7-2JPPKQY";
+          addresses = [ "tcp://10.100.0.1" "tcp://kerkouane.vpn" ];
+        };
+        aion = {
+          id = "YORNSGU-UC4IAG5-IWJCD7T-MVPIU7O-AYM36UK-LEHF7AP-CBC4L6C-ZWKUYQF";
+          addresses = [ "tcp://aion.light" "tcp://aion.vpn" "tcp://aion.sbr.pm" ];
+        };
+      };
+      folders = {
+        "/home/vincent/sync" = {
+          label = "sync";
+          id = "7dshg-r8zr6";
+          devices = [ "aomi" "aion" "shikoku" "sakhalin" ];
+          rescanIntervalS = 3600 * 6;
+        };
+        "/home/vincent/desktop/org" = {
+          label = "org";
+          id = "sjpsr-xfwdu";
+          devices = [ "aomi" "aion" "shikoku" "sakhalin" ];
+          rescanIntervalS = 3600 * 6;
+        };
+        "/home/vincent/desktop/documents" = {
+          label = "documents";
+          id = "oftdb-t5anv";
+          devices = [ "aomi" "aion" "shikoku" "sakhalin" ];
+          rescanIntervalS = 3600 * 6;
+        };
+        "/home/vincent/desktop/pictures/screenshots" = {
+          label = "screenshots";
+          id = "prpsz-azlz9";
+          devices = [ "aomi" "aion" "shikoku" "sakhalin" ];
+          rescanIntervalS = 3600 * 6;
+        };
+        "/home/vincent/desktop/pictures/wallpapers" = {
+          label = "wallpapers";
+          id = "wpiah-ydwwx";
+          devices = [ "aomi" "aion" "shikoku" "sakhalin" ];
+          rescanIntervalS = 3600 * 6;
+        };
+      };
+    };
   };
 }
