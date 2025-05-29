bb39c758fc482d0081765d2f05989c71df4bc2de
Vincent Demeester <vincent@sbr.pm>
2025-05-29 17:39:13 +0200

    systems/aix: add /data disk
    Signed-off-by: Vincent Demeester <vincent@sbr.pm>

--- systems/aix/hardware.nix
+++ systems/aix/hardware.nix
@@ -5,5 +5,10 @@ _: {
       fsType = "ext4";
       options = [ "noatime" ];
     };
+    "/data" = {
+      device = "/dev/disk/by-label/data";
+      fsType = "ext4";
+      options = [ "noatime" ];
+    };
   };
 }
