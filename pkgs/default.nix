3936306cf618aad9f2210da4c5781a70f65ee508
Vincent Demeester <vincent@sbr.pm>
2025-06-19 15:09:37 +0200

    pkg: remove dired-plus (not used and not building)
    Signed-off-by: Vincent Demeester <vincent@sbr.pm>

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -35,13 +35,13 @@ in
       sha256 = "09aprlawi3m7qrdas3zlk52niap2sr741qzfpjwc2c22hrlyv8ng";
     };
   };
-  dired-plus = compileEmacsFiles {
-    name = "dired-plus";
-    src = pkgs.fetchFromGitHub {
-      owner = "emacsmirror";
-      repo = "dired-plus";
-      rev = "db4d82a6b1995a3aa31bd7f2dcaf9b83335d5576";
-      sha256 = "10rfjf6gn5cx1kxq97xq7p24rnkw0hnzj32x4hny7bc6s3635d3x";
-    };
-  };
+  # dired-plus = compileEmacsFiles {
+  #   name = "dired-plus";
+  #   src = pkgs.fetchFromGitHub {
+  #     owner = "emacsmirror";
+  #     repo = "dired-plus";
+  #     rev = "db4d82a6b1995a3aa31bd7f2dcaf9b83335d5576";
+  #     sha256 = "10rfjf6gn5cx1kxq97xq7p24rnkw0hnzj32x4hny7bc6s3635d3x";
+  #   };
+  # };
 }
