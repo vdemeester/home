2f9da3d6fa295ffe490d5ead66a6c8b23ce5ad2d
Vincent Demeester <vincent@sbr.pm>
2025-06-27 15:17:20 +0200

    home: fix wheel bindings for mpv
    Signed-off-by: Vincent Demeester <vincent@sbr.pm>

--- home/common/desktop/mpv.nix
+++ home/common/desktop/mpv.nix
@@ -6,6 +6,13 @@
       hwdec = "auto";
       osc = "no";
     };
+    bindings = {
+      WHEEL_UP = "seek 10";
+      WHEEL_DOWN = "seek -10";
+      WHEEL_LEFT = "add volume -2";
+      WHEEL_RIGHT = "add volume 2";
+      "Alt+0" = "set window-scale 0.5";
+    };
     scripts = [
       pkgs.mpvScripts.mpris
       pkgs.mpvScripts.modernz
