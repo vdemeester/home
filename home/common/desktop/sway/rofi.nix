6d221ff703c2237695a81927cb434bc537c0e191
Vincent Demeester <vincent@sbr.pm>
2025-06-27 15:18:24 +0200

    home: configure fuzzel a little bit
    Signed-off-by: Vincent Demeester <vincent@sbr.pm>

--- home/common/desktop/sway/rofi.nix
+++ home/common/desktop/sway/rofi.nix
@@ -1,9 +1,34 @@
 { lib, pkgs, ... }:
 {
   home.packages = with pkgs; [
-    fuzzel
     raffi
   ];
+
+  programs.fuzzel = {
+    enable = true;
+    settings = {
+      main = {
+        font = "JetBrains Mono:size=12";
+        dpi-aware = "auto";
+      };
+      colors = {
+        background = "1f1e25ff";
+        text = "969696ff";
+        match = "00d3d0ff";
+        selection = "303030ff";
+        selection-match = "ffffffff";
+        selection-text = "ffffffff";
+        border = "348e9eff";
+      };
+      border = {
+        width = 2;
+        radius = 2;
+      };
+      key-bindings = {
+        cancel = "Escape Control+g";
+      };
+    };
+  };
 
   # FIXME updates this
   xdg.configFile."raffi/raffi.yaml".text = lib.generators.toYAML { } {
