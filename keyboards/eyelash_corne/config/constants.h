279b2ba439b0eb83431d7e66548e3b1fc1df42bf
Vincent Demeester <vincent@sbr.pm>
2025-07-10 15:48:27 +0200

    keyboards/eyelash_corne: remove bépo accent layer...
    ... and implement the symbols layer.
    
    Signed-off-by: Vincent Demeester <vincent@sbr.pm>

--- keyboards/eyelash_corne/config/constants.h
+++ keyboards/eyelash_corne/config/constants.h
@@ -6,15 +6,15 @@
 
 // Layers
 #define BEP 0
-#define BAC 1
-#define ERG 2
-#define EAC 3
-#define QWE 4
-#define QAC 5
-#define SYM 6
-#define NAV 7
-#define NUM 8
-#define MOU 9
+/* #define BAC 1 */
+#define ERG 1
+#define EAC 2
+#define QWE 3
+#define QAC 4
+#define SYM 5
+#define NAV 6
+#define NUM 7
+#define MOU 8
 // #define SYS 10
 
 // Modifier keys
