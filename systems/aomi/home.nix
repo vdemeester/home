943fa6438e12df4c8771d75932d4d77b6a87c5cf
Vincent Demeester <vincent@sbr.pm>
2025-07-23 10:37:31 +0200

    systems/aomi/home: trying to understand something...
    Signed-off-by: Vincent Demeester <vincent@sbr.pm>

--- systems/aomi/home.nix
+++ systems/aomi/home.nix
@@ -1,7 +1,12 @@
-_: {
-  imports = [
-    ../../home/common/dev/base.nix
-  ];
+{ pkgs, ... }:
+{
+  # imports = [
+  #   ../../home/common/dev/base.nix
+  # ];
   services.ssh-agent.enable = true;
   systemd.user.services.syncthing.Install.WantedBy = [ "multi-user.target" ];
+
+  home.packages = with pkgs; [
+    gnumake
+  ];
 }
