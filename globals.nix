31814900c2ac3b28e765fb3a950df86ea0934651
Vincent Demeester <vincent@sbr.pm>
2025-07-23 10:47:01 +0200

    globals: trying something with ssh keys
    Signed-off-by: Vincent Demeester <vincent@sbr.pm>

--- globals.nix
+++ globals.nix
@@ -80,6 +80,7 @@ let
               else
                 x;
             forwardAgent = true;
+            identityFile = [ "~/.ssh/kyushu" ];
           };
         })
         (
@@ -98,6 +99,7 @@ in
       "ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBBFzxC16VqwTgWDQfw2YCiOw2JzpH3z9XgHtKoHhBdHi2i9m9XUc7fIUeEIIf7P8ARRNd8q5bjvl8JY7LtPkNCU="
       # AOMI (only "trusted" one)
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILJmTdMKYdgqpbQWBif58VBuwX+GqMGsMfB1ey1TKrM3 vincent@aomi"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGThdcaPfIaB7d+K5uODqEusLKGI5ZCye0aNOCaMoInO Kyushu's ssh key"
     ];
   };
   syncthingFolders = {
