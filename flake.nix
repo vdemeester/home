cd2d745623863231f1152d41718f936ffe8c27f7
Vincent Demeester <vincent@sbr.pm>
2025-08-14 12:28:06 +0200

    flake.nix: trying to go back in time for nagoya
    Signed-off-by: Vincent Demeester <vincent@sbr.pm>

--- flake.nix
+++ flake.nix
@@ -90,9 +90,61 @@
           pkgsInput = inputs.nixpkgs-25_05;
           homeInput = inputs.home-manager-25_05;
         };
-        nagoya = libx.mkRPIHost {
-          hostname = "nagoya";
-          hardwareType = "rpi4"; # to disable tpm2 TODO find a better way
+        # nagoya = libx.mkRPIHost {
+        #   hostname = "nagoya";
+        #   hardwareType = "rpi4"; # to disable tpm2 TODO find a better way
+        # };
+        nagoya = nixos-raspberrypi.lib.nixosSystem {
+          specialArgs = inputs;
+          modules = [
+            (
+              { ... }:
+              {
+                imports = with nixos-raspberrypi.nixosModules; [
+                  raspberry-pi-5.base
+                  raspberry-pi-5.bluetooth
+                ];
+              }
+            )
+            (
+              { ... }:
+              {
+                networking.hostName = "yourHostName";
+                users.users.yourUserName = {
+                  initialPassword = "yourInitialPassword";
+                  isNormalUser = true;
+                  extraGroups = [
+                    "wheel"
+                  ];
+                };
+
+                services.openssh.enable = true;
+              }
+            )
+
+            (
+              { ... }:
+              {
+                fileSystems = {
+                  "/boot/firmware" = {
+                    device = "/dev/disk/by-uuid/2175-794E";
+                    fsType = "vfat";
+                    options = [
+                      "noatime"
+                      "noauto"
+                      "x-systemd.automount"
+                      "x-systemd.idle-timeout=1min"
+                    ];
+                  };
+                  "/" = {
+                    device = "/dev/disk/by-uuid/44444444-4444-4444-8888-888888888888";
+                    fsType = "ext4";
+                    options = [ "noatime" ];
+                  };
+                };
+              }
+            )
+          ];
         };
         sakhalin = libx.mkHost {
           hostname = "sakhalin";
