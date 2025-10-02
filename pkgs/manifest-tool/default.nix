2ea16891be8d4cd5829884280b7e7671f549da45
Vincent Demeester <vincent@sbr.pm>
2025-10-02 16:29:26 +0200

    pkgs/manifest-tool: update to 2.2.1
    Signed-off-by: Vincent Demeester <vincent@sbr.pm>

--- pkgs/manifest-tool/default.nix
+++ pkgs/manifest-tool/default.nix
@@ -6,7 +6,8 @@
 
 buildGoModule rec {
   name = "manifest-tool-${version}";
-  version = "2.0.0";
+  pname = name;
+  version = "2.2.1";
   rev = "v${version}";
 
   subPackages = [ "cmd/manifest-tool" ];
@@ -16,7 +17,7 @@ buildGoModule rec {
     inherit rev;
     owner = "estesp";
     repo = "manifest-tool";
-    sha256 = "sha256-KBX/VgFKm3xT1tRxKKIO3u4JZ3htkTVVnB1GqpJ0xO0=";
+    sha256 = "sha256-aw8c8VhSFexUpQqXDOd/pRSiuRl4njBe+LDONTVK7Uw=";
   };
   vendorHash = null;
 
