ccbc520ba49357efc21ef7716270ef8288923013
Vincent Demeester <vincent@sbr.pm>
2025-10-16 11:59:09 +0200

    systems/nagoya: trying something out around config.txt and boot
    It still doesn't boot on nvme and I am not sure why.
    
    Signed-off-by: Vincent Demeester <vincent@sbr.pm>

--- systems/nagoya/boot.nix
+++ systems/nagoya/boot.nix
@@ -9,6 +9,7 @@
       systemd-boot.enable = lib.mkForce false;
     };
   };
+  boot.loader.raspberryPi.enable = true;
   # nothing ?
   # system.build.installBootLoader = config.boot.loader.raspberryPi;
 }
--- systems/nagoya/config.txt.nix
+++ systems/nagoya/config.txt.nix
@@ -12,22 +12,6 @@
 
       options = {
         camera_auto_detect.enable = false;
-
-        # https://www.raspberrypi.com/documentation/computers/config_txt.html#enable_uart
-        # in conjunction with `console=serial0,115200` in kernel command line (`cmdline.txt`)
-        # creates a serial console, accessible using GPIOs 14 and 15 (pins
-        #  8 and 10 on the 40-pin header)
-        enable_uart = {
-          enable = true;
-          value = true;
-        };
-        # https://www.raspberrypi.com/documentation/computers/config_txt.html#uart_2ndstage
-        # enable debug logging to the UART, also automatically enables
-        # UART logging in `start.elf`
-        uart_2ndstage = {
-          enable = true;
-          value = true;
-        };
       };
 
       # Base DTB parameters
