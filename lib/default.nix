7abeb5b1c41b309308ef346cca98a36e35c0d43c
Vincent Demeester <vincent@sbr.pm>
2025-10-16 12:06:04 +0200

    systems/nagoya: still trying some things out with nagoya
    Signed-off-by: Vincent Demeester <vincent@sbr.pm>

--- lib/default.nix
+++ lib/default.nix
@@ -134,6 +134,8 @@
             imports = with inputs.nixos-raspberrypi.nixosModules; [
               raspberry-pi-5.base
               raspberry-pi-5.bluetooth
+              raspberry-pi-5.page-size-16k
+              raspberry-pi-5.display-vc4
             ];
           }
         )
--- systems/nagoya/hardware.nix
+++ systems/nagoya/hardware.nix
@@ -1,4 +1,9 @@
-{ inputs, lib, ... }:
+{
+  inputs,
+  pkgs,
+  lib,
+  ...
+}:
 let
   firmwarePartition = lib.recursiveUpdate {
     # label = "FIRMWARE";
@@ -45,12 +50,31 @@ let
       ];
     };
   };
+  kernelBundle = pkgs.linuxAndFirmware.v6_6_31;
 in
 {
   imports = [
     inputs.disko.nixosModules.disko
     ./config.txt.nix
   ];
+  boot = {
+    loader.raspberryPi.firmwarePackage = kernelBundle.raspberrypifw;
+    loader.raspberryPi.bootloader = "kernel";
+    kernelPackages = kernelBundle.linuxPackages_rpi5;
+  };
+
+  nixpkgs.overlays = lib.mkAfter [
+    (self: super: {
+      # This is used in (modulesPath + "/hardware/all-firmware.nix") when at least
+      # enableRedistributableFirmware is enabled
+      # I know no easier way to override this package
+      inherit (kernelBundle) raspberrypiWirelessFirmware;
+      # Some derivations want to use it as an input,
+      # e.g. raspberrypi-dtbs, omxplayer, sd-image-* modules
+      inherit (kernelBundle) raspberrypifw;
+    })
+  ];
+
   disko.devices = {
     disk.root = {
       type = "disk";
