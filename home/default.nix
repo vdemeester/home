7f9a349b64e5ac8650656a0be7771fbabc7614e7
Vincent Demeester <vincent@sbr.pm>
2025-10-17 09:29:29 +0200

    home: cleanup some comments
    Signed-off-by: Vincent Demeester <vincent@sbr.pm>

--- home/default.nix
+++ home/default.nix
@@ -36,31 +36,4 @@
     ];
     use-xdg-base-directories = true;
   };
-
-  # nixpkgs = {
-  #   overlays = [
-  #     # Our own flake exports (from overlays and pkgs dir)
-  #     outputs.overlays.additions
-  #     outputs.overlays.modifications
-  #     outputs.overlays.unstable-packages
-  #
-  #     # And from other flakes
-  #     inputs.emacs-overlay.overlay
-  #     inputs.chapeau-rouge.overlays.openshift
-  #     inputs.chick-group.overlays.default
-  #     inputs.agenix.overlays.default
-  #     inputs.niri.overlays.niri
-  #
-  #     # Migrate to "modifications"
-  #     (_: prev: {
-  #       inherit (inputs.buildkit-tekton.packages.${prev.system}) tkn-local;
-  #       inherit (inputs.dagger.packages.${prev.system}) dagger;
-  #     })
-  #   ];
-  #   config = {
-  #     allowUnfree = true;
-  #     # Workaround for https://github.com/nix-community/home-manager/issues/2942
-  #     allowUnfreePredicate = _: true;
-  #   };
-  # };
 }
