24ec42d60aa94fd44e1568bb419b252b735e9f46
Vincent Demeester <vincent@sbr.pm>
2025-10-23 21:50:42 +0200

    systems/nagoya: still trying to fix it
    Signed-off-by: Vincent Demeester <vincent@sbr.pm>

--- flake.nix
+++ flake.nix
@@ -105,9 +105,70 @@
           pkgsInput = inputs.nixpkgs-25_05;
           homeInput = inputs.home-manager-25_05;
         };
-        nagoya = libx.mkRPIHost {
-          hostname = "nagoya";
-          hardwareType = "rpi4"; # to disable tpm2 TODO find a better way
+        # nagoya = libx.mkRPIHost {
+        #   hostname = "nagoya";
+        #   hardwareType = "rpi4"; # to disable tpm2 TODO find a better way
+        # };
+        nagoya = inputs.nixos-raspberrypi.lib.nixosSystem {
+          specialArgs = inputs;
+          modules = [
+            {
+              # Hardware specific configuration, see section below for a more complete
+              # list of modules
+              imports = with inputs.nixos-raspberrypi.nixosModules; [
+                raspberry-pi-5.base
+                raspberry-pi-5.page-size-16k
+                raspberry-pi-5.display-vc4
+                raspberry-pi-5.bluetooth
+              ];
+            }
+            (
+              {
+                config,
+                pkgs,
+                lib,
+                ...
+              }:
+              {
+                networking.hostName = "nagoya";
+                boot.loader.raspberryPi.bootloader = "kernel";
+                system.nixos.tags =
+                  let
+                    cfg = config.boot.loader.raspberryPi;
+                  in
+                  [
+                    "raspberry-pi-${cfg.variant}"
+                    cfg.bootloader
+                    config.boot.kernelPackages.kernel.version
+                  ];
+                environment.systemPackages = with pkgs; [
+                  tree
+                ];
+                fileSystems = {
+                  "/boot/firmware" = {
+                    device = "/dev/disk/by-uuid/2175-794E";
+                    fsType = "vfat";
+                    options = [
+                      "noatime"
+                      "noauto"
+                      "x-systemd.automount"
+                      "x-systemd.idle-timeout=1min"
+                    ];
+                  };
+                  # "/" = {
+                  #   device = "/dev/disk/by-uuid/44444444-4444-4444-8888-888888888888";
+                  #   fsType = "ext4";
+                  #   options = [ "noatime" ];
+                  # };
+                  "/" = {
+                    device = "/dev/disk/by-uuid/e769fd8d-1fed-4a59-a987-e21f35294d5f";
+                    fsType = "ext4";
+                    options = [ "noatime" ];
+                  };
+                };
+              }
+            )
+          ];
         };
         kerkouane = libx.mkHost {
           hostname = "kerkouane";
