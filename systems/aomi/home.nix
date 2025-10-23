b640454ca13dfc74087903f2b3cdc6047f766bec
Vincent Demeester <vincent@sbr.pm>
2025-10-23 10:00:03 +0200

    systems/aomi: fix wrong import
    Signed-off-by: Vincent Demeester <vincent@sbr.pm>

--- systems/aomi/home.nix
+++ systems/aomi/home.nix
@@ -4,7 +4,7 @@
     ../../home/common/dev/default.nix
     ../../home/common/dev/emacs.nix
     ../../home/common/dev/containers.nix
-    ../../home/common/dev/tekton.nix
+    ../../home/common/dev/tektoncd.nix
   ];
   services.ssh-agent.enable = true;
   systemd.user.services.syncthing.Install.WantedBy = [ "multi-user.target" ];
