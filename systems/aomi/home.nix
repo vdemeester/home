8b0db8fddd2c28e8113f8cbe7f11f9d9123cb261
Vincent Demeester <vincent@sbr.pm>
2025-10-23 09:10:31 +0200

    systems/aomi: add emacs and containers to home
    Signed-off-by: Vincent Demeester <vincent@sbr.pm>

--- systems/aomi/home.nix
+++ systems/aomi/home.nix
@@ -2,6 +2,8 @@
 {
   imports = [
     ../../home/common/dev/base.nix
+    ../../home/common/dev/emacs.nix
+    ../../home/common/dev/containers.nix
   ];
   services.ssh-agent.enable = true;
   systemd.user.services.syncthing.Install.WantedBy = [ "multi-user.target" ];
