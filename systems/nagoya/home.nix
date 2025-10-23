e2314720cc7849a9b3c376a6ce635eab7df8b4c8
Vincent Demeester <vincent@sbr.pm>
2025-10-23 14:39:53 +0200

    systems/nagoya: add ntfy-sh package

--- systems/nagoya/home.nix
+++ systems/nagoya/home.nix
@@ -1,8 +1,12 @@
-_: {
+{ pkgs, ... }:
+{
   imports = [
     ../../home/common/dev/base.nix
     # ../../home/common/dev/emacs.nix
     ../../home/common/dev/containers.nix
   ];
   systemd.user.services.syncthing.Install.WantedBy = [ "multi-user.target" ];
+  home.packages = with pkgs; [
+    ntfy-sh
+  ];
 }
