aa082e52c70684197a318d0d39a338d564f39639
Vincent Demeester <vincent@sbr.pm>
2025-11-05 16:59:00 +0100

    systems/demeter: migrate stuff from nagoya
    Signed-off-by: Vincent Demeester <vincent@sbr.pm>

--- systems/demeter/extra.nix
+++ systems/demeter/extra.nix
@@ -1,5 +1,6 @@
 {
   libx,
+  lib,
   globals,
   ...
 }:
@@ -20,6 +21,46 @@
       ips = libx.wg-ips globals.machines.demeter.net.vpn.ips;
       endpoint = "${globals.net.vpn.endpoint}";
       endpointPublicKey = "${globals.machines.kerkouane.net.vpn.pubkey}";
+    };
+    nginx = {
+      enable = true;
+      recommendedGzipSettings = true;
+      recommendedTlsSettings = true;
+      recommendedOptimisation = true;
+      # TODO: should probably switch to sync.sbr.pm or something maybe ?
+      virtualHosts."demeter.sbr.pm" = {
+        locations = lib.attrsets.mapAttrs' (
+          name: value:
+          lib.attrsets.nameValuePair "/syncthing/${name}/" {
+            proxyPass = "http://${builtins.head value.net.vpn.ips}:8384/";
+            recommendedProxySettings = true;
+          }
+        ) (lib.attrsets.filterAttrs (_name: value: (libx.hasVPNips value)) globals.machines);
+        # // {
+        #   "/n8n/" = {
+        #     proxyPass = "http://127.0.0.1:5678/";
+        #     recommendedProxySettings = true;
+        #   };
+        # };
+      };
+      virtualHosts."demeter.vpn" = {
+        locations = lib.attrsets.mapAttrs' (
+          name: value:
+          lib.attrsets.nameValuePair "/syncthing/${name}/" {
+            proxyPass = "http://${builtins.head value.net.vpn.ips}:8384/";
+            recommendedProxySettings = true;
+          }
+        ) (lib.attrsets.filterAttrs (_name: value: (libx.hasVPNips value)) globals.machines);
+      };
+      virtualHosts."demeter.home" = {
+        locations = lib.attrsets.mapAttrs' (
+          name: value:
+          lib.attrsets.nameValuePair "/syncthing/${name}/" {
+            proxyPass = "http://${builtins.head value.net.ips}:8384/";
+            recommendedProxySettings = true;
+          }
+        ) (lib.attrsets.filterAttrs (_name: value: (libx.hasIps value)) globals.machines);
+      };
     };
   };
 
