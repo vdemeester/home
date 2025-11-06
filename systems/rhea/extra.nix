265bbfaaa0728f73f0d5d70f644cfff7c55c4781
Vincent Demeester <vincent@sbr.pm>
2025-11-06 11:26:29 +0100

    systems/rhea: add wireguard configuration
    Signed-off-by: Vincent Demeester <vincent@sbr.pm>

--- systems/rhea/extra.nix
+++ systems/rhea/extra.nix
@@ -1,6 +1,19 @@
-{ lib, pkgs, ... }:
+{
+  libx,
+  globals,
+  lib,
+  pkgs,
+  ...
+}:
 {
   users.users.vincent.linger = true;
+
+  wireguard = {
+    enable = true;
+    ips = libx.wg-ips globals.machines.rhea.net.vpn.ips;
+    endpoint = "${globals.net.vpn.endpoint}";
+    endpointPublicKey = "${globals.machines.kerkouane.net.vpn.pubkey}";
+  };
 
   services.openssh.enable = true;
   services.openssh.settings = {
