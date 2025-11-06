40e8edd3458a40ab0727de10e4dd71e3b7d906de
Vincent Demeester <vincent@sbr.pm>
2025-11-06 22:15:34 +0100

    systems/aion: update extras
    Signed-off-by: Vincent Demeester <vincent@sbr.pm>

--- systems/aion/extra.nix
+++ systems/aion/extra.nix
@@ -1,21 +1,27 @@
-{ lib, pkgs, ... }:
+{
+  libx,
+  globals,
+  lib,
+  pkgs,
+  ...
+}:
 {
   users.users.vincent.linger = true;
 
-  services.openssh.enable = true;
-  services.openssh.settings = {
-    PermitRootLogin = "yes";
-    PasswordAuthentication = true;
+  services = {
+    wireguard = {
+      enable = true;
+      ips = libx.wg-ips globals.machines.aion.net.vpn.ips;
+      endpoint = "${globals.net.vpn.endpoint}";
+      endpointPublicKey = "${globals.machines.kerkouane.net.vpn.pubkey}";
+    };
   };
 
   networking.useDHCP = lib.mkDefault true;
 
   environment.systemPackages = with pkgs; [
-    vim
-    git
-    htop
-    helix
-    # Add any additional packages you need.
+    lm_sensors
+    gnumake
   ];
 
 }
