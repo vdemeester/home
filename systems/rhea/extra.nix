30b22ca0d11e8a6e6e487974eb6e8cf870772f45
Vincent Demeester <vincent@sbr.pm>
2025-11-17 15:53:08 +0100

    systems/rhea: enable some services
    Signed-off-by: Vincent Demeester <vincent@sbr.pm>

--- systems/rhea/extra.nix
+++ systems/rhea/extra.nix
@@ -15,6 +15,42 @@
       endpoint = "${globals.net.vpn.endpoint}";
       endpointPublicKey = "${globals.machines.kerkouane.net.vpn.pubkey}";
     };
+    # smartd = {
+    #   enable = true;
+    #   devices = [ { device = "/dev/nvme0n1"; } ];
+    # };
+    aria2 = {
+      enable = true;
+      openPorts = true;
+      settings = {
+        max-concurrent-downloads = 20;
+      };
+      downloadDir = "/link/downloads";
+      rpcSecretFile = "${pkgs.writeText "aria" "aria2rpc\n"}"; # FIXME: use secrets for this somehow
+    };
+    transmission = {
+      enable = true;
+    };
+    sonarr = {
+      enable = true;
+      # user = "vincent";
+      # group = "users";
+    };
+    radarr = {
+      enable = true;
+    };
+    bazarr = {
+      enable = true;
+    };
+    prowlarr = {
+      enable = true;
+    };
+    readarr = {
+      enable = true;
+    };
+    lidarr = {
+      enable = true;
+    };
   };
 
   networking.useDHCP = lib.mkDefault true;
