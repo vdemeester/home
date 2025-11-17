2e8e64e66e120045361aebaeae7f1c78b4191cf9
Vincent Demeester <vincent@sbr.pm>
2025-11-17 15:40:22 +0100

    systems/rhea: add lvm mount point
    Signed-off-by: Vincent Demeester <vincent@sbr.pm>

--- systems/rhea/hardware.nix
+++ systems/rhea/hardware.nix
@@ -1,8 +1,15 @@
-_: {
+{ config, ... }:
+{
   fileSystems."/" = {
     device = "/dev/disk/by-uuid/44444444-4444-4444-8888-888888888888";
     fsType = "ext4";
   };
+  fileSystems."/neo" = {
+    device = "/dev/disk/by-uuid/7ff6e217-ec49-40af-96a1-ad28a24bb687";
+    fsType = "ext4";
+  };
+
+  networking.hostId = builtins.substring 0 8 (builtins.hashString "md5" config.networking.hostName);
 
   swapDevices = [ ];
 
