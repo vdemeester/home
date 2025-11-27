ca4d61166d911df19dcd43733f3f06222cdd646c
Vincent Demeester <vincent@sbr.pm>
2025-11-27 22:21:32 +0100

    systems/demeter: remove nginx service
    It is now handled by rhea and traefik.
    
    Signed-off-by: Vincent Demeester <vincent@sbr.pm>

--- systems/demeter/extra.nix
+++ systems/demeter/extra.nix
@@ -22,46 +22,6 @@
       endpoint = "${globals.net.vpn.endpoint}";
       endpointPublicKey = "${globals.machines.kerkouane.net.vpn.pubkey}";
     };
-    nginx = {
-      enable = true;
-      recommendedGzipSettings = true;
-      recommendedTlsSettings = true;
-      recommendedOptimisation = true;
-      # TODO: should probably switch to sync.sbr.pm or something maybe ?
-      virtualHosts."demeter.sbr.pm" = {
-        locations = lib.attrsets.mapAttrs' (
-          name: value:
-          lib.attrsets.nameValuePair "/syncthing/${name}/" {
-            proxyPass = "http://${builtins.head value.net.vpn.ips}:8384/";
-            recommendedProxySettings = true;
-          }
-        ) (lib.attrsets.filterAttrs (_name: value: (libx.hasVPNips value)) globals.machines);
-        # // {
-        #   "/n8n/" = {
-        #     proxyPass = "http://127.0.0.1:5678/";
-        #     recommendedProxySettings = true;
-        #   };
-        # };
-      };
-      virtualHosts."demeter.vpn" = {
-        locations = lib.attrsets.mapAttrs' (
-          name: value:
-          lib.attrsets.nameValuePair "/syncthing/${name}/" {
-            proxyPass = "http://${builtins.head value.net.vpn.ips}:8384/";
-            recommendedProxySettings = true;
-          }
-        ) (lib.attrsets.filterAttrs (_name: value: (libx.hasVPNips value)) globals.machines);
-      };
-      virtualHosts."demeter.home" = {
-        locations = lib.attrsets.mapAttrs' (
-          name: value:
-          lib.attrsets.nameValuePair "/syncthing/${name}/" {
-            proxyPass = "http://${builtins.head value.net.ips}:8384/";
-            recommendedProxySettings = true;
-          }
-        ) (lib.attrsets.filterAttrs (_name: value: (libx.hasIps value)) globals.machines);
-      };
-    };
   };
 
   # TODO: could be enable by default for all ?
