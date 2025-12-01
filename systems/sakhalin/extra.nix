f0714239e3e8d480f6c80938285346401533c315
Vincent Demeester <vincent@sbr.pm>
2025-12-01 10:34:13 +0100

    fix: Remove --library flag from kiwix-serve configuration
    - Enable service to start without requiring library.xml file
    - Allow direct serving of ZIM files from directory wildcard
    
    Signed-off-by: Vincent Demeester <vincent@sbr.pm>

--- systems/sakhalin/extra.nix
+++ systems/sakhalin/extra.nix
@@ -212,7 +212,7 @@
     serviceConfig = {
       Type = "simple";
       User = "vincent";
-      ExecStart = "${pkgs.kiwix-tools}/bin/kiwix-serve --port=8080 --library /mnt/gaia/kiwix/*.zim";
+      ExecStart = "${pkgs.kiwix-tools}/bin/kiwix-serve --port=8080 /mnt/gaia/kiwix/*.zim";
       Restart = "on-failure";
       RestartSec = "5s";
     };
