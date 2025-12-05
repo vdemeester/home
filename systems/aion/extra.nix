9449a089d5b50549771109cbb7705ce06bc49368
Vincent Demeester <vincent@sbr.pm>
2025-12-05 14:40:36 +0100

    chore(aion): Remove immich photo management service
    - Free up system resources on aion host
    - Simplify PostgreSQL configuration
    - Service no longer required on this system

--- systems/aion/extra.nix
+++ systems/aion/extra.nix
@@ -3,7 +3,6 @@
   globals,
   lib,
   pkgs,
-  config,
   ...
 }:
 {
@@ -16,36 +15,7 @@
       endpoint = "${globals.net.vpn.endpoint}";
       endpointPublicKey = "${globals.machines.kerkouane.net.vpn.pubkey}";
     };
-
-    immich = {
-      enable = true;
-      user = "vincent";
-      group = "users";
-      mediaLocation = "/neo/pictures/photos";
-    };
-
-    postgresql = {
-      ensureDatabases = [ "immich" ];
-      ensureUsers = [
-        {
-          name = "vincent";
-        }
-      ];
-    };
   };
-
-  # Grant vincent ownership and superuser privileges for the immich database
-  systemd.services.postgresql.postStart = lib.mkAfter ''
-    PSQL="${config.services.postgresql.package}/bin/psql --port=${toString config.services.postgresql.settings.port}"
-    $PSQL -tAc "SELECT 1 FROM pg_roles WHERE rolname = 'vincent'" | grep -q 1 || $PSQL -tAc "CREATE ROLE vincent WITH LOGIN SUPERUSER"
-    $PSQL -tAc "ALTER ROLE vincent WITH SUPERUSER"
-    $PSQL -tAc "ALTER DATABASE immich OWNER TO vincent"
-    $PSQL immich -tAc "ALTER SCHEMA public OWNER TO vincent"
-    $PSQL immich -tAc "GRANT ALL PRIVILEGES ON SCHEMA public TO vincent"
-    $PSQL immich -tAc "GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO vincent"
-    $PSQL immich -tAc "GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA public TO vincent"
-    $PSQL immich -tAc "ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT ALL ON TABLES TO vincent"
-  '';
 
   networking.useDHCP = lib.mkDefault true;
 
