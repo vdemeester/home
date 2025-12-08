f1de362fff443df9a358b53ef6d773250cbc8857
Vincent Demeester <vincent@sbr.pm>
2025-12-08 11:30:13 +0100

    systems/aion: add neo mountpoint
    Signed-off-by: Vincent Demeester <vincent@sbr.pm>

--- systems/aion/hardware.nix
+++ systems/aion/hardware.nix
@@ -3,6 +3,10 @@ _: {
     device = "/dev/disk/by-uuid/44444444-4444-4444-8888-888888888888";
     fsType = "ext4";
   };
+  fileSystems."/neo" = {
+    device = "/dev/disk/by-uuid/0a779a30-903e-4bef-b1bc-bcdd4a978afe";
+    fsType = "ext4";
+  };
 
   swapDevices = [ ];
 
